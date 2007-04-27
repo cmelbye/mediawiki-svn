cmelbye/mediawiki-svn

commit 11a6cddedfe6ec1ac108663ad32df6d57dd81abd

* Introduced media handler modules for file-type specific operations: thumbnailing, img_metadata, capabilities, etc. * Deprecated $wgUseImageResize, thumbnailing will be enabled unconditionally. * Fixed interaction of page parameter to ImagePage with the HTML file cache * Improved error reporting for image thumbnailing * Fixed MIME type for SVG files, will be silently changed from image/svg to image/svg+xml after loading from the database. * Workaround for djvutoxml bug #1704049 (poor performance). Use djvudump instead. * Fixed odd behaviour in ImagePage on DjVu thumbnailing errors * Improved error reporting for image thumbnailing * Added sharpening option for ImageMagick thumbnailing * Removed Image::selectPage(), added page parameters to getWidth() and getHeight(), deprecated Image::renderThumb() and Image::getThumbnail() * Changed default contents of img_metadata to empty string instead of a:0:{} * Moved responsibility for respecting $wgGenerateThumbnailOnParse from the UI to Image.php
git-svn-id: svn+ssh://svn.wikimedia.org/svnroot/mediawiki/trunk@21411 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

phase3/includes/ImagePage.php
@@ -166,11 +166,9 @@ class ImagePage extends Article {
 
 	function openShowImage() {
 		global $wgOut, $wgUser, $wgImageLimits, $wgRequest, $wgLang;
-		global $wgUseImageResize, $wgGenerateThumbnailOnParse;
 
 		$full_url  = $this->img->getURL();
-		$anchoropen = '';
-		$anchorclose = '';
+		$linkAttribs = false;
 		$sizeSel = intval( $wgUser->getOption( 'imagesize') );
 		if( !isset( $wgImageLimits[$sizeSel] ) ) {
 			$sizeSel = User::getDefaultOption( 'imagesize' );
@@ -190,10 +188,11 @@ class ImagePage extends Article {
 		if ( $this->img->exists() ) {
 			# image
 			$page = $wgRequest->getIntOrNull( 'page' );
-			if ( ! is_null( $page ) ) {
-				$this->img->selectPage( $page );
-			} else {
+			if ( is_null( $page ) ) {
+				$params = array();
 				$page = 1;
+			} else {
+				$params = array( 'page' => $page );
 			}
 			$width_orig = $this->img->getWidth();
 			$width = $width_orig;
@@ -201,6 +200,7 @@ class ImagePage extends Article {
 			$height = $height_orig;
 			$mime = $this->img->getMimeType();
 			$showLink = false;
+			$linkAttribs = array( 'href' => $full_url );
 
 			if ( $this->img->allowInlineDisplay() and $width and $height) {
 				# image
@@ -223,39 +223,33 @@ class ImagePage extends Article {
 						# Note that $height <= $maxHeight now, but might not be identical
 						# because of rounding.
 					}
+				}
+				$params['width'] = $width;
+				$thumbnail = $this->img->transform( $params );
 
-					if( $wgUseImageResize ) {
-						$thumbnail = $this->img->getThumbnail( $width, -1, $wgGenerateThumbnailOnParse );
-						if ( $thumbnail == null ) {
-							$url = $this->img->getViewURL();
-						} else {
-							$url = $thumbnail->getURL();
-						}
-					} else {
-						# No resize ability? Show the full image, but scale
-						# it down in the browser so it fits on the page.
-						$url = $this->img->getViewURL();
-					}
-					$anchoropen  = "<a href=\"{$full_url}\">";
-					$anchorclose = "</a><br />";
-					if( $this->img->mustRender() ) {
-						$showLink = true;
-					} else {
-						$anchorclose .= wfMsg('show-big-image-thumb', $width, $height ) .
-							'<br />' . "\n$anchoropen{$msgbig}</a> " . $msgsize;
-					}
-				} else {
-					$url = $this->img->getViewURL();
+				$anchorclose = "<br />";
+				if( $this->img->mustRender() ) {
 					$showLink = true;
+				} else {
+					$anchorclose .= 
+						wfMsg('show-big-image-thumb', $width, $height ) .
+						'<br />' . Xml::tags( 'a', $linkAttribs,  $msgbig ) . ' ' . $msgsize;
 				}
 
 				if ( $this->img->isMultipage() ) {
 					$wgOut->addHTML( '<table class="multipageimage"><tr><td>' );
 				}
 
-				$wgOut->addHTML( '<div class="fullImageLink" id="file">' . $anchoropen .
-				     "<img border=\"0\" src=\"{$url}\" width=\"{$width}\" height=\"{$height}\" alt=\"" .
-				     htmlspecialchars( $this->img->getTitle()->getPrefixedText() ).'" />' . $anchorclose . '</div>' );
+				$imgAttribs = array(
+					'border' => 0,
+					'alt' => $this->img->getTitle()->getPrefixedText()
+				);
+
+				if ( $thumbnail ) {
+					$wgOut->addHTML( '<div class="fullImageLink" id="file">' . 
+						$thumbnail->toHtml( $imgAttribs, $linkAttribs ) .
+						$anchorclose . '</div>' );
+				}
 
 				if ( $this->img->isMultipage() ) {
 					$count = $this->img->pageCount();
@@ -263,17 +257,17 @@ class ImagePage extends Article {
 					if ( $page > 1 ) {
 						$label = $wgOut->parse( wfMsg( 'imgmultipageprev' ), false );
 						$link = $sk->makeLinkObj( $this->mTitle, $label, 'page='. ($page-1) );
-						$this->img->selectPage( $page - 1 );
-						$thumb1 = $sk->makeThumbLinkObj( $this->img, $link, $label, 'none' );
+						$thumb1 = $sk->makeThumbLinkObj( $this->img, $link, $label, 'none', 
+							array( 'page' => $page - 1 ) );
 					} else {
 						$thumb1 = '';
 					}
 
 					if ( $page < $count ) {
 						$label = wfMsg( 'imgmultipagenext' );
-						$this->img->selectPage( $page + 1 );
 						$link = $sk->makeLinkObj( $this->mTitle, $label, 'page='. ($page+1) );
-						$thumb2 = $sk->makeThumbLinkObj( $this->img, $link, $label, 'none' );
+						$thumb2 = $sk->makeThumbLinkObj( $this->img, $link, $label, 'none', 
+							array( 'page' => $page + 1 ) );
 					} else {
 						$thumb2 = '';
 					}
@@ -294,7 +288,7 @@ class ImagePage extends Article {
 						htmlspecialchars( wfMsg( 'imgmultigo' ) ) . '"></form>';
 
 					$wgOut->addHTML( '</td><td><div class="multipageimagenavbox">' .
-					   "$select<hr />$thumb1\n$thumb2<br clear=\"all\" /></div></td></tr></table>" );
+						"$select<hr />$thumb1\n$thumb2<br clear=\"all\" /></div></td></tr></table>" );
 				}
 			} else {
 				#if direct link is allowed but it's not a renderable image, show an icon.

extensions/WebStore/404-handler.php
@@ -39,15 +39,42 @@ class WebStore404Handler extends WebStoreCommon {
 			return false;
 		}
 
-		if ( !preg_match( '!^(\w)/(\w\w)/([^/]*)/(page(\d*)-)?(\d*)px-([^/]*)$!', $rel, $parts ) ) {
+		if ( !preg_match( '!^(\w)/(\w\w)/([^/]*)/([^/]*)$!', $rel, $parts ) ) {
 			header( 'X-Debug: regex mismatch' );
 			$this->real404();
 			return false;
 		}
 
-		list( $all, $hash1, $hash2, $filename, $pagefull, $pagenum, $size, $fn2 ) = $parts;
-		if( $filename != $fn2 && "$filename.png" != $fn2 ) {
+		list( $all, $hash1, $hash2, $filename, $thumbName ) = $parts;
+		$srcNamePos = strrpos( $thumbName, $filename );
+		if ( $srcNamePos === false ) {
 			header( 'X-Debug: filename/fn2 mismatch' );
+			$this->real404();
+			return false;
+		}
+		$extraExt = substr( $thumbName, $srcNamePos + strlen( $filename ) );
+		if ( $extraExt != '' && $extraExt[0] != '.' ) {
+			header( "X-Debug: invalid trailing characters in filename: $extraExt" );
+			$this->real404();
+			return false;
+		}
+		// Determine MIME type
+		$extPos = strrpos( $filename, '.' );
+		$srcExt = $extPos === false ? '' : substr( $filename, $extPos + 1 );
+		$magic = MimeMagic::singleton();
+		$mime = $magic->guessTypesForExtension( $srcExt );
+		$handler = MediaHandler::getHandler( $mime );
+		if ( !$handler ) {
+			header( 'X-Debug: no handler' );
+			$this->real404();
+			return false;
+		}
+
+		// Parse parameter string
+		$paramString = substr( $thumbName, 0, $srcNamePos - 1 );
+		$params = $handler->parseParamString( $paramString );
+		if ( !$params ) {
+			header( "X-Debug: handler for $mime says param string is invalid" );
 			$this->real404();
 			return false;
 		}
@@ -114,7 +141,7 @@ class WebStore404Handler extends WebStoreCommon {
 
 				$post = WebStorePostFile::post( $scalerUrl, 'data', 
 					"{$this->publicDir}/$hash1/$hash2/$filename",
-					array( 'width' => $size, 'page' => $pagenum ), 
+					$params, 
 					$server, $tmpFile, $this->httpConnectTimeout, $this->httpOverallTimeout );
 
 				// Try next server unless that one was successful
@@ -132,6 +159,12 @@ class WebStore404Handler extends WebStoreCommon {
 				$info = self::$httpErrors[$post->responseCode];
 				header( "HTTP/1.1 {$post->responseCode} $info" );
 				$this->streamFile( $tmpFile );
+				break;
+			}
+
+			fseek( $tmpFile, 0, SEEK_END );
+			if ( ftell( $tmpFile ) == 0 ) {
+				$this->htmlError( 500, 'webstore_scaler_empty_response' );
 				break;
 			}
 

extensions/WebStore/WebStore.i18n.php
@@ -8,6 +8,9 @@ $messages = array(
 		'inplace_scaler_not_enough_params' => 'Not enough parameters.',
 		'inplace_scaler_invalid_image' => 'Invalid image, could not determine size.',
 		'inplace_scaler_failed' => 'An error was encountered during image scaling: $1',
+		'inplace_scaler_no_handler' => 'No handler for transforming this MIME type',
+		'inplace_scaler_no_output' => 'No transformation output file was produced.',
+		'inplace_scaler_zero_size' => 'Transformation produced a zero-sized output file.',
 
 		'webstore_access' => 'This service is restricted by client IP.',
 		'webstore_path_invalid' => 'The filename was invalid.',
@@ -28,11 +31,12 @@ $messages = array(
 		'webstore_temp_copy' => 'Error copying temporary file "$1" to destination file "$2".',
 		'webstore_temp_close' => 'Error closing temporary file "$1".',
 		'webstore_temp_lock' => 'Error locking temporary file "$1".',
+		'webstore_no_archive' => 'Destination file exists and no archive was given.',
 
 		'webstore_no_file' => 'No file was uploaded.',
 		'webstore_move_uploaded' => 'Error moving uploaded file "$1" to temporary location "$2".',
 
-		'webstore_invalid_repository' => 'Invalid repository "$1".',
+		'webstore_invalid_zone' => 'Invalid zone "$1".',
 
 		'webstore_no_deleted' => 'No archive directory for deleted files is defined.',
 		'webstore_curl' => 'Error from cURL: $1',
@@ -40,11 +44,14 @@ $messages = array(
 		'webstore_php_warning' => 'PHP Warning: $1',
 		'webstore_metadata_not_found' => 'File not found: $1',
 		'webstore_postfile_not_found' => 'File to post not found.',
+		'webstore_scaler_empty_response' => 'The image scaler gave an empty response with a 200 ' .
+			'response code. This could be due to a PHP fatal error in the scaler.',
 
 		'webstore_invalid_response' => "Invalid response from server:\n\n$1\n",
 		'webstore_no_response' => 'No response from server',
 		'webstore_backend_error' => "Error from storage server:\n\n$1\n",
 		'webstore_php_error' => 'PHP errors were encountered:',
+		'webstore_no_handler' => 'No handler for transforming this MIME type',
 	),
 );
 

extensions/WebStore/WebStore.php
@@ -64,6 +64,7 @@ $wgWebStoreSettings = array(
 
 $wgAutoloadClasses['WebStoreClient'] = 'extensions/WebStore/WebStoreClient.php';
 $wgAutoloadClasses['WebStoreCommon'] = 'extensions/WebStore/WebStoreCommon.php';
+$wgAutoloadClasses['WebStoreLocalImage'] = 'extensions/WebStore/WebStoreCommon.php';
 $wgAutoloadClasses['WebStorePostFile'] = 'extensions/WebStore/WebStorePostFile.php';
 $wgHooks['LoadAllMessages'][] = 'WebStoreCommon::initialiseMessages';
 

extensions/WebStore/WebStoreClient.php
@@ -66,20 +66,29 @@ class WebStoreClient extends FileStore {
 	/**
 	 * Publish a file, return "archived" if the file existed already and was archived,
 	 * "new" if the file didn't exist, and "failure" if there was a problem. Details
-	 * of the problem are put in $this->lastError
+	 * of the problem are put in $this->lastError.
+	 *
+	 * The source zone may be one of "local", "temp", "public" or "deleted". The 
+	 * file is copied if it is "public", and moved otherwise.
 	 */
-	function publish( $srcRepo, $srcRel, $dstRel, $archiveRel ) {
+	function publish( $srcZone, $srcRel, $dstRel, $archiveRel ) {
 		global $wgServer, $wgScriptPath;
 
-		if ( $srcRepo == 'local' ) {
-			// Need to store the file first.
-			$srcRel = $this->store( $srcRel );
-			$srcRepo = 'temp';
+		if ( $srcZone == 'local' ) {
+			// Local pseudo-zone, need to store the file first.
+			$sharedSrcRel = $this->store( $srcRel );
+			if ( !$sharedSrcRel ) {
+				return 'failure';
+			}
+			// Delete temporary source file, to simulate rename()
+			unlink( $srcRel );
+			$srcRel = $sharedSrcRel;
+			$srcZone = 'temp';
 		}
 
 		$content = $this->post( $this->getURL( 'publish.php' ),
 			array(
-				'srcRepo' => $srcRepo,
+				'srcZone' => $srcZone,
 				'src' => $srcRel,
 				'dst' => $dstRel,
 				'archive' => $archiveRel
@@ -113,10 +122,10 @@ class WebStoreClient extends FileStore {
 		return isset( $response->status ) && $response->status == 'success';
 	}
 
-	function metadata( $repository, $path ) {
+	function metadata( $zone, $path ) {
 		$content = $this->post( $this->getURL( 'metadata.php' ),
 			array( 
-				'repository' => $repository,
+				'zone' => $zone,
 				'path' => $path,
 			));
 		$response = $this->parseResponse( $content );

extensions/WebStore/WebStoreCommon.php
@@ -79,20 +79,32 @@ EOT;
 	}
 
     function htmlError( $code, $msgName /*, ... */ ) {
-        $params = array_slice( func_get_args(), 1 );
-        $msgText = htmlspecialchars( call_user_func_array( 'wfMsg', $params ) );
-        $encMsgName = htmlspecialchars( $msgName );
-        $info = self::$httpErrors[$code];
+		$params = array_slice( func_get_args(), 1 );
+		$this->htmlErrorReal( $code, $msgName, $params );
+	}
+
+	function htmlErrorReal( $code, $msgName, $msgParams = array(), $extra = '' ) {
+		global $wgLogo;
+        $msgText = htmlspecialchars( wfMsgReal( $msgName, $msgParams ) );
+		$encMsgName = htmlspecialchars( $msgName );
+		$info = self::$httpErrors[$code];
+		$logo = htmlspecialchars( $wgLogo );
         header( "HTTP/1.1 $code $info" );
         echo $this->dtd();
         echo <<<EOT
-<html><head><title>$info</title></head>
-<body><h1>$info</h1><p>
+<html>
+<head>
+<title>$info</title></head>
+<body>
+<h1><img src="$logo" style='float:left;margin-right:1em' alt=''>$info</h1><p>
 $encMsgName: $msgText
-</p></body></html>
+</p>
+$extra
+</body>
+</html>
 
 EOT;
-    }
+	}
 
 	function executeCommon() {
 		if ( !$this->checkAccess() ) {
@@ -378,10 +390,10 @@ EOT;
 	}
 
 	/**
-	 * Get the root directory for a given repository: public, temp or deleted
+	 * Get the root directory for a given zone: public, temp or deleted
 	 */
-	function getRepositoryRoot( $repository ) {
-		switch ( $repository ) {
+	function getZoneRoot( $zone ) {
+		switch ( $zone ) {
 			case 'public':
 				return $this->publicDir;
 			case 'temp':
@@ -480,5 +492,59 @@ class WebStoreWarning extends WebStoreError {
 		$this->params = array_slice( func_get_args(), 1 );
 	}
 }
+
+class WebStoreLocalImage {
+	function __construct( $path, $mime ) {
+		$this->imagePath = $path;
+		$this->mime = $mime;
+		$this->handler = MediaHandler::getHandler( $mime );
+		$this->dims = array();
+	}
+
+	function getPageDimensions( $page = 1 ) {
+		if ( !isset( $this->dims[$page] ) ) {
+			$this->dims[$page] = $this->handler->getPageDimensions( $this, $page );
+		}
+		return $this->dims[$page];
+	}
+
+	function getWidth( $page = 1 ) {
+		$dim = $this->getPageDimensions( $page );
+		return $dim['width'];
+	}
+
+	function getHeight( $page = 1 ) {
+		$dim = $this->getPageDimensions( $page );
+		return $dim['height'];
+	}
+
+	function getMimeType() {
+		return $this->mime;
+	}
+
+	function getImagePath() {
+		return $this->imagePath;
+	}
+
+	function getHandler() {
+		return $this->handler;
+	}
+
+	function getImageSize() {
+		return $this->handler->getImageSize( $this, $this->getImagePath() );
+	}
+
+	function getMetadata() {
+		if ( !isset( $this->metadata ) ) {
+			$this->metadata = $this->handler->getMetadata( $this, $this->getImagePath() );
+		}
+		return $this->metadata;
+	}
+
+	function getURL() {
+		return false;
+	}
+}
+
 
 ?>

extensions/WebStore/inplace-scaler.php
@@ -7,7 +7,7 @@ class InplaceScaler extends WebStoreCommon {
 		global $wgRequest, $wgContLanguageCode;
 
 		if ( !$this->scalerAccessRanges ) {
-			$this->error( 403, 'inplace_access_disabled' );
+			$this->htmlError( 403, 'inplace_access_disabled' );
 			return false;
 		}
 
@@ -24,7 +24,7 @@ class InplaceScaler extends WebStoreCommon {
 		}
 		
 		if ( !$allowed ) {
-			$this->error( 403, 'inplace_access_denied' );
+			$this->htmlError( 403, 'inplace_access_denied' );
 			return false;
 		}
 
@@ -49,20 +49,18 @@ class InplaceScaler extends WebStoreCommon {
 		$tempDir = $this->tmpDir . '/' . gmdate( self::$tempDirFormat );
 		if ( !is_dir( $tempDir ) ) {
 			if ( !wfMkdirParents( $tempDir ) ) {
-				$this->error( 500, 'inplace_scaler_no_temp' );
+				$this->htmlError( 500, 'inplace_scaler_no_temp' );
 				return false;
 			}
 		}
 
 		$name = $wgRequest->getFileName( 'data' );
 		$srcTemp = $wgRequest->getFileTempname( 'data' );
-		$page = $wgRequest->getInt( 'page', 1 );
-		$dstWidth = $wgRequest->getInt( 'width', 0 );
 
-		# Check that the parameters are present
-		if ( is_null( $name ) || !$dstWidth ) {
-			$this->error( 400, 'inplace_scaler_not_enough_params' );
-			return false;
+		$params = $_REQUEST;
+		unset( $params['file'] );
+		if ( get_magic_quotes_gpc() ) { 
+			$params = array_map( 'stripslashes', $params );
 		}
 
 		$i = strrpos( $name, '.' );
@@ -70,16 +68,26 @@ class InplaceScaler extends WebStoreCommon {
 
 		$magic = MimeMagic::singleton();
 		$mime = $magic->guessTypesForExtension( $ext );
-		$deja = false;
-		$size = Image::getImageSize( $srcTemp, $mime, $deja );
-		if ( !$size ) {
-			$this->error( 400, 'inplace_scaler_invalid_image' );
+
+		$image = new WebStoreLocalImage( $srcTemp, $mime );
+
+		$handler = $image->getHandler();
+		if ( !$handler ) {
+			$this->htmlError( 400, 'inplace_scaler_no_handler' );
 			return false;
 		}
 
-		$dstHeight = Image::scaleHeight( $size[0], $size[1], $dstWidth );
+		if ( !isset( $params['page'] ) ) {
+			$params['page'] = 1;
+		}
+		$srcWidth = $image->getWidth( $params['page'] );
+		$srcHeight = $image->getHeight( $params['page'] );
+		if ( $srcWidth <= 0 || $srcHeight <= 0 ) {
+			$this->htmlError( 400, 'inplace_scaler_invalid_image' );
+			return false;
+		}
 
-		list( $dstExt, $dstMime ) = Image::getThumbType( $ext, $mime );
+		list( $dstExt, $dstMime ) = $handler->getThumbType( $ext, $mime );
 		if ( preg_match( '/[ \\n;=]/', $name ) ) {
 			$dstName = "thumb.$ext";
 		} else {
@@ -91,13 +99,26 @@ class InplaceScaler extends WebStoreCommon {
 
 		$dstTemp = tempnam( $tempDir, 'mwimg' );
 
-		$error = Image::reallyRenderThumb( $srcTemp, $dstTemp, $mime, $dstWidth, $dstHeight, $page );
-		if ( $error !== true ) {
-			$this->error( 500, 'inplace_scaler_failed', $error );
-			@unlink( $dstTemp );
+		$thumb = $handler->doTransform( $image, $dstTemp, false, $params );
+		if ( !$thumb || $thumb->isError()  ) {
+			$error = $thumb ? $thumb->getHtmlMsg() : '';
+			$this->htmlErrorReal( 500, 'inplace_scaler_failed', array(''), $error );
+			unlink( $dstTemp );
+			return false;
+		}
+		$stat = stat( $dstTemp );
+		if ( !$stat  ) {
+			$this->htmlError( 500, 'inplace_scaler_no_output' );
 			return false;
 		}
 
+		if ( $stat['size'] == 0 ) {
+			$this->htmlError( 500, 'inplace_scaler_no_output' );
+			unlink( $dstTemp );
+			return false;
+		}
+
+		wfDebug( __METHOD__.": transformation completed successfully, streaming output...\n" );
 		header( "Content-Type: $dstMime" );
 		header( "Content-Disposition: inline;filename*=utf-8'$wgContLanguageCode'" . urlencode( $dstName ) );
 		readfile( $dstTemp );
@@ -105,7 +126,10 @@ class InplaceScaler extends WebStoreCommon {
 	}
 }
 
-
+// Fatal errors can cause PHP to spew out some HTML and exit with a 200 response, 
+// which would leave a corrupt image file permanently on disk. Prevent this from
+// happening.
+ini_set( 'display_errors', false );
 $s = new InplaceScaler;
 $s->execute();
 

extensions/WebStore/metadata.php
@@ -6,18 +6,6 @@
 
 require( dirname( __FILE__ ) . '/WebStoreStart.php' );
 
-class WebStoreImage extends Image {
-	function __construct( $path ) {
-		$title = Title::makeTitle( NS_IMAGE, 'Metadata.php dummy image');
-		$this->imagePath = $path;
-		parent::__construct( $title );
-	}
-
-	function getFullPath() {
-		return $this->imagePath;
-	}
-}
-
 class WebStoreMetadata extends WebStoreCommon {
 	function execute() {
 		global $wgRequest;
@@ -29,7 +17,7 @@ class WebStoreMetadata extends WebStoreCommon {
 <head><title>metadata.php Test interface</title>
 <body>
 <form method="post" action="metadata.php">
-<p>Repository: <select name="repository" value="public">
+<p>Zone: <select name="zone" value="public">
 <option>public</option>
 <option>temp</option>
 <option>deleted</option>
@@ -43,10 +31,10 @@ class WebStoreMetadata extends WebStoreCommon {
 			return true;
 		}
 
-		$repository = $wgRequest->getVal( 'repository' );
-		$root = $this->getRepositoryRoot( $repository );
+		$zone = $wgRequest->getVal( 'zone' );
+		$root = $this->getZoneRoot( $zone );
 		if ( strval( $root ) == '' ) {
-				$this->error( 400, 'webstore_invalid_repository', $repository );
+				$this->error( 400, 'webstore_invalid_zone', $zone );
 				return false;
 		}
 
@@ -58,21 +46,42 @@ class WebStoreMetadata extends WebStoreCommon {
 
 		$fullPath = $root . '/' . $rel;
 
-		if ( !file_exists( $fullPath ) ) {
+		$name = basename( $fullPath );
+		$i = strrpos( $name, '.' );
+		$ext = Image::normalizeExtension( $i ? substr( $name, $i + 1 ) : '' );
+		$magic = MimeMagic::singleton();
+		$mime = $magic->guessTypesForExtension( $ext );
+		$type = $magic->getMediaType( $fullPath, $mime);
+
+		$stat = stat( $fullPath );
+		if ( !$stat ) {
 			$this->error( 400, 'webstore_metadata_not_found', $fullPath );
 			return false;
 		}
 
-		$image = new WebStoreImage( $fullPath );
-		$image->loadFromFile();
+		$image = new WebStoreLocalImage( $fullPath, $mime );
+		if ( !$image->getHandler() ) {
+			$this->error( 400, 'webstore_no_handler' );
+			return false;
+		}
+		$gis = $image->getImageSize();
+		$handlerMeta = $image->getMetadata();
+		$stat = stat( $fullPath );
 
-		$fields = array( 'width', 'height', 'bits', 'type', 'mime', 'metadata', 'size' );
+		$metadata = array(
+			'width' => $gis[0],
+			'height' => $gis[1],
+			'bits' => isset( $gis['bits'] ) ? $gis['bits'] : '',
+			'type' => $type, 
+			'mime' => $mime,
+			'metadata' => $handlerMeta,
+			'size' => $stat['size'],
+		);
 
 		header( 'Content-Type: text/xml' );
 		echo "<?xml version=\"1.0\" encoding=\"utf-8\"?>\n<response><status>success</status><metadata>\n";
-		foreach ( $fields as $field ) {
-			$value = $image->$field;
-			if ( is_bool( $image->$field ) ) {
+		foreach ( $metadata as $field => $value ) {
+			if ( is_bool( $value ) ) {
 				$value = $value ? 1 : 0;
 			}
 			echo "<item name=\"$field\">" . htmlspecialchars( $value ) . "</item>\n";

commit 05d685ab375ce380f0ba504e924b4e9ef8594525

Fix regression, broken since r21411: Wrong messages were shown beneath small images at image description page.
git-svn-id: svn+ssh://svn.wikimedia.org/svnroot/mediawiki/trunk@21673 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

phase3/includes/ImagePage.php
@@ -208,7 +208,6 @@ class ImagePage extends Article {
 				# image
 
 				# "Download high res version" link below the image
-				$msgbig  = wfMsgHtml('show-big-image');
 				$msgsize = wfMsgHtml('file-info-size', $width_orig, $height_orig, $sk->formatSize( $this->img->getSize() ), $mime );
 				# We'll show a thumbnail of this image
 				if ( $width > $maxWidth || $height > $maxHeight ) {
@@ -225,7 +224,14 @@ class ImagePage extends Article {
 						# Note that $height <= $maxHeight now, but might not be identical
 						# because of rounding.
 					}
+					$msgbig  = wfMsgHtml('show-big-image');
+					$msgsmall = wfMsg('show-big-image-thumb', $width, $height );
+				} else {
+					# Image is small enough to show full size on image page
+					$msgbig = $this->img->getName();
+					$msgsmall = wfMsg( 'file-nohires' );
 				}
+
 				$params['width'] = $width;
 				$thumbnail = $this->img->transform( $params );
 
@@ -234,7 +240,7 @@ class ImagePage extends Article {
 					$showLink = true;
 				} else {
 					$anchorclose .= 
-						wfMsg('show-big-image-thumb', $width, $height ) .
+						$msgsmall .
 						'<br />' . Xml::tags( 'a', $linkAttribs,  $msgbig ) . ' ' . $msgsize;
 				}
 
@@ -317,13 +323,6 @@ class ImagePage extends Article {
 				// Check for MIME type. Other types may have more information in the future.
 				if (substr($mime,0,9) == 'image/svg' ) {
 					$infores = wfMsg('file-svg', $width_orig, $height_orig ) . '<br />';
-				} elseif ( substr($mime,0,5) == 'image' ) { 
-					$infores = wfMsg('file-nohires') . '<br />';
-					$info = wfMsg( 
-						'file-info-size',
-						$width_orig, $height_orig,
-						$sk->formatSize( $this->img->getSize() ),
-						$mime );
 				}
 
 				global $wgContLang;