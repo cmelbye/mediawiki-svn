cmelbye/mediawiki-svn

commit 45697e6cf65d75bb50afb09b2c2c8db6a1031919

* Split off ultimate base class FileRepo from FSRepo * Use rawurlencode() for paths instead of urlencode() * Moved functionality of LocalFile::loadFromFile() to a public static function. This allows Special:Upload to entirely avoid loading metadata from the (potentially remote) store. Changed purgeMetadataCache() to use loadFromDB() instead of loadFromFile(). * Redefined private virtual URLs, giving them a repo name component instead of a host. * Moved is_writable($wgUploadDirectory) in SpecialUpload.php to the repo * Added some filename validation to the FSRepo's store/publish * Changed store and publish to use relative destinations instead of absolute, as was documented * Deprecated File::recordUpload()! Now use upload() to do both publish and record. Moved the UI bits to SpecialUpload. * Create a null revision on reupload * Changed most of the member variable names in UploadForm. $this->mUpload followed by some ambiguous word or abbreviation was not a good naming convention. Also did some reformatting and assorted code cleanup.
git-svn-id: svn+ssh://svn.wikimedia.org/svnroot/mediawiki/trunk@23023 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

phase3/includes/filerepo/LocalFile.php
@@ -150,58 +150,7 @@ class LocalFile extends File
 	 * Load metadata from the file itself
 	 */
 	function loadFromFile() {
-		wfProfileIn( __METHOD__ );
-		$path = $this->getPath();
-		$this->fileExists = file_exists( $path );
-		$gis = array();
-
-		if ( $this->fileExists ) {
-			$magic=& MimeMagic::singleton();
-
-			$this->mime = $magic->guessMimeType($path,true);
-			list( $this->major_mime, $this->minor_mime ) = self::splitMime( $this->mime );
-			$this->media_type = $magic->getMediaType($path,$this->mime);
-			$handler = MediaHandler::getHandler( $this->mime );
-
-			# Get size in bytes
-			$this->size = filesize( $path );
-
-			# Height, width and metadata
-			if ( $handler ) {
-				$gis = $handler->getImageSize( $this, $path );
-				$this->metadata = $handler->getMetadata( $this, $path );
-			} else {
-				$gis = false;
-				$this->metadata = '';
-			}
-
-			wfDebug(__METHOD__.": $path loaded, {$this->size} bytes, {$this->mime}.\n");
-		} else {
-			$this->mime = NULL;
-			$this->media_type = MEDIATYPE_UNKNOWN;
-			$this->metadata = '';
-			wfDebug(__METHOD__.": $path NOT FOUND!\n");
-		}
-
-		if( $gis ) {
-			$this->width = $gis[0];
-			$this->height = $gis[1];
-		} else {
-			$this->width = 0;
-			$this->height = 0;
-		}
-
-		#NOTE: $gis[2] contains a code for the image type. This is no longer used.
-
-		#NOTE: we have to set this flag early to avoid load() to be called
-		# be some of the functions below. This may lead to recursion or other bad things!
-		# as ther's only one thread of execution, this should be safe anyway.
-		$this->dataLoaded = true;
-
-		if ( isset( $gis['bits'] ) )  $this->bits = $gis['bits'];
-		else $this->bits = 0;
-
-		wfProfileOut( __METHOD__ );
+		$this->setProps( self::getInfoFromPath( $this->getPath() ) );
 	}
 
 	function getCacheFields( $prefix = 'img_' ) {
@@ -347,6 +296,17 @@ class LocalFile extends File
 		);
 		$this->saveToCache();
 		wfProfileOut( __METHOD__ );
+	}
+
+	function setProps( $info ) {
+		$this->dataLoaded = true;
+		$fields = $this->getCacheFields( '' );
+		$fields[] = 'fileExists';
+		foreach ( $fields as $field ) {
+			if ( isset( $info[$field] ) ) {
+				$this->$field = $info[$field];
+			}
+		}
 	}
 
 	/** splitMime inherited */
@@ -517,20 +477,29 @@ class LocalFile extends File
 	 * Refresh metadata in memcached, but don't touch thumbnails or squid
 	 */
 	function purgeMetadataCache() {
-		clearstatcache();
-		$this->loadFromFile();
+		$this->loadFromDB();
 		$this->saveToCache();
 	}
 
 	/**
 	 * Delete all previously generated thumbnails, refresh metadata in memcached and purge the squid
 	 */
-	function purgeCache( $archiveFiles = array() ) {
-		global $wgUseSquid;
-
+	function purgeCache() {
 		// Refresh metadata cache
 		$this->purgeMetadataCache();
 
+		// Delete thumbnails
+		$this->purgeThumbnails();
+
+		// Purge squid cache for this file
+		wfPurgeSquidServers( array( $this->getURL() ) );
+	}
+
+	/**
+	 * Delete cached transformed files
+	 */
+	function purgeThumbnails() {
+		global $wgUseSquid;
 		// Delete thumbnails
 		$files = $this->getThumbnails();
 		$dir = $this->getThumbPath();
@@ -548,10 +517,6 @@ class LocalFile extends File
 
 		// Purge the squid
 		if ( $wgUseSquid ) {
-			$urls[] = $this->getURL();
-			foreach ( $archiveFiles as $file ) {
-				$urls[] = $this->getArchiveUrl( $file );
-			}
 			wfPurgeSquidServers( $urls );
 		}
 	}
@@ -632,17 +597,66 @@ class LocalFile extends File
 	/** isHashed inherited */
 
 	/**
+	 * Upload a file and record it in the DB
+	 * @param string $srcPath Source path or virtual URL
+	 * @param string $comment Upload description
+	 * @param string $pageText Text to use for the new description page, if a new description page is created
+	 * @param integer $flags Flags for publish()
+	 * @param array $props File properties, if known. This can be used to reduce the
+	 *                         upload time when uploading virtual URLs for which the file info
+	 *                         is already known
+	 * @param string $timestamp Timestamp for img_timestamp, or false to use the current time
+	 *
+	 * @return Wikitext-formatted WikiError or true on success
+	 */
+	function upload( $srcPath, $comment, $pageText, $flags = 0, $props = false, $timestamp = false ) {
+		$archive = $this->publish( $srcPath, $flags );
+		if ( WikiError::isError( $archive ) ){ 
+			return $archive;
+		}
+		if ( !$this->recordUpload2( $archive, $comment, $pageText, $props, $timestamp ) ) {
+			return new WikiErrorMsg( 'filenotfound', wfEscapeWikiText( $srcPath ) );
+		}
+		return true;
+	}
+
+	/**
 	 * Record a file upload in the upload log and the image table
+	 * @deprecated use upload()
 	 */
 	function recordUpload( $oldver, $desc, $license = '', $copyStatus = '', $source = '', 
 		$watch = false, $timestamp = false ) 
 	{
-		global $wgUser, $wgUseCopyrightUpload;
+		$pageText = UploadForm::getInitialPageText( $desc, $license, $copyStatus, $source );
+		if ( !$this->recordUpload2( $oldver, $desc, $pageText ) ) {
+			return false;
+		}
+		if ( $watch ) {
+			global $wgUser;
+			$wgUser->addWatch( $this->getTitle() );
+		}
+		return true;
+
+	}
+
+	/**
+	 * Record a file upload in the upload log and the image table
+	 */
+	function recordUpload2( $oldver, $comment, $pageText, $props = false, $timestamp = false ) 
+	{
+		global $wgUser;
 
 		$dbw = $this->repo->getMasterDB();
 
+		if ( !$props ) {
+			$props = $this->repo->getFileProps( $this->getVirtualUrl() );
+		}
+		$this->setProps( $props );
+
 		// Delete thumbnails and refresh the metadata cache
-		$this->purgeCache();
+		$this->purgeThumbnails();
+		$this->saveToCache();
+		wfPurgeSquidServers( array( $this->getURL() ) );
 
 		// Fail now if the file isn't there
 		if ( !$this->fileExists ) {
@@ -650,35 +664,8 @@ class LocalFile extends File
 			return false;
 		}
 
-		if ( $wgUseCopyrightUpload ) {
-			if ( $license != '' ) {
-				$licensetxt = '== ' . wfMsgForContent( 'license' ) . " ==\n" . '{{' . $license . '}}' . "\n";
-			}
-			$textdesc = '== ' . wfMsg ( 'filedesc' ) . " ==\n" . $desc . "\n" .
-			  '== ' . wfMsgForContent ( 'filestatus' ) . " ==\n" . $copyStatus . "\n" .
-			  "$licensetxt" .
-			  '== ' . wfMsgForContent ( 'filesource' ) . " ==\n" . $source ;
-		} else {
-			if ( $license != '' ) {
-				$filedesc = $desc == '' ? '' : '== ' . wfMsg ( 'filedesc' ) . " ==\n" . $desc . "\n";
-				 $textdesc = $filedesc .
-					 '== ' . wfMsgForContent ( 'license' ) . " ==\n" . '{{' . $license . '}}' . "\n";
-			} else {
-				$textdesc = $desc;
-			}
-		}
-
 		if ( $timestamp === false ) {
 			$timestamp = $dbw->timestamp();
-		}
-
-		#split mime type
-		if (strpos($this->mime,'/')!==false) {
-			list($major,$minor)= explode('/',$this->mime,2);
-		}
-		else {
-			$major= $this->mime;
-			$minor= "unknown";
 		}
 
 		# Test to see if the row exists using INSERT IGNORE
@@ -692,10 +679,10 @@ class LocalFile extends File
 				'img_height' => intval( $this->height ),
 				'img_bits' => $this->bits,
 				'img_media_type' => $this->media_type,
-				'img_major_mime' => $major,
-				'img_minor_mime' => $minor,
+				'img_major_mime' => $this->major_mime,
+				'img_minor_mime' => $this->minor_mime,
 				'img_timestamp' => $timestamp,
-				'img_description' => $desc,
+				'img_description' => $comment,
 				'img_user' => $wgUser->getID(),
 				'img_user_text' => $wgUser->getName(),
 				'img_metadata' => $this->metadata,
@@ -730,10 +717,10 @@ class LocalFile extends File
 					'img_height' => intval( $this->height ),
 					'img_bits' => $this->bits,
 					'img_media_type' => $this->media_type,
-					'img_major_mime' => $major,
-					'img_minor_mime' => $minor,
+					'img_major_mime' => $this->major_mime,
+					'img_minor_mime' => $this->minor_mime,
 					'img_timestamp' => $timestamp,
-					'img_description' => $desc,
+					'img_description' => $comment,
 					'img_user' => $wgUser->getID(),
 					'img_user_text' => $wgUser->getName(),
 					'img_metadata' => $this->metadata,
@@ -750,30 +737,27 @@ class LocalFile extends File
 
 		$descTitle = $this->getTitle();
 		$article = new Article( $descTitle );
-		$minor = false;
-		$watch = $watch || $wgUser->isWatched( $descTitle );
-		$suppressRC = true; // There's already a log entry, so don't double the RC load
+
+		# Add the log entry
+		$log = new LogPage( 'upload' );
+		$log->addEntry( 'upload', $descTitle, $comment );
 
 		if( $descTitle->exists() ) {
-			// TODO: insert a null revision into the page history for this update.
-			if( $watch ) {
-				$wgUser->addWatch( $descTitle );
-			}
+			# Create a null revision
+			$nullRevision = Revision::newNullRevision( $dbw, $descTitle->getArticleId(), $log->getRcComment(), false );
+			$nullRevision->insertOn( $dbw );
 
 			# Invalidate the cache for the description page
 			$descTitle->invalidateCache();
 			$descTitle->purgeSquid();
 		} else {
 			// New file; create the description page.
-			$article->insertNewArticle( $textdesc, $desc, $minor, $watch, $suppressRC );
+			// There's already a log entry, so don't make a second RC entry
+			$article->doEdit( $pageText, $comment, EDIT_NEW | EDIT_SUPPRESS_RC );
 		}
 
 		# Hooks, hooks, the magic of hooks...
 		wfRunHooks( 'FileUpload', array( $this ) );
-
-		# Add the log entry
-		$log = new LogPage( 'upload' );
-		$log->addEntry( 'upload', $descTitle, $desc );
 
 		# Commit the transaction now, in case something goes wrong later
 		# The most important thing is that files don't get lost, especially archives
@@ -803,11 +787,11 @@ class LocalFile extends File
 	 *     file, and a wikitext-formatted WikiError object on failure. 
 	 */
 	function publish( $srcPath, $flags = 0 ) {
-		$dstPath = $this->getFullPath();
+		$dstRel = $this->getRel();
 		$archiveName = gmdate( 'YmdHis' ) . '!'. $this->getName();
-		$archivePath = $this->getArchivePath( $archiveName );
+		$archiveRel = 'archive/' . $this->getHashPath() . $archiveName;
 		$flags = $flags & File::DELETE_SOURCE ? LocalRepo::DELETE_SOURCE : 0;
-		$status = $this->repo->publish( $srcPath, $dstPath, $archivePath, $flags );
+		$status = $this->repo->publish( $srcPath, $dstRel, $archiveRel, $flags );
 		if ( WikiError::isError( $status ) ) {
 			return $status;
 		} elseif ( $status == 'new' ) {

phase3/includes/AutoLoader.php
@@ -254,6 +254,7 @@ function __autoload($className) {
 		# filerepo
 		'ArchivedFile' => 'includes/filerepo/ArchivedFile.php',
 		'File' => 'includes/filerepo/File.php',
+		'FileRepo' => 'includes/filerepo/FileRepo.php',
 		'ForeignDBFile' => 'includes/filerepo/ForeignDBFile.php',
 		'ForeignDBRepo' => 'includes/filerepo/ForeignDBRepo.php',
 		'FSRepo' => 'includes/filerepo/FSRepo.php',

phase3/includes/DefaultSettings.php
@@ -308,34 +308,34 @@ $wgAntivirus= NULL;
  *
  * @global array $wgAntivirusSetup
  */
-$wgAntivirusSetup= array(
+$wgAntivirusSetup = array(
 
 	#setup for clamav
 	'clamav' => array (
 		'command' => "clamscan --no-summary ",
 
-		'codemap'=> array (
-			"0"=>  AV_NO_VIRUS, #no virus
-			"1"=>  AV_VIRUS_FOUND, #virus found
-			"52"=> AV_SCAN_ABORTED, #unsupported file format (probably imune)
-			"*"=>  AV_SCAN_FAILED, #else scan failed
+		'codemap' => array (
+			"0" =>  AV_NO_VIRUS, # no virus
+			"1" =>  AV_VIRUS_FOUND, # virus found
+			"52" => AV_SCAN_ABORTED, # unsupported file format (probably imune)
+			"*" =>  AV_SCAN_FAILED, # else scan failed
 		),
 
-		'messagepattern'=> '/.*?:(.*)/sim',
+		'messagepattern' => '/.*?:(.*)/sim',
 	),
 
 	#setup for f-prot
 	'f-prot' => array (
 		'command' => "f-prot ",
 
-		'codemap'=> array (
-			"0"=> AV_NO_VIRUS, #no virus
-			"3"=> AV_VIRUS_FOUND, #virus found
-			"6"=> AV_VIRUS_FOUND, #virus found
-			"*"=> AV_SCAN_FAILED, #else scan failed
+		'codemap' => array (
+			"0" => AV_NO_VIRUS, # no virus
+			"3" => AV_VIRUS_FOUND, # virus found
+			"6" => AV_VIRUS_FOUND, # virus found
+			"*" => AV_SCAN_FAILED, # else scan failed
 		),
 
-		'messagepattern'=> '/.*?Infection:(.*)$/m',
+		'messagepattern' => '/.*?Infection:(.*)$/m',
 	),
 );
 

phase3/includes/ImagePage.php
@@ -664,7 +664,8 @@ EOT
 		}
 
 		$sourcePath = $this->img->getArchiveVirtualUrl( $oldimage );
-		$result = $this->img->publish( $sourcePath );
+		$comment = wfMsg( "reverted" );
+		$result = $this->img->upload( $sourcePath, $comment, $comment );
 
 		if ( WikiError::isError( $result ) ) {
 			$this->showError( $result );

phase3/includes/LogPage.php
@@ -79,18 +79,22 @@ class LogPage {
 		# And update recentchanges
 		if ( $this->updateRecentChanges ) {
 			$titleObj = SpecialPage::getTitleFor( 'Log', $this->type );
-			$rcComment = $this->actionText;
-			if( '' != $this->comment ) {
-				if ($rcComment == '')
-					$rcComment = $this->comment;
-				else
-					$rcComment .= ': ' . $this->comment;
-			}
-
+			$rcComment = $this->getRcComment();
 			RecentChange::notifyLog( $now, $titleObj, $wgUser, $rcComment, '',
 				$this->type, $this->action, $this->target, $this->comment, $this->params );
 		}
 		return true;
+	}
+
+	public function getRcComment() {
+		$rcComment = $this->actionText;
+		if( '' != $this->comment ) {
+			if ($rcComment == '')
+				$rcComment = $this->comment;
+			else
+				$rcComment .= ': ' . $this->comment;
+		}
+		return $rcComment;
 	}
 
 	/**

phase3/includes/filerepo/FSRepo.php
@@ -3,85 +3,20 @@
 /**
  * A repository for files accessible via the local filesystem. Does not support
  * database access or registration.
- *
- * TODO: split off abstract base FileRepo
  */
 
-class FSRepo {
-	const DELETE_SOURCE = 1;
-
-	var $directory, $url, $hashLevels, $thumbScriptUrl, $transformVia404;
-	var $descBaseUrl, $scriptDirUrl, $articleUrl, $fetchDescription, $initialCapital;
+class FSRepo extends FileRepo {
+	var $directory, $url, $hashLevels;
 	var $fileFactory = array( 'UnregisteredLocalFile', 'newFromTitle' );
 	var $oldFileFactory = false;
 
 	function __construct( $info ) {
+		parent::__construct( $info );
+
 		// Required settings
-		$this->name = $info['name'];
 		$this->directory = $info['directory'];
 		$this->url = $info['url'];
 		$this->hashLevels = $info['hashLevels'];
-		$this->transformVia404 = !empty( $info['transformVia404'] );
-
-		// Optional settings
-		$this->initialCapital = true; // by default
-		foreach ( array( 'descBaseUrl', 'scriptDirUrl', 'articleUrl', 'fetchDescription', 
-			'thumbScriptUrl', 'initialCapital' ) as $var ) 
-		{
-			if ( isset( $info[$var] ) ) {
-				$this->$var = $info[$var];
-			}
-		}
-	}
-
-	/**
-	 * Create a new File object from the local repository
-	 * @param mixed $title Title object or string
-	 * @param mixed $time Time at which the image is supposed to have existed. 
-	 *                    If this is specified, the returned object will be an 
-	 *                    instance of the repository's old file class instead of
-	 *                    a current file. Repositories not supporting version 
-	 *                    control should return false if this parameter is set.
-	 */
-	function newFile( $title, $time = false ) {
-		if ( !($title instanceof Title) ) {
-			$title = Title::makeTitleSafe( NS_IMAGE, $title );
-			if ( !is_object( $title ) ) {
-				return null;
-			}
-		}
-		if ( $time ) {
-			if ( $this->oldFileFactory ) {
-				return call_user_func( $this->oldFileFactory, $title, $this, $time );
-			} else {
-				return false;
-			}
-		} else {
-			return call_user_func( $this->fileFactory, $title, $this );
-		}
-	}
-
-	/**
-	 * Find an instance of the named file that existed at the specified time
-	 * Returns false if the file did not exist. Repositories not supporting 
-	 * version control should return false if the time is specified.
-	 *
-	 * @param mixed $time 14-character timestamp, or false for the current version
-	 */
-	function findFile( $title, $time = false ) {
-		# First try the current version of the file to see if it precedes the timestamp
-		$img = $this->newFile( $title );
-		if ( !$img ) {
-			return false;
-		}
-		if ( $img->exists() && ( !$time || $img->getTimestamp() <= $time ) ) {
-			return $img;
-		}
-		# Now try an old version of the file
-		$img = $this->newFile( $title, $time );
-		if ( $img->exists() ) {
-			return $img;
-		}
 	}
 
 	/**
@@ -103,20 +38,6 @@ class FSRepo {
 	 */
 	function isHashed() {
 		return (bool)$this->hashLevels;
-	}
-
-	/**
-	 * Get the URL of thumb.php
-	 */
-	function getThumbScriptUrl() {
-		return $this->thumbScriptUrl;
-	}
-
-	/**
-	 * Returns true if the repository can transform files via a 404 handler
-	 */
-	function canTransformVia404() {
-		return $this->transformVia404;
 	}
 
 	/**
@@ -153,11 +74,13 @@ class FSRepo {
 
 	/**
 	 * Get a URL referring to this repository, with the private mwrepo protocol.
+	 * The suffix, if supplied, is considered to be unencoded, and will be 
+	 * URL-encoded before being returned.
 	 */
 	function getVirtualUrl( $suffix = false ) {
-		$path = 'mwrepo://';
+		$path = 'mwrepo://' . $this->name;
 		if ( $suffix !== false ) {
-			$path .= '/' . $suffix;
+			$path .= '/' . rawurlencode( $suffix );
 		}
 		return $path;
 	}
@@ -174,21 +97,24 @@ class FSRepo {
 		if ( count( $bits ) != 3 ) {
 			throw new MWException( __METHOD__.": invalid mwrepo URL: $url" );
 		}
-		list( $host, $zone, $rel ) = $bits;
-		if ( $host !== '' ) {
+		list( $repo, $zone, $rel ) = $bits;
+		if ( $repo !== $this->name ) {
 			throw new MWException( __METHOD__.": fetching from a foreign repo is not supported" );
 		}
 		$base = $this->getZonePath( $zone );
 		if ( !$base ) {
 			throw new MWException( __METHOD__.": invalid zone: $zone" );
 		}
-		return $base . '/' . urldecode( $rel );
+		return $base . '/' . rawurldecode( $rel );
 	}
 
 	/**
 	 * Store a file to a given destination.
 	 */
 	function store( $srcPath, $dstZone, $dstRel, $flags = 0 ) {
+		if ( !is_writable( $this->directory ) ) {
+			return new WikiErrorMsg( 'upload_directory_read_only', wfEscapeWikiText( $this->directory ) );
+		}
 		$root = $this->getZonePath( $dstZone );
 		if ( !$root ) {
 			throw new MWException( "Invalid zone: $dstZone" );
@@ -198,8 +124,8 @@ class FSRepo {
 		if ( !is_dir( dirname( $dstPath ) ) ) {
 			wfMkdirParents( dirname( $dstPath ) );
 		}
-			
-		if ( substr( $srcPath, 0, 9 ) == 'mwrepo://' ) {
+		
+		if ( self::isVirtualUrl( $srcPath ) ) {
 			$srcPath = $this->resolveVirtualUrl( $srcPath );
 		}
 
@@ -245,7 +171,7 @@ class FSRepo {
 	 * @return boolean True on success, false on failure
 	 */
 	function freeTemp( $virtualUrl ) {
-		$temp = 'mwrepo:///temp';
+		$temp = "mwrepo://{$this->name}/temp";
 		if ( substr( $virtualUrl, 0, strlen( $temp ) ) != $temp ) {
 			wfDebug( __METHOD__.": Invalid virtual URL\n" );
 			return false;
@@ -263,16 +189,28 @@ class FSRepo {
 	 * virtual URL, into this repository at the specified destination location.
 	 *
 	 * @param string $srcPath The source path or URL
-	 * @param string $dstPath The destination relative path
-	 * @param string $archivePath The relative path where the existing file is to
-	 *        be archived, if there is one.
-	 * @param integer $flags Bitfield, may be FSRepo::DELETE_SOURCE to indicate
+	 * @param string $dstRel The destination relative path
+	 * @param string $archiveRel The relative path where the existing file is to
+	 *        be archived, if there is one. Relative to the public zone root.
+	 * @param integer $flags Bitfield, may be FileRepo::DELETE_SOURCE to indicate
 	 *        that the source file should be deleted if possible
 	 */
-	function publish( $srcPath, $dstPath, $archivePath, $flags = 0 ) {
+	function publish( $srcPath, $dstRel, $archiveRel, $flags = 0 ) {
+		if ( !is_writable( $this->directory ) ) {
+			return new WikiErrorMsg( 'upload_directory_read_only', wfEscapeWikiText( $this->directory ) );
+		}
 		if ( substr( $srcPath, 0, 9 ) == 'mwrepo://' ) {
 			$srcPath = $this->resolveVirtualUrl( $srcPath );
 		}
+		if ( !$this->validateFilename( $dstRel ) ) {
+			throw new MWException( 'Validation error in $dstRel' );
+		}
+		if ( !$this->validateFilename( $archiveRel ) ) {
+			throw new MWException( 'Validation error in $archiveRel' );
+		}
+		$dstPath = "{$this->directory}/$dstRel";
+		$archivePath = "{$this->directory}/$archiveRel";
+		
 		$dstDir = dirname( $dstPath );
 		if ( !is_dir( $dstDir ) ) wfMkdirParents( $dstDir );
 
@@ -324,81 +262,7 @@ class FSRepo {
 	 * If the repo is not hashed, returns an empty string
 	 */
 	function getHashPath( $name ) {
-		if ( $this->isHashed() ) {
-			$hash = md5( $name );
-			$path = '';
-			for ( $i = 1; $i <= $this->hashLevels; $i++ ) {
-				$path .= substr( $hash, 0, $i ) . '/';
-			}
-			return $path;
-		} else {
-			return '';
-		}
-	}
-
-	/**
-	 * Get the name of this repository, as specified by $info['name]' to the constructor
-	 */
-	function getName() {
-		return $this->name;
-	}
-
-	/**
-	 * Get the file description page base URL, or false if there isn't one.
-	 * @private
-	 */
-	function getDescBaseUrl() {
-		if ( is_null( $this->descBaseUrl ) ) {
-			if ( !is_null( $this->articleUrl ) ) {
-				$this->descBaseUrl = str_replace( '$1', 
-					urlencode( Namespace::getCanonicalName( NS_IMAGE ) ) . ':', $this->articleUrl );
-			} elseif ( !is_null( $this->scriptDirUrl ) ) {
-				$this->descBaseUrl = $this->scriptDirUrl . '/index.php?title=' . 
-					urlencode( Namespace::getCanonicalName( NS_IMAGE ) ) . ':';
-			} else {
-				$this->descBaseUrl = false;
-			}
-		}
-		return $this->descBaseUrl;
-	}
-
-	/**
-	 * Get the URL of an image description page. May return false if it is
-	 * unknown or not applicable. In general this should only be called by the 
-	 * File class, since it may return invalid results for certain kinds of 
-	 * repositories. Use File::getDescriptionUrl() in user code.
-	 *
-	 * In particular, it uses the article paths as specified to the repository
-	 * constructor, whereas local repositories use the local Title functions.
-	 */
-	function getDescriptionUrl( $name ) {
-		$base = $this->getDescBaseUrl();
-		if ( $base ) {
-			return $base . wfUrlencode( $name );
-		} else {
-			return false;
-		}
-	}
-
-	/**
-	 * Get the URL of the content-only fragment of the description page. For 
-	 * MediaWiki this means action=render. This should only be called by the 
-	 * repository's file class, since it may return invalid results. User code 
-	 * should use File::getDescriptionText().
-	 */
-	function getDescriptionRenderUrl( $name ) {
-		if ( isset( $this->scriptDirUrl ) ) {
-			return $this->scriptDirUrl . '/index.php?title=' . 
-				wfUrlencode( Namespace::getCanonicalName( NS_IMAGE ) . ':' . $name ) .
-				'&action=render';
-		} else {
-			$descBase = $this->getDescBaseUrl();
-			if ( $descBase ) {
-				return wfAppendQuery( $descBase . wfUrlencode( $name ), 'action=render' );
-			} else {
-				return false;
-			}
-		}
+		return FileRepo::getHashPathForLevel( $name, $this->hashLevels );
 	}
 
 	/**
@@ -424,7 +288,7 @@ class FSRepo {
 	}
 
 	/**
-	 * Call a callaback function for every file in the repository
+	 * Call a callback function for every file in the repository
 	 * May use either the database or the filesystem
 	 */
 	function enumFiles( $callback ) {
@@ -432,20 +296,12 @@ class FSRepo {
 	}
 
 	/**
-	 * Get the name of an image from its title object
+	 * Get properties of a file with a given virtual URL
+	 * The virtual URL must refer to this repo
 	 */
-	function getNameFromTitle( $title ) {
-		global $wgCapitalLinks;
-		if ( $this->initialCapital != $wgCapitalLinks ) {
-			global $wgContLang;
-			$name = $title->getUserCaseDBKey();
-			if ( $this->initialCapital ) {
-				$name = $wgContLang->ucfirst( $name );
-			}
-		} else {
-			$name = $title->getDBkey();
-		}
-		return $name;
+	function getFileProps( $virtualUrl ) {
+		$path = $this->resolveVirtualUrl( $virtualUrl );
+		return File::getPropsFromPath( $path );
 	}
 }
 

phase3/includes/filerepo/File.php
@@ -503,7 +503,7 @@ class File {
 				break;
 			}
 
-			wfDebug( "Doing stat for $thumbPath\n" );
+			wfDebug( __METHOD__.": Doing stat for $thumbPath\n" );
 			$this->migrateThumbFile( $thumbName );
 			if ( file_exists( $thumbPath ) ) {
 				$thumb = $this->handler->getTransform( $this, $thumbPath, $thumbUrl, $params );
@@ -663,7 +663,7 @@ class File {
 	 * Get urlencoded relative path of the file
 	 */
 	function getUrlRel() {
-		return $this->getHashPath() . urlencode( $this->getName() );
+		return $this->getHashPath() . rawurlencode( $this->getName() );
 	}
 
 	/** Get the path of the archive directory, or a particular file if $suffix is specified */
@@ -692,7 +692,7 @@ class File {
 		if ( $suffix === false ) {
 			$path = substr( $path, 0, -1 );
 		} else {
-			$path .= urlencode( $suffix );
+			$path .= rawurlencode( $suffix );
 		}
 		return $path;
 	}
@@ -701,7 +701,7 @@ class File {
 	function getThumbUrl( $suffix = false ) {
 		$path = $this->repo->getZoneUrl('public') . '/thumb/' . $this->getUrlRel();
 		if ( $suffix !== false ) {
-			$path .= '/' . urlencode( $suffix );
+			$path .= '/' . rawurlencode( $suffix );
 		}
 		return $path;
 	}
@@ -712,7 +712,7 @@ class File {
 		if ( $suffix === false ) {
 			$path = substr( $path, 0, -1 );
 		} else {
-			$path .= urlencode( $suffix );
+			$path .= rawurlencode( $suffix );
 		}
 		return $path;
 	}
@@ -721,10 +721,19 @@ class File {
 	function getThumbVirtualUrl( $suffix = false ) {
 		$path = $this->repo->getVirtualUrl() . '/public/thumb/' . $this->getUrlRel();
 		if ( $suffix !== false ) {
-			$path .= '/' . urlencode( $suffix );
+			$path .= '/' . rawurlencode( $suffix );
 		}
 		return $path;
 	}
+
+	/** Get the virtual URL for the file itself */
+	function getVirtualUrl( $suffix = false ) {
+		$path = $this->repo->getVirtualUrl() . '/public/' . $this->getUrlRel();
+		if ( $suffix !== false ) {
+			$path .= '/' . rawurlencode( $suffix );
+		}
+		return $path;
+	}	
 
 	/**
 	 * @return bool
@@ -989,6 +998,59 @@ class File {
 	 */
 	function userCan( $field ) {
 		return true;
+	}
+
+	/**
+	 * Get an associative array containing information about a file in the local filesystem
+	 */
+	static function getPropsFromPath( $path ) {
+		wfProfileIn( __METHOD__ );
+		wfDebug( __METHOD__.": Getting file info for $path\n" );
+		$info = array( 'fileExists' => file_exists( $path ) );
+		$gis = false;
+		if ( $info['fileExists'] ) {
+			$magic = MimeMagic::singleton();
+
+			$info['mime'] = $magic->guessMimeType( $path, true );
+			list( $info['major_mime'], $info['minor_mime'] ) = self::splitMime( $info['mime'] );
+			$info['media_type'] = $magic->getMediaType( $path, $info['mime'] );
+
+			# Get size in bytes
+			$info['size'] = filesize( $path );
+
+			# Height, width and metadata
+			$handler = MediaHandler::getHandler( $info['mime'] );
+			if ( $handler ) {
+				$tempImage = (object)array();
+				$gis = $handler->getImageSize( $tempImage, $path );
+				$info['metadata'] = $handler->getMetadata( $tempImage, $path );
+			} else {
+				$gis = false;
+				$info['metadata'] = '';
+			}
+
+			wfDebug(__METHOD__.": $path loaded, {$info['size']} bytes, {$info['mime']}.\n");
+		} else {
+			$info['mime'] = NULL;
+			$info['media_type'] = MEDIATYPE_UNKNOWN;
+			$info['metadata'] = '';
+			wfDebug(__METHOD__.": $path NOT FOUND!\n");
+		}
+		if( $gis ) {
+			# NOTE: $gis[2] contains a code for the image type. This is no longer used.
+			$info['width'] = $gis[0];
+			$info['height'] = $gis[1];
+			if ( isset( $gis['bits'] ) ) {
+				$info['bits'] = $gis['bits'];
+			} else {
+				$info['bits'] = 0;
+			}
+		} else {
+			$info['width'] = 0;
+			$info['height'] = 0;
+		}
+		wfProfileOut( __METHOD__ );
+		return $info;
 	}
 }
 

commit 0a61ca16fa1fa396c7a6c5c4bc9a83cf2ed8f70c

Fix regression introduced in r23023; use the right function name when calling to load file properties :)
git-svn-id: svn+ssh://svn.wikimedia.org/svnroot/mediawiki/trunk@23099 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

phase3/includes/filerepo/LocalFile.php
@@ -150,7 +150,7 @@ class LocalFile extends File
 	 * Load metadata from the file itself
 	 */
 	function loadFromFile() {
-		$this->setProps( self::getInfoFromPath( $this->getPath() ) );
+		$this->setProps( self::getPropsFromPath( $this->getPath() ) );
 	}
 
 	function getCacheFields( $prefix = 'img_' ) {