cmelbye/mediawiki-svn

commit abf3e93137bb65f3508325679974f1913402ab33

* Fixed call to deprecated functions * Remove unused global declaration of $wgArticle in SkinTemplate::buildNavUrls()
git-svn-id: svn+ssh://svn.wikimedia.org/svnroot/mediawiki/trunk/phase3/includes@32372 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

SpecialUpload.php
@@ -166,7 +166,7 @@ class UploadForm {
 		$url =  trim( $url );
 		if( stripos($url, 'http://') !== 0 && stripos($url, 'ftp://') !== 0 ) {
 			# Only HTTP or FTP URLs
-			$wgOut->errorPage( 'upload-proto-error', 'upload-proto-error-text' );
+			$wgOut->showErrorPage( 'upload-proto-error', 'upload-proto-error-text' );
 			return true;
 		}
 
@@ -174,7 +174,7 @@ class UploadForm {
 		$this->mCurlDestHandle = @fopen( $this->mTempPath, "wb" );
 		if( $this->mCurlDestHandle === false ) {
 			# Could not open temporary file to write in
-			$wgOut->errorPage( 'upload-file-error', 'upload-file-error-text');
+			$wgOut->showErrorPage( 'upload-file-error', 'upload-file-error-text');
 			return true;
 		}
 
@@ -195,9 +195,9 @@ class UploadForm {
 		if( $error ) {
 			unlink( $dest );
 			if( wfEmptyMsg( "upload-curl-error$errornum", wfMsg("upload-curl-error$errornum") ) )
-				$wgOut->errorPage( 'upload-misc-error', 'upload-misc-error-text' );
+				$wgOut->showErrorPage( 'upload-misc-error', 'upload-misc-error-text' );
 			else
-				$wgOut->errorPage( "upload-curl-error$errornum", "upload-curl-error$errornum-text" );
+				$wgOut->showErrorPage( "upload-curl-error$errornum", "upload-curl-error$errornum-text" );
 		}
 
 		return $error;
@@ -376,9 +376,9 @@ class UploadForm {
 			return self::BEFORE_PROCESSING;
 		}
 
-		/* Check for PHP error if any, requires php 4.2 or newer */
-		if( $this->mCurlError == 1/*UPLOAD_ERR_INI_SIZE*/ ) {
-			return self::LARGE_FILE_SERVER;
+		/* Check for curl error */
+		if( $this->mCurlError ) {
+			return self::BEFORE_PROCESSING;
 		}
 
 		/**

Article.php
@@ -1518,7 +1518,7 @@ class Article {
 		# Check patrol config options
 
 		if ( !($wgUseNPPatrol || $wgUseRCPatrol)) {
-			$wgOut->errorPage( 'rcpatroldisabled', 'rcpatroldisabledtext' );
+			$wgOut->showErrorPage( 'rcpatroldisabled', 'rcpatroldisabledtext' );
 			return;		
 		}
 
@@ -1527,7 +1527,7 @@ class Article {
 		$rc = $rcid ? RecentChange::newFromId($rcid) : null;
 		if ( is_null ( $rc ) )
 		{
-			$wgOut->errorPage( 'markedaspatrollederror', 'markedaspatrollederrortext' );
+			$wgOut->showErrorPage( 'markedaspatrollederror', 'markedaspatrollederrortext' );
 			return;
 		}
 
@@ -1535,7 +1535,7 @@ class Article {
 			// Only new pages can be patrolled if the general patrolling is off....???
 			// @fixme -- is this necessary? Shouldn't we only bother controlling the
 			// front end here?
-			$wgOut->errorPage( 'rcpatroldisabled', 'rcpatroldisabledtext' );
+			$wgOut->showErrorPage( 'rcpatroldisabled', 'rcpatroldisabledtext' );
 			return;
 		}
 		

EditPage.php
@@ -687,7 +687,7 @@ class EditPage {
 			if( $title instanceof Title && $title->exists() && $title->userCanRead() ) {
 				global $wgOut;
 				$revision = Revision::newFromTitle( $title );
-				$wgOut->addSecondaryWikiText( $revision->getText() );
+				$wgOut->addWikiTextTitleTidy( $revision->getText(), $this->mTitle );
 				return true;
 			} else {
 				return false;

ImagePage.php
@@ -38,7 +38,7 @@ class ImagePage extends Article {
 	function render() {
 		global $wgOut;
 		$wgOut->setArticleBodyOnly( true );
-		$wgOut->addSecondaryWikitext( $this->getContent() );
+		$wgOut->addWikiTextTitleTidy( $this->getContent(), $this->mTitle );
 	}
 
 	function view() {

SkinTemplate.php
@@ -397,7 +397,7 @@ class SkinTemplate extends Skin {
 		$tpl->set( 'about', $this->aboutLink() );
 
 		$tpl->setRef( 'debug', $out->mDebugtext );
-		$tpl->set( 'reporttime', $out->reportTime() );
+		$tpl->set( 'reporttime', wfReportTime() );
 		$tpl->set( 'sitenotice', wfGetSiteNotice() );
 		$tpl->set( 'bottomscripts', $this->bottomScripts() );
 
@@ -838,13 +838,11 @@ class SkinTemplate extends Skin {
 	 * @private
 	 */
 	function buildNavUrls () {
-		global $wgUseTrackbacks, $wgTitle, $wgArticle;
+		global $wgUseTrackbacks, $wgTitle, $wgUser, $wgRequest;
+		global $wgEnableUploads, $wgUploadNavigationUrl;
 
 		$fname = 'SkinTemplate::buildNavUrls';
 		wfProfileIn( $fname );
-
-		global $wgUser, $wgRequest;
-		global $wgEnableUploads, $wgUploadNavigationUrl;
 
 		$action = $wgRequest->getText( 'action' );
 

SpecialLockdb.php
@@ -104,7 +104,7 @@ END
 			# This used to show a file not found error, but the likeliest reason for fopen()
 			# to fail at this point is insufficient permission to write to the file...good old
 			# is_writable() is plain wrong in some cases, it seems...
-			$this->notWritable();
+			self::notWritable();
 			return;
 		}
 		fwrite( $fp, $this->reason );
@@ -126,7 +126,7 @@ END
 	
 	public static function notWritable() {
 		global $wgOut;
-		$wgOut->errorPage( 'lockdb', 'lockfilenotwritable' );
+		$wgOut->showErrorPage( 'lockdb', 'lockfilenotwritable' );
 	}
 	
 }

SpecialPreferences.php
@@ -356,7 +356,7 @@ class PreferencesForm {
 			return;
 		}
 
-		$wgOut->setParserOptions( ParserOptions::newFromUser( $wgUser ) );
+		$wgOut->parserOptions( ParserOptions::newFromUser( $wgUser ) );
 		$this->mainPrefsForm( $error === false ? 'success' : 'error', $error);
 	}
 

SpecialRecentchangeslinked.php
@@ -25,12 +25,12 @@ function wfSpecialRecentchangeslinked( $par = NULL ) {
 	$sk = $wgUser->getSkin();
 
 	if (is_null($target)) {
-		$wgOut->errorpage( 'notargettitle', 'notargettext' );
+		$wgOut->showErrorPage( 'notargettitle', 'notargettext' );
 		return;
 	}
 	$nt = Title::newFromURL( $target );
 	if( !$nt ) {
-		$wgOut->errorpage( 'notargettitle', 'notargettext' );
+		$wgOut->showErrorPage( 'notargettitle', 'notargettext' );
 		return;
 	}
 	$id = $nt->getArticleId();

commit 0f3b81533fc7ec768b5fc4a45aaa6cc436494c18

(bug 13556, and possible regression from r32372) Don't show a blank form if no image is attached in Special:Upload
git-svn-id: svn+ssh://svn.wikimedia.org/svnroot/mediawiki/trunk/phase3/includes@32569 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

SpecialUpload.php
@@ -280,7 +280,7 @@ class UploadForm {
 	 	$details = null;
 	 	$value = null;
 	 	$value = $this->internalProcessUpload( $details );
-		
+
 	 	switch($value) {
 			case self::SUCCESS:
 				$wgOut->redirect( $this->mLocalFile->getTitle()->getFullURL() );
@@ -376,16 +376,16 @@ class UploadForm {
 			return self::BEFORE_PROCESSING;
 		}
 
-		/* Check for curl error */
-		if( $this->mCurlError ) {
-			return self::BEFORE_PROCESSING;
-		}
-
 		/**
 		 * If there was no filename or a zero size given, give up quick.
 		 */
 		if( trim( $this->mSrcName ) == '' || empty( $this->mFileSize ) ) {
 			return self::EMPTY_FILE;
+		}
+
+		/* Check for curl error */
+		if( $this->mCurlError ) {
+			return self::BEFORE_PROCESSING;
 		}
 
 		# Chop off any directories in the given filename
@@ -998,7 +998,7 @@ wgUploadAutoFill = {$autofill};
 				$val2 = $val;
 		}
 		$val2 = $wgAllowCopyUploads ? min( $wgMaxUploadSize, $val2 ) : $val2;
-		$maxUploadSize = wfMsgExt( 'upload-maxfilesize', array( 'parseinline', 'escape' ), $wgLang->formatSize( $val2 ) );
+		$maxUploadSize = wfMsgExt( 'upload-maxfilesize', array( 'parseinline', 'escapenoentities' ), $wgLang->formatSize( $val2 ) );
 
 		$sourcefilename = wfMsgExt( 'sourcefilename', 'escapenoentities' );
 		$destfilename = wfMsgExt( 'destfilename', 'escapenoentities' );