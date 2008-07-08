cmelbye/mediawiki-svn

commit 38883a7e03a398c376bf497e0914459ba1b44fb0

Changing capitalisation and CamelCase into proper text in extensions A-E. * In Contributors extension, added PLURAL * In DeleteBatch extension, removing capitals intended to make a button "irritatably large"; that should be done with CSS * In EmailArticle extension, changed wording of two messages
git-svn-id: svn+ssh://svn.wikimedia.org/svnroot/mediawiki/trunk@37310 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

extensions/Contributors/Contributors.i18n.php
@@ -19,8 +19,8 @@ $messages['en'] = array(
 	'contributors-target'      => 'Title:',
 	'contributors-submit'      => 'Show',
 	'contributors-subtitle'    => 'Main contributors to $1',
-	'contributors-others-long' => 'There are $1 other contributors to this page.',
-	'contributors-others'      => 'and $1 others',
+	'contributors-others-long' => 'There are {{PLURAL:$1|one other contributor|$1 other contributors}} to this page.',
+	'contributors-others'      => 'and {{PLURAL:$1|one other|$1 others}}',
 	'contributors-badtitle'    => 'Invalid title.',
 	'contributors-nosuchpage'  => 'The page $1 does not exist.',
 );

extensions/APC/ViewAPC.i18n.php
@@ -5,7 +5,7 @@ $messages = array();
 $messages['en'] = array(
 	'viewapc'      => 'APC information',
 	'viewapc-desc' => '[[Special:ViewAPC|View and manage APC cache]] with MediaWiki',
-	'right-apc'    => 'Use all features in Special:ViewAPC',
+	'right-apc'    => 'Use all features in [[Special:ViewAPC]]',
 
 	'viewapc-apc-not-available' => 'No cache info available.
 APC does not appear to be running.',
@@ -45,9 +45,9 @@ APC does not appear to be running.',
 	'viewapc-info-runtime' => 'Runtime information',
 
 	'viewapc-info-memory'           => 'Host status diagrams',
-	'viewapc-memory-usage-detailed' => 'Memory Usage<br />
+	'viewapc-memory-usage-detailed' => 'Memory usage<br />
 (multiple slices indicate fragments)',
-	'viewapc-memory-usage'          => 'Memory Usage',
+	'viewapc-memory-usage'          => 'Memory usage',
 	'viewapc-cache-efficiency'      => 'Hits and misses',
 
 	'viewapc-memory-free' => 'Free: $1 ($2)',

extensions/AbuseFilter/AbuseFilter.i18n.php
@@ -15,8 +15,8 @@ $messages['en'] = array(
 	'abusefilter-desc' => 'Applies automatic heuristics to edits.',
 
 	// Special pages
-	'abusefilter' => 'Abuse Filter Configuration',
-	'abuselog' => 'Abuse Log',
+	'abusefilter' => 'Abuse filter configuration',
+	'abuselog' => 'Abuse log',
 	
 	// Hooks
 	'abusefilter-warning' => "<big>'''Warning'''</big>: This action has been automatically identified as harmful.
@@ -40,7 +40,7 @@ A brief description of the abuse rule which your action matched is: $1",
 	'abusefilter-autopromote-blocked' => "This action has been automatically identified as harmful, and it has been disallowed.
 In addition, as a security measure, some privileges routinely granted to established accounts have been temporarily revoked from your account.
 A brief description of the abuse rule which your action matched is: $1",
-	'abusefilter-blocker' => 'Abuse Filter',
+	'abusefilter-blocker' => 'Abuse filter',
 	'abusefilter-blockreason' => 'Automatically blocked by abuse filter. Rule description: $1',
 	
 	'abusefilter-accountreserved' => 'This account name is reserved for use by the abuse filter.',
@@ -53,7 +53,7 @@ A brief description of the abuse rule which your action matched is: $1",
 	'right-abusefilter-private' => 'View private data in the abuse log',
 	
 	// Abuse Log
-	'abusefilter-log' => 'Abuse Filter Log',
+	'abusefilter-log' => 'Abuse filter log',
 	'abusefilter-log-search' => 'Search the abuse log',
 	'abusefilter-log-search-user' => 'User:',
 	'abusefilter-log-search-filter' => 'Filter ID:',
@@ -71,7 +71,7 @@ A brief description of the abuse rule which your action matched is: $1",
 	'abusefilter-log-noactions' => 'none',
 	
 	// Abuse filter management
-	'abusefilter-management' => 'Abuse Filter Management',
+	'abusefilter-management' => 'Abuse filter management',
 	'abusefilter-list' => 'All filters',
 	'abusefilter-list-id' => 'Filter ID',
 	'abusefilter-list-status' => 'Status',
@@ -91,7 +91,7 @@ A brief description of the abuse rule which your action matched is: $1",
 	// The edit screen
 	'abusefilter-edit-subtitle' => 'Editing filter $1',
 	'abusefilter-edit-new' => 'New filter',
-	'abusefilter-edit-save' => 'Save Filter',
+	'abusefilter-edit-save' => 'Save filter',
 	'abusefilter-edit-id' => 'Filter ID:',
 	'abusefilter-edit-description' => "Description:\n:''(publicly viewable)''",
 	'abusefilter-edit-flags' => 'Flags:',

extensions/AjaxQueryPages/AjaxQueryPages.i18n.php
@@ -8,7 +8,7 @@
 $messages = array();
 
 $messages['en'] = array(
-	'ajax-qp-desc' => 'Add some AJAX to QueryPages such as [[Special:Shortpages]]',
+	'ajax-qp-desc' => 'Add some AJAX to query pages such as [[Special:Shortpages]]',
 );
 
 /** Arabic (العربية)

extensions/AjaxShowEditors/AjaxShowEditors.i18n.php
@@ -13,8 +13,8 @@ $messages['en'] = array(
 	'ajax-se-pending'          => 'pending refresh… (click this box or start editing)',
 	'ajax-se-idling'           => '($1s ago)',
 	'ajax-se-pagedoesnotexist' => 'page does not exist',
-	'ajax-se-userinvalid'      => 'ERR: user invalid',
-	'ajax-se-usernotfound'     => 'ERR: user not found',
+	'ajax-se-userinvalid'      => 'Error: user invalid',
+	'ajax-se-usernotfound'     => 'Error: user not found',
 );
 
 /** Afrikaans (Afrikaans)

extensions/Babel/Babel.i18n.php
@@ -12,7 +12,7 @@ $messages = array();
  * @author Siebrand
  */
 $messages['en'] = array(
-	'babel'      => 'Babel User Information',
+	'babel'      => 'Babel user information',
 	'babel-desc' => 'Adds the <tt>#babel</tt> parser function to allow automated generation of a babel userbox column with the ability to include custom templates',
 	'babel-url'  => 'Project:Babel',
 

extensions/Blahtex/Blahtex.i18n.php
@@ -8,7 +8,7 @@
 $messages = array();
 
 $messages['en'] = array(
-	'math_noblahtex'                        => 'Can\'t execute blahtex, which should be at $1',
+	'math_noblahtex'                        => 'Cannot execute blahtex, which should be at $1',
 	'math-desc'				=> 'MathML output for &lt;math&gt; tags',
 	'math_AmbiguousInfix'                   => 'Ambiguous placement of "$1".
 Try using additional braces "{ ... }" to disambiguate.',

extensions/BlockTitles/BlockTitles.i18n.php
@@ -12,7 +12,7 @@ $messages = array();
  * @author Travis Derouin
  */
 $messages['en'] = array(
-	'block_title_error_page_title' => 'Blocked Title',
+	'block_title_error_page_title' => 'Blocked title',
 	'block_title_error-desc'       => 'Provides a basic way of preventing pages with certain titles from being saved or created',
 	'block_title_error'            => 'Sorry, the title of this page is not allowed to be saved.',
 );

commit 7e7034897f87817aee5091eca361e10a93f51df8

Fixing grammatical error introduced in r37310
git-svn-id: svn+ssh://svn.wikimedia.org/svnroot/mediawiki/trunk@37312 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

extensions/Contributors/Contributors.i18n.php
@@ -19,7 +19,7 @@ $messages['en'] = array(
 	'contributors-target'      => 'Title:',
 	'contributors-submit'      => 'Show',
 	'contributors-subtitle'    => 'Main contributors to $1',
-	'contributors-others-long' => 'There are {{PLURAL:$1|one other contributor|$1 other contributors}} to this page.',
+	'contributors-others-long' => 'There {{PLURAL:$1|is one other contributor|are $1 other contributors}} to this page.',
 	'contributors-others'      => 'and {{PLURAL:$1|one other|$1 others}}',
 	'contributors-badtitle'    => 'Invalid title.',
 	'contributors-nosuchpage'  => 'The page $1 does not exist.',