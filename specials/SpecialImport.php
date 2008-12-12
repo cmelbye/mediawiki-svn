cmelbye/mediawiki-svn

commit 3e0c712a62608f71fc9e54702d9db1054098a5a6

Refactor Special:Import to extend SpecialPage instead of wfSpecialImport() stuff.
git-svn-id: svn+ssh://svn.wikimedia.org/svnroot/mediawiki/trunk/phase3/includes@43963 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

specials/SpecialImport.php
@@ -23,26 +23,50 @@
  * @ingroup SpecialPage
  */
 
-/**
- * Constructor
- */
-function wfSpecialImport( $page = '' ) {
-	global $wgUser, $wgOut, $wgRequest, $wgTitle, $wgImportSources;
-	global $wgImportTargetNamespace;
-
-	$interwiki = false;
-	$namespace = $wgImportTargetNamespace;
-	$frompage = '';
-	$history = true;
-
-	if ( wfReadOnly() ) {
-		$wgOut->readOnlyPage();
-		return;
+class SpecialImport extends SpecialPage {
+	
+	private $interwiki = false;
+	private $namespace;
+	private $frompage = '';
+	private $history = true;
+	
+	/**
+	 * Constructor
+	 */
+	public function __construct() {
+		parent::__construct( 'Import', 'import' );
+		global $wgImportTargetNamespace;
+		$this->namespace = $wgImportTargetNamespace;
 	}
-
-	if( $wgRequest->wasPosted() && $wgRequest->getVal( 'action' ) == 'submit') {
+	
+	/**
+	 * Execute
+	 */
+	function execute( $par ) {
+		global $wgRequest;
+		
+		$this->setHeaders();
+		$this->outputHeader();
+		
+		if ( wfReadOnly() ) {
+			global $wgOut;
+			$wgOut->readOnlyPage();
+			return;
+		}
+		
+		if ( $wgRequest->wasPosted() && $wgRequest->getVal( 'action' ) == 'submit' ) {
+			$this->doImport();
+		}
+		$this->showForm();
+	}
+	
+	/**
+	 * Do the actual import
+	 */
+	private function doImport() {
+		global $wgOut, $wgRequest, $wgUser, $wgImportSources;
 		$isUpload = false;
-		$namespace = $wgRequest->getIntOrNull( 'namespace' );
+		$this->namespace = $wgRequest->getIntOrNull( 'namespace' );
 		$sourceName = $wgRequest->getVal( "source" );
 
 		if ( !$wgUser->matchEditToken( $wgRequest->getVal( 'editToken' ) ) ) {
@@ -55,16 +79,16 @@ function wfSpecialImport( $page = '' ) {
 				return $wgOut->permissionRequired( 'importupload' );
 			}
 		} elseif ( $sourceName == "interwiki" ) {
-			$interwiki = $wgRequest->getVal( 'interwiki' );
-			if ( !in_array( $interwiki, $wgImportSources ) ) {
+			$this->interwiki = $wgRequest->getVal( 'interwiki' );
+			if ( !in_array( $this->interwiki, $wgImportSources ) ) {
 				$source = new WikiErrorMsg( "import-invalid-interwiki" );
 			} else {
-				$history = $wgRequest->getCheck( 'interwikiHistory' );
-				$frompage = $wgRequest->getText( "frompage" );
+				$this->history = $wgRequest->getCheck( 'interwikiHistory' );
+				$this->frompage = $wgRequest->getText( "frompage" );
 				$source = ImportStreamSource::newFromInterwiki(
-					$interwiki,
-					$frompage,
-					$history );
+					$this->interwiki,
+					$this->frompage,
+					$this->history );
 			}
 		} else {
 			$source = new WikiErrorMsg( "importunknownsource" );
@@ -76,10 +100,10 @@ function wfSpecialImport( $page = '' ) {
 			$wgOut->addWikiMsg( "importstart" );
 
 			$importer = new WikiImporter( $source );
-			if( !is_null( $namespace ) ) {
-				$importer->setTargetNamespace( $namespace );
+			if( !is_null( $this->namespace ) ) {
+				$importer->setTargetNamespace( $this->namespace );
 			}
-			$reporter = new ImportReporter( $importer, $isUpload, $interwiki );
+			$reporter = new ImportReporter( $importer, $isUpload, $this->interwiki );
 
 			$reporter->open();
 			$result = $importer->doImport();
@@ -99,79 +123,82 @@ function wfSpecialImport( $page = '' ) {
 		}
 	}
 
-	$action = $wgTitle->getLocalUrl( 'action=submit' );
+	private function showForm() {
+		global $wgUser, $wgOut, $wgRequest, $wgTitle, $wgImportSources;
+		$action = $wgTitle->getLocalUrl( 'action=submit' );
 
-	if( $wgUser->isAllowed( 'importupload' ) ) {
-		$wgOut->addWikiMsg( "importtext" );
-		$wgOut->addHTML(
-			Xml::openElement( 'fieldset' ).
-			Xml::element( 'legend', null, wfMsg( 'import-upload' ) ) .
-			Xml::openElement( 'form', array( 'enctype' => 'multipart/form-data', 'method' => 'post', 'action' => $action ) ) .
-			Xml::hidden( 'action', 'submit' ) .
-			Xml::hidden( 'source', 'upload' ) .
-			Xml::input( 'xmlimport', 50, '', array( 'type' => 'file' ) ) . ' ' .
-			Xml::hidden( 'editToken', $wgUser->editToken() ) .
-			Xml::submitButton( wfMsg( 'uploadbtn' ) ) .
-			Xml::closeElement( 'form' ) .
-			Xml::closeElement( 'fieldset' )
-		);
-	} else {
-		if( empty( $wgImportSources ) ) {
-			$wgOut->addWikiMsg( 'importnosources' );
+		if( $wgUser->isAllowed( 'importupload' ) ) {
+			$wgOut->addWikiMsg( "importtext" );
+			$wgOut->addHTML(
+				Xml::openElement( 'fieldset' ).
+				Xml::element( 'legend', null, wfMsg( 'import-upload' ) ) .
+				Xml::openElement( 'form', array( 'enctype' => 'multipart/form-data', 'method' => 'post', 'action' => $action ) ) .
+				Xml::hidden( 'action', 'submit' ) .
+				Xml::hidden( 'source', 'upload' ) .
+				Xml::input( 'xmlimport', 50, '', array( 'type' => 'file' ) ) . ' ' .
+				Xml::hidden( 'editToken', $wgUser->editToken() ) .
+				Xml::submitButton( wfMsg( 'uploadbtn' ) ) .
+				Xml::closeElement( 'form' ) .
+				Xml::closeElement( 'fieldset' )
+			);
+		} else {
+			if( empty( $wgImportSources ) ) {
+				$wgOut->addWikiMsg( 'importnosources' );
+			}
 		}
-	}
-
-	if( !empty( $wgImportSources ) ) {
-		$wgOut->addHTML(
-			Xml::openElement( 'fieldset' ) .
-			Xml::element( 'legend', null, wfMsg( 'importinterwiki' ) ) .
-			Xml::openElement( 'form', array( 'method' => 'post', 'action' => $action ) ) .
-			wfMsgExt( 'import-interwiki-text', array( 'parse' ) ) .
-			Xml::hidden( 'action', 'submit' ) .
-			Xml::hidden( 'source', 'interwiki' ) .
-			Xml::hidden( 'editToken', $wgUser->editToken() ) .
-			Xml::openElement( 'table', array( 'id' => 'mw-import-table' ) ) .
-			"<tr>
-				<td>" .
-					Xml::openElement( 'select', array( 'name' => 'interwiki' ) )
-		);
-		foreach( $wgImportSources as $prefix ) {
-			$selected = ( $interwiki === $prefix ) ? ' selected="selected"' : '';
-			$wgOut->addHTML( Xml::option( $prefix, $prefix, $selected ) );
+	
+		if( !empty( $wgImportSources ) ) {
+			$wgOut->addHTML(
+				Xml::openElement( 'fieldset' ) .
+				Xml::element( 'legend', null, wfMsg( 'importinterwiki' ) ) .
+				Xml::openElement( 'form', array( 'method' => 'post', 'action' => $action ) ) .
+				wfMsgExt( 'import-interwiki-text', array( 'parse' ) ) .
+				Xml::hidden( 'action', 'submit' ) .
+				Xml::hidden( 'source', 'interwiki' ) .
+				Xml::hidden( 'editToken', $wgUser->editToken() ) .
+				Xml::openElement( 'table', array( 'id' => 'mw-import-table' ) ) .
+				"<tr>
+					<td>" .
+						Xml::openElement( 'select', array( 'name' => 'interwiki' ) )
+			);
+			foreach( $wgImportSources as $prefix ) {
+				$selected = ( $this->interwiki === $prefix ) ? ' selected="selected"' : '';
+				$wgOut->addHTML( Xml::option( $prefix, $prefix, $selected ) );
+			}
+			$wgOut->addHTML(
+						Xml::closeElement( 'select' ) .
+					"</td>
+					<td>" .
+						Xml::input( 'frompage', 50, $this->frompage ) .
+					"</td>
+				</tr>
+				<tr>
+					<td>
+					</td>
+					<td>" .
+						Xml::checkLabel( wfMsg( 'import-interwiki-history' ), 'interwikiHistory', 'interwikiHistory', $this->history ) .
+					"</td>
+				</tr>
+				<tr>
+					<td>
+					</td>
+					<td>" .
+						Xml::label( wfMsg( 'import-interwiki-namespace' ), 'namespace' ) .
+						Xml::namespaceSelector( $this->namespace, '' ) .
+					"</td>
+				</tr>
+				<tr>
+					<td>
+					</td>
+					<td>" .
+						Xml::submitButton( wfMsg( 'import-interwiki-submit' ), array( 'accesskey' => 's' ) ) .
+					"</td>
+				</tr>" .
+				Xml::closeElement( 'table' ).
+				Xml::closeElement( 'form' ) .
+				Xml::closeElement( 'fieldset' )
+			);
 		}
-		$wgOut->addHTML(
-					Xml::closeElement( 'select' ) .
-				"</td>
-				<td>" .
-					Xml::input( 'frompage', 50, $frompage ) .
-				"</td>
-			</tr>
-			<tr>
-				<td>
-				</td>
-				<td>" .
-					Xml::checkLabel( wfMsg( 'import-interwiki-history' ), 'interwikiHistory', 'interwikiHistory', $history ) .
-				"</td>
-			</tr>
-			<tr>
-				<td>
-				</td>
-				<td>" .
-					Xml::label( wfMsg( 'import-interwiki-namespace' ), 'namespace' ) .
-					Xml::namespaceSelector( $namespace, '' ) .
-				"</td>
-			</tr>
-			<tr>
-				<td>
-				</td>
-				<td>" .
-					Xml::submitButton( wfMsg( 'import-interwiki-submit' ), array( 'accesskey' => 's' ) ) .
-				"</td>
-			</tr>" .
-			Xml::closeElement( 'table' ).
-			Xml::closeElement( 'form' ) .
-			Xml::closeElement( 'fieldset' )
-		);
 	}
 }
 

AutoLoader.php
@@ -478,6 +478,7 @@ $wgAutoloadLocalClasses = array(
 	'ShortPagesPage' => 'includes/specials/SpecialShortpages.php',
 	'SpecialAllpages' => 'includes/specials/SpecialAllpages.php',
 	'SpecialBookSources' => 'includes/specials/SpecialBooksources.php',
+	'SpecialImport' => 'includes/specials/SpecialImport.php',
 	'SpecialListGroupRights' => 'includes/specials/SpecialListgrouprights.php',
 	'SpecialMostlinkedtemplates' => 'includes/specials/SpecialMostlinkedtemplates.php',
 	'SpecialPrefixindex' => 'includes/specials/SpecialPrefixindex.php',

SpecialPage.php
@@ -146,7 +146,7 @@ class SpecialPage
 		'Log'                       => array( 'SpecialPage', 'Log' ),
 		'Blockip'                   => array( 'SpecialPage', 'Blockip', 'block' ),
 		'Undelete'                  => array( 'SpecialPage', 'Undelete', 'deletedhistory' ),
-		'Import'                    => array( 'SpecialPage', 'Import', 'import' ),
+		'Import'                    => 'SpecialImport',
 		'Lockdb'                    => array( 'SpecialPage', 'Lockdb', 'siteadmin' ),
 		'Unlockdb'                  => array( 'SpecialPage', 'Unlockdb', 'siteadmin' ),
 		'Userrights'                => 'UserrightsPage',

commit 01faf61e6f5ebf315a705088aa7fd2f0162a2b03

Quick hack to disable import for non privileged users. Prolly a regression from r43963
git-svn-id: svn+ssh://svn.wikimedia.org/svnroot/mediawiki/trunk/phase3/includes@44501 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

specials/SpecialImport.php
@@ -128,6 +128,11 @@ class SpecialImport extends SpecialPage {
 
 	private function showForm() {
 		global $wgUser, $wgOut, $wgRequest, $wgTitle, $wgImportSources;
+		# FIXME: Quick hack to disable import for non privileged users /Raymond 
+		# Regression from 43963 
+		if( !$wgUser->isAllowed( 'import' ) && !$wgUser->isAllowed( 'importupload' ) )
+			return $wgOut->permissionRequired( 'import' );
+
 		$action = $wgTitle->getLocalUrl( 'action=submit' );
 
 		if( $wgUser->isAllowed( 'importupload' ) ) {
@@ -173,8 +178,8 @@ class SpecialImport extends SpecialPage {
 				$wgOut->addWikiMsg( 'importnosources' );
 			}
 		}
-	
-		if( !empty( $wgImportSources ) ) {
+
+		if( $wgUser->isAllowed( 'import' ) && !empty( $wgImportSources ) ) {
 			$wgOut->addHTML(
 				Xml::openElement( 'fieldset' ) .
 				Xml::element( 'legend', null, wfMsg( 'importinterwiki' ) ) .