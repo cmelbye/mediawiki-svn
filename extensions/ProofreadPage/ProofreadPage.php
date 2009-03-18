cmelbye/mediawiki-svn

commit c18a3b4405b63f882ed22d5ee011e7112cb71906

Cleanup to r48486: * Make this disable-able by setting $wgDjvutext to null by default (commented) * Use wfFindFile instead of deprecated Image::newFromTitle * Only apply this when it's a djvu mime type
git-svn-id: svn+ssh://svn.wikimedia.org/svnroot/mediawiki/trunk@48533 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

extensions/ProofreadPage/ProofreadPage.php
@@ -14,7 +14,8 @@ $wgHooks['ImageOpenShowImageInlineBefore'][] = 'pr_imageMessage';
 $wgHooks['ArticleSaveComplete'][] = 'pr_articleSave';
 $wgHooks['EditFormPreloadText'][] = 'pr_preloadText';
 
-$wgDjvutxt = 'djvutxt';
+# Allows for extracting text from djvu files. To enable, set to 'djvutxt' or similar
+$wgDjvutxt = null;
 
 
 $wgExtensionCredits['other'][] = array(
@@ -659,15 +660,15 @@ function pr_preloadText( $textbox1, $mTitle ) {
 
 	$page_namespace = preg_quote( wfMsgForContent( 'proofreadpage_namespace' ), '/' );
 
-	if ( preg_match( "/^$page_namespace:(.*?)\/([0-9]*)$/", $mTitle->getPrefixedText(), $m ) ) {
+	if ( $wgDjvutxt && preg_match( "/^$page_namespace:(.*?)\/([0-9]*)$/", $mTitle->getPrefixedText(), $m ) ) {
 
 		$imageTitle = Title::makeTitleSafe( NS_IMAGE, $m[1] );
 		if ( !$imageTitle ) {
 			return true;
 		}
 
-		$image = Image::newFromTitle( $imageTitle );
-		if ( $image->exists() ) {
+		$image = wfFindFile( $title );
+		if ( $image->exists() && $image->getMimeType() == 'image/vnd.djvu' ) {
 			$srcPath = $image->getPath();
 			$cmd = "( " .wfEscapeShellArg( $wgDjvutxt );
 			$cmd .= " --page {$m[2]} ". wfEscapeShellArg( $srcPath )." )";

commit 648feeaf22152199682bb33ca309ec84348090fe

fix bug introduced in r48533 (variable name)
git-svn-id: svn+ssh://svn.wikimedia.org/svnroot/mediawiki/trunk@48536 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

extensions/ProofreadPage/ProofreadPage.php
@@ -663,7 +663,7 @@ function pr_preloadText( $textbox1, $mTitle ) {
 			return true;
 		}
 
-		$image = wfFindFile( $title );
+		$image = wfFindFile( $imageTitle );
 		if ( $image->exists() && $image->getMimeType() == 'image/vnd.djvu' ) {
 			$srcPath = $image->getPath();
 			$cmd = "( " .wfEscapeShellArg( $wgDjvutxt );