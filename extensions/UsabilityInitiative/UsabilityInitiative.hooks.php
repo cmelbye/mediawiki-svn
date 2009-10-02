cmelbye/mediawiki-svn

commit 1480b72a5b58af7ee247828738f3619d9e4de7d8

NavigableTOC: (bug 20918) Section headers with trailing spaces aren't shown in the NTOC. Also simplified the header regex by stealing it from Parser.php
git-svn-id: svn+ssh://svn.wikimedia.org/svnroot/mediawiki/trunk@57211 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

extensions/UsabilityInitiative/UsabilityInitiative.hooks.php
@@ -67,16 +67,16 @@ class UsabilityInitiativeHooks {
 				array( 'src' => 'js/plugins/jquery.wikiEditor.js', 'version' => 8 ),
 				array( 'src' => 'js/plugins/jquery.wikiEditor.toolbar.js', 'version' => 11 ),
 				array( 'src' => 'js/plugins/jquery.wikiEditor.dialogs.js', 'version' => 4 ),
-				array( 'src' => 'js/plugins/jquery.wikiEditor.toc.js', 'version' => 14 ),
+				array( 'src' => 'js/plugins/jquery.wikiEditor.toc.js', 'version' => 15 ),
 				// FIXME: jQuery UI doesn't belong here, should move to no_js2
 				// once we figure out how to do jQuery UI properly in JS2 
 				array( 'src' => 'js/js2/jquery-ui-1.7.2.js', 'version' => '1.7.2y' ),
 			),
 			'combined' => array(
-				array( 'src' => 'js/plugins.combined.js', 'version' => 35 ),
+				array( 'src' => 'js/plugins.combined.js', 'version' => 36 ),
 			),
 			'minified' => array(
-				array( 'src' => 'js/plugins.combined.min.js', 'version' => 35 ),
+				array( 'src' => 'js/plugins.combined.min.js', 'version' => 36 ),
 			),
 		),
 	);

extensions/UsabilityInitiative/js/plugins.combined.js
@@ -2182,7 +2182,7 @@ fn: {
 		// Build outline from wikitext
 		var outline = [];
 		var wikitext = '\n' + $.wikiEditor.fixOperaBrokenness( context.$textarea.val() ) + '\n';
-		var headings = wikitext.match( /(\r|\n)={1,5}.*={1,5}(?=(\r|\n))/g );
+		var headings = wikitext.match( /^={1,6}.+={1,6}\s*$/gm );
 		var offset = 0;
 		headings = $.makeArray( headings );
 		for ( var h = 0; h < headings.length; h++ ) {

extensions/UsabilityInitiative/js/plugins/jquery.wikiEditor.toc.js
@@ -173,7 +173,7 @@ fn: {
 		// Build outline from wikitext
 		var outline = [];
 		var wikitext = '\n' + $.wikiEditor.fixOperaBrokenness( context.$textarea.val() ) + '\n';
-		var headings = wikitext.match( /(\r|\n)={1,5}.*={1,5}(?=(\r|\n))/g );
+		var headings = wikitext.match( /^={1,6}.+={1,6}\s*$/gm );
 		var offset = 0;
 		headings = $.makeArray( headings );
 		for ( var h = 0; h < headings.length; h++ ) {

commit 2645eba4d04ff09c472aa4b2596e009fac4fe3d6

* NavigableTOC: (bug 20940) Previous header focused when cursor placed just before first = sign. Regression from r57211 * (bug 20942) Autoellipsed section titles too long when highlighted * Fix a bug where a page with starting with two identical headers and an empty section 0 (i.e. first character is '=') would confuse highlighting
git-svn-id: svn+ssh://svn.wikimedia.org/svnroot/mediawiki/trunk@57304 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

extensions/UsabilityInitiative/UsabilityInitiative.hooks.php
@@ -67,16 +67,16 @@ class UsabilityInitiativeHooks {
 				array( 'src' => 'js/plugins/jquery.wikiEditor.js', 'version' => 9 ),
 				array( 'src' => 'js/plugins/jquery.wikiEditor.toolbar.js', 'version' => 11 ),
 				array( 'src' => 'js/plugins/jquery.wikiEditor.dialogs.js', 'version' => 4 ),
-				array( 'src' => 'js/plugins/jquery.wikiEditor.toc.js', 'version' => 16 ),
+				array( 'src' => 'js/plugins/jquery.wikiEditor.toc.js', 'version' => 17 ),
 				// FIXME: jQuery UI doesn't belong here, should move to no_js2
 				// once we figure out how to do jQuery UI properly in JS2 
 				array( 'src' => 'js/js2/jquery-ui-1.7.2.js', 'version' => '1.7.2y' ),
 			),
 			'combined' => array(
-				array( 'src' => 'js/plugins.combined.js', 'version' => 37 ),
+				array( 'src' => 'js/plugins.combined.js', 'version' => 38 ),
 			),
 			'minified' => array(
-				array( 'src' => 'js/plugins.combined.min.js', 'version' => 37 ),
+				array( 'src' => 'js/plugins.combined.min.js', 'version' => 38 ),
 			),
 		),
 	);

extensions/UsabilityInitiative/js/plugins.combined.js
@@ -2193,23 +2193,22 @@ fn: {
 		}
 		// Build outline from wikitext
 		var outline = [];
-		var wikitext = '\n' + $.wikiEditor.fixOperaBrokenness( context.$textarea.val() ) + '\n';
+		var wikitext = $.wikiEditor.fixOperaBrokenness( context.$textarea.val() );
 		var headings = wikitext.match( /^={1,6}.+={1,6}\s*$/gm );
 		var offset = 0;
 		headings = $.makeArray( headings );
 		for ( var h = 0; h < headings.length; h++ ) {
-			text = headings[h];
+			text = $.trim( headings[h] );
 			// Get position of first occurence
 			var position = wikitext.indexOf( text, offset );
 			// Update offset to avoid stumbling on duplicate headings
-			if ( position > offset ) {
+			if ( position >= offset ) {
 				offset = position + 1;
 			} else if ( position == -1 ) {
 				// Not sure this is possible, or what should happen
 				continue;
 			}
-			// Trim off whitespace
-			text = $.trim( text );
+			
 			// Detect the starting and ending heading levels
 			var startLevel = 0;
 			for ( var c = 0; c < text.length; c++ ) {
@@ -2258,7 +2257,13 @@ fn: {
 		if ( $( 'input[name=wpSection]' ).val() == '' )
 			structure.unshift( { 'text': wgPageName.replace(/_/g, ' '), 'level': 1, 'index': 0, 'position': 0 } );
 		context.modules.$toc.html( buildList( structure ) );
-		context.modules.$toc.find( 'ul a' ).autoEllipse( { 'position': 'right', 'tooltip': true } );
+		
+		var links = context.modules.$toc.find( 'ul a' );
+		// Highlighted links are wider; autoEllipse links in
+		// highlighted state
+		links.addClass( 'currentSelection' );
+		links.autoEllipse( { 'position': 'right', 'tooltip': true } );
+		links.removeClass( 'currentSelection' );
 		// Cache the outline for later use
 		context.data.outline = outline;
 	}

extensions/UsabilityInitiative/js/plugins/jquery.wikiEditor.toc.js
@@ -172,23 +172,22 @@ fn: {
 		}
 		// Build outline from wikitext
 		var outline = [];
-		var wikitext = '\n' + $.wikiEditor.fixOperaBrokenness( context.$textarea.val() ) + '\n';
+		var wikitext = $.wikiEditor.fixOperaBrokenness( context.$textarea.val() );
 		var headings = wikitext.match( /^={1,6}.+={1,6}\s*$/gm );
 		var offset = 0;
 		headings = $.makeArray( headings );
 		for ( var h = 0; h < headings.length; h++ ) {
-			text = headings[h];
+			text = $.trim( headings[h] );
 			// Get position of first occurence
 			var position = wikitext.indexOf( text, offset );
 			// Update offset to avoid stumbling on duplicate headings
-			if ( position > offset ) {
+			if ( position >= offset ) {
 				offset = position + 1;
 			} else if ( position == -1 ) {
 				// Not sure this is possible, or what should happen
 				continue;
 			}
-			// Trim off whitespace
-			text = $.trim( text );
+			
 			// Detect the starting and ending heading levels
 			var startLevel = 0;
 			for ( var c = 0; c < text.length; c++ ) {
@@ -237,7 +236,13 @@ fn: {
 		if ( $( 'input[name=wpSection]' ).val() == '' )
 			structure.unshift( { 'text': wgPageName.replace(/_/g, ' '), 'level': 1, 'index': 0, 'position': 0 } );
 		context.modules.$toc.html( buildList( structure ) );
-		context.modules.$toc.find( 'ul a' ).autoEllipse( { 'position': 'right', 'tooltip': true } );
+		
+		var links = context.modules.$toc.find( 'ul a' );
+		// Highlighted links are wider; autoEllipse links in
+		// highlighted state
+		links.addClass( 'currentSelection' );
+		links.autoEllipse( { 'position': 'right', 'tooltip': true } );
+		links.removeClass( 'currentSelection' );
 		// Cache the outline for later use
 		context.data.outline = outline;
 	}