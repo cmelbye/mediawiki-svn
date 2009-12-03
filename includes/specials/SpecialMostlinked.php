cmelbye/mediawiki-svn

commit 775dcb38bbe1c0e5eff4b076fd698f8f49cad989

Merging core changes from trunk r56150 through r57447 ... there may be some merge inconsistencies at the moment still that need some fixes.
git-svn-id: svn+ssh://svn.wikimedia.org/svnroot/mediawiki/branches/wmf-deployment@57448 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

includes/specials/SpecialMostlinked.php
@@ -38,7 +38,7 @@ class MostlinkedPage extends QueryPage {
 			FROM $pagelinks
 			LEFT JOIN $page ON pl_namespace=page_namespace AND pl_title=page_title
 			GROUP BY pl_namespace, pl_title
-			HAVING COUNT(*) > $cutoff";
+			HAVING COUNT(*) > 1";
 	}
 
 	/**

commit 641989feeeb94ba7fd243516b55c8f6c797bcf83

Fixed miser mode hack broken by Brion in r57448: merged my version of it from trunk r59693.
git-svn-id: svn+ssh://svn.wikimedia.org/svnroot/mediawiki/branches/wmf-deployment@59694 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

includes/specials/SpecialMostlinked.php
@@ -27,7 +27,19 @@ class MostlinkedPage extends QueryPage {
 
 		$dbr = wfGetDB( DB_SLAVE );
 
-		if ($wgMiserMode) { $cutoff = 100; } else { $cutoff = 1; }
+		# In miser mode, reduce the query cost by adding a threshold for large wikis
+		if ( $wgMiserMode ) {
+			$numPages = SiteStats::pages();
+			if ( $numPages > 10000 ) {
+				$cutoff = 100;
+			} elseif ( $numPages > 100 ) {
+				$cutoff = intval( sqrt( $numPages ) );
+			} else {
+				$cutoff = 1;
+			}
+		} else {
+			$cutoff = 1;
+		}
 
 		list( $pagelinks, $page ) = $dbr->tableNamesN( 'pagelinks', 'page' );
 		return
@@ -38,7 +50,7 @@ class MostlinkedPage extends QueryPage {
 			FROM $pagelinks
 			LEFT JOIN $page ON pl_namespace=page_namespace AND pl_title=page_title
 			GROUP BY pl_namespace, pl_title
-			HAVING COUNT(*) > 1";
+			HAVING COUNT(*) > $cutoff";
 	}
 
 	/**