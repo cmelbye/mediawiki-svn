cmelbye/mediawiki-svn

commit 35b7a5d566d929ee082517319d8625fe636adf0d

Integrated Michael Dale's patch to add a preference to the Labs section of user preferences for the AddMediaWizard, which hooks into the toolbar's file tool.
git-svn-id: svn+ssh://svn.wikimedia.org/svnroot/mediawiki/trunk@65660 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

extensions/UsabilityInitiative/Makefile
@@ -33,7 +33,7 @@ PLUGINS := \
 	js/plugins/jquery.wikiEditor.highlight.js\
 	js/plugins/jquery.wikiEditor.preview.js\
 	js/plugins/jquery.wikiEditor.publish.js\
-	js/plugins/jquery.wikiEditor.templateEditor.js\
+	js/plugins/jquery.wikiEditor.templateEditor.js\	
 	js/plugins/jquery.wikiEditor.toc.js\
 	js/plugins/jquery.wikiEditor.toolbar.js
 
@@ -44,6 +44,7 @@ WIKIEDITOR_MODULES := \
 	WikiEditor/Modules/Toc/Toc.js\
 	WikiEditor/Modules/Toolbar/Toolbar.js\
 	WikiEditor/Modules/TemplateEditor/TemplateEditor.js
+	WikiEditor/Modules/AddMediaWizard/AddMediaWizard.js
 
 VECTOR_MODULES := \
 	Vector/Modules/CollapsibleNav/CollapsibleNav.js\

extensions/UsabilityInitiative/WikiEditor/Modules/AddMediaWizard/AddMediaWizard.js
@@ -1,0 +1,10 @@
+
+/* JavaScript for AddMediaWizard gadget */
+
+if( typeof mwAddMediaConfig == 'undefined'){
+    mwAddMediaConfig = {};
+}
+mwAddMediaConfig['enabled_providers'] = [ 'wiki_commons', 'upload' ];
+
+// Transclude mwEmbed.js
+importScriptURI('http://prototype.wikimedia.org/s-2/js/mwEmbed/remotes/mediaWiki.js?&uselang=' +  wgUserLanguage );

extensions/UsabilityInitiative/WikiEditor/WikiEditor.hooks.php
@@ -18,6 +18,7 @@ class WikiEditorHooks {
 			array( 'src' => 'Modules/Toc/Toc.js', 'version' => 7 ),
 			array( 'src' => 'Modules/Toolbar/Toolbar.js', 'version' => 83 ),
 			array( 'src' => 'Modules/TemplateEditor/TemplateEditor.js', 'version' => 6 ),
+			array( 'src' => 'Modules/AddMediaWizard/AddMediaWizard.js', 'version' => 6 ),
 		),
 		'combined' => array(
 			array( 'src' => 'WikiEditor.combined.js', 'version' => 87 ),
@@ -66,6 +67,19 @@ class WikiEditorHooks {
 				'wikieditor-template-editor-dialog-submit',
 				'wikieditor-template-editor-dialog-cancel',
 			),
+		),
+		'addMediaWizard' => array(
+			'i18n' => 'addMediaWizard',
+			'preferences' => array(
+				'enable' => array(
+				 	'key' => 'addmediawizard-gadget',
+					'ui' => array(
+						'type' => 'toggle',
+						'label-message' => 'wikieditor-addMediaWizard-preference',
+						'section' => 'editing/labs',
+					),
+				),
+			)
 		),
 		'preview' => array(
 			'i18n' => 'WikiEditorPreview',
@@ -130,7 +144,7 @@ class WikiEditorHooks {
 				'wikieditor-toc-show',
 				'wikieditor-toc-hide',
 			),
-		),
+		),		
 		'toolbar' => array(
 			'i18n' => 'WikiEditorToolbar',
 			'preferences' => array(
@@ -443,7 +457,6 @@ class WikiEditorHooks {
 					if ( $key == 'enable' && !$enable['user'] ) {
 						continue;
 					}
-					
 					// The preference with the key 'enable' determines if the rest are even relevant, so in the future
 					// setting up some dependencies on that might make sense
 					$defaultPreferences[$preference['key']] = $preference['ui'];

extensions/UsabilityInitiative/WikiEditor/Modules/AddMediaWizard/AddMediaWizard.i18n.php
@@ -1,0 +1,16 @@
+<?php 
+/**
+ * Internationalisation for the AddMediaWizard gadget
+ *
+ * @file
+ * @ingroup Extensions
+ */
+
+$messages = array();
+
+/** English
+ * @author Michael Dale
+ */
+$messages['en'] = array(
+    'wikieditor-addMediaWizard-preference' => 'Enable Add Media Wizard Gadget',
+);

extensions/UsabilityInitiative/WikiEditor/WikiEditor.php
@@ -18,6 +18,7 @@
 
 /* Configuration */
 
+
 // Each module may be configured individually to be globally on/off or user preference based
 $wgWikiEditorModules = array(
 	'toolbar' => array( 'global' => false, 'user' => true ), // Order is significant: makes beta prefs appear before labs prefs
@@ -26,6 +27,7 @@ $wgWikiEditorModules = array(
 	'publish' => array( 'global' => false, 'user' => true ),
 	'toc' => array( 'global' => false, 'user' => true ),
 	'templateEditor' => array( 'global' => false, 'user' => true ),
+	'addMediaWizard' => array( 'global' => false, 'user' => false ),
 );
 
 /* Setup */
@@ -57,6 +59,7 @@ $wgExtensionMessagesFiles['WikiEditorPublish'] = dirname( __FILE__ ) . '/Modules
 $wgExtensionMessagesFiles['WikiEditorToc'] = dirname( __FILE__ ) . '/Modules/Toc/Toc.i18n.php';
 $wgExtensionMessagesFiles['WikiEditorToolbar'] = dirname( __FILE__ ) . '/Modules/Toolbar/Toolbar.i18n.php';
 $wgExtensionMessagesFiles['WikiEditorTemplateEditor'] = dirname( __FILE__ ) . '/Modules/TemplateEditor/TemplateEditor.i18n.php';
+$wgExtensionMessagesFiles['WikiEditorAddMediaWizard'] = dirname( __FILE__ ) . '/Modules/AddMediaWizard/AddMediaWizard.i18n.php';
 
 // Register Hooks
 $wgHooks['EditPageBeforeEditToolbar'][] = 'WikiEditorHooks::addModules';

commit 11e1623d035c071f22b658ad1d110c891ee849c6

UsabilityInitiative: Various fixes * Fix Makefile broken by r65660 and run it to combine+minify the AMW code into WikiEditor.combined.js and .min.js * Fix some whitespace in AddMediaWizard.js while I'm in there * Fix excessive $newElement.html() calls added in r65646, these are expensive. Instead, build the HTML first, then add it with .html() * Fix some style and a missing semicolon for r65646 and previous revs
git-svn-id: svn+ssh://svn.wikimedia.org/svnroot/mediawiki/trunk@65763 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

extensions/UsabilityInitiative/Makefile
@@ -33,7 +33,7 @@ PLUGINS := \
 	js/plugins/jquery.wikiEditor.highlight.js\
 	js/plugins/jquery.wikiEditor.preview.js\
 	js/plugins/jquery.wikiEditor.publish.js\
-	js/plugins/jquery.wikiEditor.templateEditor.js\	
+	js/plugins/jquery.wikiEditor.templateEditor.js\
 	js/plugins/jquery.wikiEditor.toc.js\
 	js/plugins/jquery.wikiEditor.toolbar.js
 
@@ -43,7 +43,7 @@ WIKIEDITOR_MODULES := \
 	WikiEditor/Modules/Publish/Publish.js\
 	WikiEditor/Modules/Toc/Toc.js\
 	WikiEditor/Modules/Toolbar/Toolbar.js\
-	WikiEditor/Modules/TemplateEditor/TemplateEditor.js
+	WikiEditor/Modules/TemplateEditor/TemplateEditor.js\
 	WikiEditor/Modules/AddMediaWizard/AddMediaWizard.js
 
 VECTOR_MODULES := \

extensions/UsabilityInitiative/UsabilityInitiative.hooks.php
@@ -72,7 +72,7 @@ class UsabilityInitiativeHooks {
 				array( 'src' => 'js/plugins/jquery.delayedBind.js', 'version' => 1 ),
 				array( 'src' => 'js/plugins/jquery.suggestions.js', 'version' => 11 ),
 				array( 'src' => 'js/plugins/jquery.textSelection.js', 'version' => 30 ),
-				array( 'src' => 'js/plugins/jquery.wikiEditor.js', 'version' => 183 ),
+				array( 'src' => 'js/plugins/jquery.wikiEditor.js', 'version' => 184 ),
 				array( 'src' => 'js/plugins/jquery.wikiEditor.highlight.js', 'version' => 52 ),
 				array( 'src' => 'js/plugins/jquery.wikiEditor.toolbar.js', 'version' => 59 ),
 				array( 'src' => 'js/plugins/jquery.wikiEditor.dialogs.js', 'version' => 24 ),
@@ -82,10 +82,10 @@ class UsabilityInitiativeHooks {
 				array( 'src' => 'js/plugins/jquery.wikiEditor.publish.js', 'version' => 5 ),
 			),
 			'combined' => array(
-				array( 'src' => 'js/plugins.combined.js', 'version' => 387 ),
+				array( 'src' => 'js/plugins.combined.js', 'version' => 388 ),
 			),
 			'minified' => array(
-				array( 'src' => 'js/plugins.combined.min.js', 'version' => 387 ),
+				array( 'src' => 'js/plugins.combined.min.js', 'version' => 388 ),
 			),
 		),
 	);

extensions/UsabilityInitiative/WikiEditor/Modules/AddMediaWizard/AddMediaWizard.js
@@ -1,11 +1,10 @@
-
 /* JavaScript for AddMediaWizard gadget */
-if ( wgWikiEditorEnabledModules.addMediaWizard ) { 
-	if( typeof mwAddMediaConfig == 'undefined'){
+if ( wgWikiEditorEnabledModules.addMediaWizard ) {
+	if( typeof mwAddMediaConfig == 'undefined' ) {
 	    mwAddMediaConfig = {};
 	}
 	mwAddMediaConfig['enabled_providers'] = [ 'wiki_commons', 'upload' ];
 
 	// Transclude mwEmbed support
-	importScriptURI('http://prototype.wikimedia.org/s-2/js/mwEmbed/remotes/mediaWiki.js?&uselang=' +  wgUserLanguage );	
+	importScriptURI( 'http://prototype.wikimedia.org/s-2/js/mwEmbed/remotes/mediaWiki.js?&uselang=' +  wgUserLanguage );	
 }

extensions/UsabilityInitiative/WikiEditor/WikiEditor.combined.js
@@ -2078,3 +2078,13 @@ $j(document).ready( function() {
 		$j( 'textarea#wpTextbox1' ).wikiEditor( 'addModule', 'templateEditor' );
 	}
 });
+/* JavaScript for AddMediaWizard gadget */
+if ( wgWikiEditorEnabledModules.addMediaWizard ) {
+	if( typeof mwAddMediaConfig == 'undefined' ) {
+	    mwAddMediaConfig = {};
+	}
+	mwAddMediaConfig['enabled_providers'] = [ 'wiki_commons', 'upload' ];
+
+	// Transclude mwEmbed support
+	importScriptURI( 'http://prototype.wikimedia.org/s-2/js/mwEmbed/remotes/mediaWiki.js?&uselang=' +  wgUserLanguage );	
+}

extensions/UsabilityInitiative/WikiEditor/WikiEditor.hooks.php
@@ -21,10 +21,10 @@ class WikiEditorHooks {
 			array( 'src' => 'Modules/AddMediaWizard/AddMediaWizard.js', 'version' => 6 ),
 		),
 		'combined' => array(
-			array( 'src' => 'WikiEditor.combined.js', 'version' => 87 ),
+			array( 'src' => 'WikiEditor.combined.js', 'version' => 88 ),
 		),
 		'minified' => array(
-			array( 'src' => 'WikiEditor.combined.min.js', 'version' => 87 ),
+			array( 'src' => 'WikiEditor.combined.min.js', 'version' => 88 ),
 		),
 	);
 	static $messages = array(

extensions/UsabilityInitiative/js/plugins.combined.js
@@ -7031,12 +7031,12 @@ if ( !context || typeof context == 'undefined' ) {
 				
 				// If the pasted content is plain text then wrap it in a <p> and adjust the <br> accordingly 
 				var pasteContent = context.fn.getOffset( cursorPos[0] ).node;
-				var removeNextBR = false
-				while ( pasteContent != null && ! $( pasteContent ).hasClass( 'wikiEditor' ) ) {
+				var removeNextBR = false;
+				while ( pasteContent != null && !$( pasteContent ).hasClass( 'wikiEditor' ) ) {
 					var currentNode = pasteContent;
 					pasteContent = pasteContent.nextSibling;
 					if ( currentNode.nodeName == '#text' && currentNode.nodeValue == currentNode.wholeText ) {
-						var pWrapper = $( '<p></p>' ).addClass( 'wikiEditor' );
+						var pWrapper = $( '<p />' ).addClass( 'wikiEditor' );
 						$( currentNode ).wrap( pWrapper );
 						$( currentNode ).addClass( 'wikiEditor' );
 						removeNextBR = true;
@@ -7057,25 +7057,25 @@ if ( !context || typeof context == 'undefined' ) {
 					var $newElement;
 					if ( $currentElement.is( 'p' ) || $currentElement.is( 'div' ) || $currentElement.is( 'pre' ) ) {
 						//Convert all <div>, <p> and <pre> that was pasted into a <p> element
-						$newElement = $( '<p></p>' );
+						$newElement = $( '<p />' );
 					} else {
 						// everything else becomes a <span>
-						$newElement = $( '<span></span>' ).addClass( 'wikiEditor' );
+						$newElement = $( '<span />' ).addClass( 'wikiEditor' );
 					}
 					
 					// If the pasted content was html, just convert it into text and <br>
-					var text = $.trim( $currentElement.text() );
-					var pieces = text.split('\n');					
+					var pieces = $.trim( $currentElement.text() ).split( '\n' );
+					var newElementHTML = '';
 					for ( var i = 0; i < pieces.length; i++ ) {
-            			if ( pieces[i] ) {
-							$newElement.html( $newElement.html() + $.trim( pieces[i] ) );
+						if ( pieces[i] ) {
+							newElementHTML += $.trim( pieces[i] );
 						} else {
-							var $brElement =  $( '<span></span>' ).html( '<br class = "wikiEditor" />' );
-							$newElement.html( $newElement.html() + $brElement.html() );
+							newElementHTML += '<span><br class="wikiEditor" /></span>';
 						}
-            		}
-					$newElement.insertAfter( $currentElement );
-					$newElement.addClass( 'wikiEditor' );
+					}
+					$newElement.html( newElementHTML )
+						.addClass( 'wikiEditor' )
+						.insertAfter( $currentElement );
 					$currentElement.remove();
 
 					$selection = context.$content.find( ':not(.wikiEditor)' );

extensions/UsabilityInitiative/js/plugins/jquery.wikiEditor.js
@@ -446,12 +446,12 @@ if ( !context || typeof context == 'undefined' ) {
 				
 				// If the pasted content is plain text then wrap it in a <p> and adjust the <br> accordingly 
 				var pasteContent = context.fn.getOffset( cursorPos[0] ).node;
-				var removeNextBR = false
-				while ( pasteContent != null && ! $( pasteContent ).hasClass( 'wikiEditor' ) ) {
+				var removeNextBR = false;
+				while ( pasteContent != null && !$( pasteContent ).hasClass( 'wikiEditor' ) ) {
 					var currentNode = pasteContent;
 					pasteContent = pasteContent.nextSibling;
 					if ( currentNode.nodeName == '#text' && currentNode.nodeValue == currentNode.wholeText ) {
-						var pWrapper = $( '<p></p>' ).addClass( 'wikiEditor' );
+						var pWrapper = $( '<p />' ).addClass( 'wikiEditor' );
 						$( currentNode ).wrap( pWrapper );
 						$( currentNode ).addClass( 'wikiEditor' );
 						removeNextBR = true;
@@ -472,25 +472,25 @@ if ( !context || typeof context == 'undefined' ) {
 					var $newElement;
 					if ( $currentElement.is( 'p' ) || $currentElement.is( 'div' ) || $currentElement.is( 'pre' ) ) {
 						//Convert all <div>, <p> and <pre> that was pasted into a <p> element
-						$newElement = $( '<p></p>' );
+						$newElement = $( '<p />' );
 					} else {
 						// everything else becomes a <span>
-						$newElement = $( '<span></span>' ).addClass( 'wikiEditor' );
+						$newElement = $( '<span />' ).addClass( 'wikiEditor' );
 					}
 					
 					// If the pasted content was html, just convert it into text and <br>
-					var text = $.trim( $currentElement.text() );
-					var pieces = text.split('\n');					
+					var pieces = $.trim( $currentElement.text() ).split( '\n' );
+					var newElementHTML = '';
 					for ( var i = 0; i < pieces.length; i++ ) {
-            			if ( pieces[i] ) {
-							$newElement.html( $newElement.html() + $.trim( pieces[i] ) );
+						if ( pieces[i] ) {
+							newElementHTML += $.trim( pieces[i] );
 						} else {
-							var $brElement =  $( '<span></span>' ).html( '<br class = "wikiEditor" />' );
-							$newElement.html( $newElement.html() + $brElement.html() );
+							newElementHTML += '<span><br class="wikiEditor" /></span>';
 						}
-            		}
-					$newElement.insertAfter( $currentElement );
-					$newElement.addClass( 'wikiEditor' );
+					}
+					$newElement.html( newElementHTML )
+						.addClass( 'wikiEditor' )
+						.insertAfter( $currentElement );
 					$currentElement.remove();
 
 					$selection = context.$content.find( ':not(.wikiEditor)' );