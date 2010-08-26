cmelbye/mediawiki-svn

commit eaf4d23a80cebd4138514b5017e0c01fc4841157

checking for bad notice names per r71613
git-svn-id: svn+ssh://svn.wikimedia.org/svnroot/mediawiki/trunk@71639 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

extensions/CentralNotice/SpecialCentralNotice.php
@@ -1081,9 +1081,12 @@ class CentralNotice extends SpecialPage {
 	public static function getNoticeId( $noticeName ) {
 		 $dbr = wfGetDB( DB_SLAVE );
 		 $eNoticeName = htmlspecialchars( $noticeName );
-		 $res = $dbr->select( 'cn_notices', 'not_id', array( 'not_name' => $eNoticeName ) );
-		 $row = $dbr->fetchObject( $res );
-		 return $row->not_id;
+		 $row = $dbr->selectRow( 'cn_notices', 'not_id', array( 'not_name' => $eNoticeName ) );
+		 if ( $row ) {
+		 	return $row->not_id;
+		 } else {
+		 	return;
+		 }
 	}
 
 	function getNoticeLanguages( $noticeName ) {
@@ -1282,7 +1285,7 @@ class CentralNotice extends SpecialPage {
 		$oldLanguages = $this->getNoticeLanguages( $notice );
 		
 		// Get the notice id
-		$row = $dbw->selectRow( 'cn_notices', 'not_id', array( 'not_name' => $notice ) );
+		$row = $this->getNoticeId( $notice );
 		
 		// Add newly assigned languages
 		$addLanguages = array_diff( $newLanguages, $oldLanguages );
@@ -1303,7 +1306,7 @@ class CentralNotice extends SpecialPage {
 		$dbw->commit();
 	}
 
-	function dropDownList( $text, $values ) {
+	public static function dropDownList( $text, $values ) {
 		$dropDown = "* {$text}\n";
 		foreach ( $values as $value ) {
 			$dropDown .= "**{$value}\n";
@@ -1335,18 +1338,26 @@ class CentralNoticePager extends TemplatePager {
 	function getQueryInfo() {
 		$notice = $this->mRequest->getVal( 'notice' );
 		$noticeId = CentralNotice::getNoticeId( $notice );
-		// Return all the banners not already assigned to the current campaign
-		return array(
-			'tables' => array( 'cn_assignments', 'cn_templates' ),
-			'fields' => array( 'cn_templates.tmp_name', 'cn_templates.tmp_id' ),
-			'conds' => array( 'cn_assignments.tmp_id IS NULL' ),
-			'join_conds' => array(
-				'cn_assignments' => array( 
-					'LEFT JOIN',
-					"cn_assignments.tmp_id = cn_templates.tmp_id AND cn_assignments.not_id = $noticeId"
+		if ( $noticeId ) {
+			// Return all the banners not already assigned to the current campaign
+			return array(
+				'tables' => array( 'cn_assignments', 'cn_templates' ),
+				'fields' => array( 'cn_templates.tmp_name', 'cn_templates.tmp_id' ),
+				'conds' => array( 'cn_assignments.tmp_id IS NULL' ),
+				'join_conds' => array(
+					'cn_assignments' => array( 
+						'LEFT JOIN',
+						"cn_assignments.tmp_id = cn_templates.tmp_id AND cn_assignments.not_id = $noticeId"
+					)
 				)
-			)
-		);
+			);
+		} else {
+			// Return all the banners in the database
+			return array(
+				'tables' => 'cn_templates',
+				'fields' => array( 'tmp_name', 'tmp_id' ),
+			);
+		}
 	}
 	
 	/**

commit 6f1f1433a0fc930c0cdb1c76a67b50978ecfb901

fixing bug introduced in r71639
git-svn-id: svn+ssh://svn.wikimedia.org/svnroot/mediawiki/trunk@71729 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

extensions/CentralNotice/SpecialCentralNotice.php
@@ -1324,7 +1324,7 @@ class CentralNotice extends SpecialPage {
 		$oldLanguages = $this->getNoticeLanguages( $notice );
 		
 		// Get the notice id
-		$row = $this->getNoticeId( $notice );
+		$row = $dbw->selectRow( 'cn_notices', 'not_id', array( 'not_name' => $notice ) );
 		
 		// Add newly assigned languages
 		$addLanguages = array_diff( $newLanguages, $oldLanguages );