cmelbye/mediawiki-svn

commit 2a3a6e2f8a7d26e2073d4b711e18392c1e7f5068

Fix broken r72956 with another approach
git-svn-id: svn+ssh://svn.wikimedia.org/svnroot/mediawiki/trunk/phase3@72961 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

includes/ResourceLoaderModule.php
@@ -740,28 +740,26 @@ abstract class ResourceLoaderWikiModule extends ResourceLoaderModule {
 		if ( isset( $this->modifiedTime[$hash] ) ) {
 			return $this->modifiedTime[$hash];
 		}
+
 		$titles = array();
 		foreach ( $this->getPages( $context ) as $page => $options ) {
-			$titles[] = Title::makeTitle( $options['ns'], $page );
+			$titles[$options['ns']][$page] = true;
 		}
-		// Do batch existence check
-		// TODO: This would work better if page_touched were loaded by this as well
-		$lb = new LinkBatch( $titles );
-		$lb->setCaller( __METHOD__ );
-		$lb->execute();
+
 		$modifiedTime = 1; // wfTimestamp() interprets 0 as "now"
 
-		$ids = array();
-		foreach ( $titles as $title ) {
-			if ( $title->exists() ) {
-				$ids[] = $title->getArticleId();
+		if ( $titles ) {
+			$dbr = wfGetDB( DB_SLAVE );
+			$latest = $dbr->selectField( 'page', 'MAX(page_touched)',
+				$dbr->makeWhereFrom2d( $titles, 'page_namespace', 'page_title' ),
+				__METHOD__ );
+
+			if ( $latest ) {
+				$modifiedTime = wfTimestamp( TS_UNIX, $modifiedTime );
 			}
 		}
 
-		$dbr = wfGetDB( DB_SLAVE );
-		$modifiedTime = $dbr->selectField( 'page', 'MAX(page_touched)', array( 'page_id' => $ids ), __METHOD__ );
-
-		return $this->modifiedTime[$hash] = wfTimestamp( TS_UNIX, $modifiedTime );
+		return $this->modifiedTime[$hash] = $modifiedTime;
 	}
 }
 

commit 037ada78be86604be34090a85fb07eb263a187aa

Fix timestamp generation for pages broken in r72961.
git-svn-id: svn+ssh://svn.wikimedia.org/svnroot/mediawiki/trunk/phase3@74057 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

includes/ResourceLoaderModule.php
@@ -822,7 +822,7 @@ abstract class ResourceLoaderWikiModule extends ResourceLoaderModule {
 				__METHOD__ );
 
 			if ( $latest ) {
-				$modifiedTime = wfTimestamp( TS_UNIX, $modifiedTime );
+				$modifiedTime = wfTimestamp( TS_UNIX, $latest );
 			}
 		}
 