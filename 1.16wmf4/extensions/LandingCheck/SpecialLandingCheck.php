b8a5478300e63d2e9384b2d523d447c3831989db
nimishg <nimishg@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2010-11-02 23:41:53 +0000

    creating LandingCheck extension in deployment
    git-svn-id: svn+ssh://svn.wikimedia.org/svnroot/mediawiki/branches/wmf@75890 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

---
+++ 1.16wmf4/extensions/LandingCheck/SpecialLandingCheck.php
@@ -0,0 +1,53 @@
+<?php
+if ( !defined( 'MEDIAWIKI' ) ) {
+	echo "LandingCheck extension\n";
+	exit( 1 );
+}
+
+/**
+ * This page checks to see if a version of a landing page exists for the user's language and 
+ * country. If not, it looks for a version localized for the user's language. If that doesn't exist 
+ * either, it looks for the English version. If any of those exist, it then redirects the user.
+ */
+class SpecialLandingCheck extends SpecialPage {
+	
+	public function __construct() {
+		// Register special page
+		parent::__construct( 'LandingCheck' );
+	}
+	
+	public function execute( $sub ) {
+		global $wgOut, $wgUser, $wgRequest, $wgScript;
+		
+		$language = $wgRequest->getText( 'language', 'en' );
+		$country = $wgRequest->getText( 'country', 'US' );
+		$landingPage = $wgRequest->getText( 'landing_page', 'Donate' );
+		
+		$tracking = wfArrayToCGI( array( 
+			'utm_source' => $wgRequest->getVal( 'utm_source' ),
+			'utm_medium' => $wgRequest->getVal( 'utm_medium' ),
+			'utm_campaign' => $wgRequest->getVal( 'utm_campaign' ),
+			'referrer' => $wgRequest->getHeader( 'referer' )
+		) );
+		
+		if ( $landingPage ) {
+			if ( strpos( $landingPage, 'Special:' ) === false ) { // landing page is not a special page
+				$target = Title::newFromText( $landingPage . '/' . $language . '/' . $country );
+				if( $target->isKnown() ) {
+					$wgOut->redirect( $target->getLocalURL( $tracking ) );
+				} else {
+					$target = Title::newFromText( $landingPage . '/' . $language );
+					if( $target->isKnown() ) {
+						$wgOut->redirect( $target->getLocalURL( $tracking ) );
+					} elseif ( $language != 'en' ) {
+						$target = Title::newFromText( $landingPage . '/en' );
+						if( $target->isKnown() ) {
+							$wgOut->redirect( $target->getLocalURL( $tracking ) );
+						}
+					}
+				}
+			}
+		}
+		
+	}
+}
