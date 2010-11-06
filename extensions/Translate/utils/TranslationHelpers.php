cmelbye/mediawiki-svn

commit ac2ce2303871f1713a13d816f9ed5904d0807e13

Cleanup javascript code a little
git-svn-id: svn+ssh://svn.wikimedia.org/svnroot/mediawiki/trunk@76102 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

extensions/Translate/utils/TranslationHelpers.php
@@ -964,25 +964,26 @@ class TranslationHelpers {
 			'page' => $this->title->getPrefixedDbKey(),
 			'loadgroup' => $this->group->getId(),
 		) );
-		$url = Xml::escapeJsString( $url );
+		$url = Xml::encodeJsVar( $url );
 
-		$dialogID = $this->dialogID();
-		$id = Sanitizer::escapeId( "tm-lazysug-$dialogID" );
+		$id = Sanitizer::escapeId( 'tm-lazysug-' . $this->dialogID() );
+		$target = self::jQueryPathId( $id );
 
-		$script = Html::inlineScript( "jQuery('#$id').load( \"$url\" )" );
+		$script = Html::inlineScript( "jQuery($target).load($url)" );
 		$spinner = Html::element( 'div', array( 'class' => 'mw-ajax-loader' ) );
 		return Html::rawElement( 'div', array( 'id' => $id ), $script . $spinner );
 	}
 
 	public function dialogID() {
-		return sha1( $this->title->getPrefixedDbKey() );
+		$hash = sha1( $this->title->getPrefixedDbKey() );
+		return substr( $hash, 0, 4 );
 	}
 
 	public function adder( $source ) {
-			$target = Xml::escapeJsString( $this->getTextareaId() );
-			$source = Xml::escapeJsString( $source );
+			$target = self::jQueryPathId( $this->getTextareaId() );
+			$source = self::jQueryPathId( $source );
 			$params = array(
-				'onclick' => "jQuery('#$target').val(jQuery('#$source').text()).focus(); return false;",
+				'onclick' => "jQuery($target).val($source).text()).focus(); return false;",
 				'href' => '#',
 				'title' => wfMsg( 'translate-use-suggestion' )
 			);
@@ -1025,6 +1026,10 @@ class TranslationHelpers {
 		$jsEdit = TranslationEditPage::jsEdit( $target, $group );
 
 		return $wgUser->getSkin()->link( $target, $text, $jsEdit, $params );
+	}
+
+	public static function jQueryPathId( $id ) {
+		return Xml::encodeJsVar( "#$id" );
 	}
 
 	/**

extensions/Translate/TranslatePage.php
@@ -477,18 +477,19 @@ class SpecialTranslate extends SpecialPage {
 		$desc = $this->getGroupDescription( $block );
 		$hasSubblocks = is_array( $blocks ) && count( $blocks );
 
-		$subid = "mw-subgroup-$id";
+		$subid = Sanitizer::escapeId( "mw-subgroup-$id" );
 
 		if ( $hasSubblocks ) {
 			$msg = wfMsgExt( 'translate-showsub', 'parsemag', $wgLang->formatNum( count( $blocks ) ) );
-			$desc .= Html::element( 'a', array( 'onclick' => "jQuery('#$subid').toggle()", 'class' => 'mw-sp-showmore' ), $msg );
+			$target = TranslationHelpers::jQueryPathId( $subid );
+			$desc .= Html::element( 'a', array( 'onclick' => "jQuery($target).toggle()", 'class' => 'mw-sp-showmore' ), $msg );
 		}
 
 		$out = "\n<tr><td>$label</td>\n<td>$desc</td></tr>\n";
 		if ( $hasSubblocks ) {
 			$out .= "<tr><td></td><td>\n";
 			$tableParams = array(
-				'id' => "mw-subgroup-$id",
+				'id' => $subid,
 				'style' => 'display:none;',
 				'class' => "mw-sp-translate-subgroup depth-$level",
 			);

extensions/Translate/_autoload.php
@@ -110,6 +110,8 @@ $wgAutoloadClasses['TranslationStats'] = $dir . 'utils/TranslationStats.php';
 $wgAutoloadClasses['TranslationMemoryUpdater'] = $dir . 'utils/TranslationMemoryUpdater.php';
 
 $wgAutoloadClasses['TranslateYaml'] = $dir . 'utils/TranslateYaml.php';
+
+$wgAutoloadClasses['TranslateBC'] = $dir . 'utils/CompatibilityCode.php';
 /**@}*/
 
 /**

extensions/Translate/utils/CompatibilityCode.php
@@ -1,0 +1,44 @@
+<?php
+/**
+ * Code for facilitiating backwards compatibility for older %MediaWikis.
+ *
+ * @file
+ * @author Niklas Laxström
+ * @copyright Copyright © 2010, Niklas Laxström
+ * @license http://www.gnu.org/copyleft/gpl.html GNU General Public License 2.0 or later
+ */
+
+/**
+ * General BC code.
+ */
+class TranslateBC {
+
+	/**
+	 * Create a call to a JavaScript function. The supplied arguments will be 
+	 * encoded using Xml::encodeJsVar(). 
+	 *
+	 * @param $name The name of the function to call, or a JavaScript expression
+	 *    which evaluates to a function object which is called.
+	 * @param $args Array of arguments to pass to the function.
+	 * @since 1.17
+	 */
+	public static function encodeJsCall( $name, $args ) {
+		if ( method_exists( 'Xml', 'encodeJsCall' ) ) {
+			return Xml::encodeJsCall( $name, $args );
+		}
+
+		$s = "$name(";
+		$first = true;
+		foreach ( $args as $arg ) {
+			if ( $first ) {
+				$first = false;
+			} else {
+				$s .= ', ';
+			}
+			$s .= Xml::encodeJsVar( $arg );
+		}
+		$s .= ");\n";
+		return $s;
+	}
+
+}

extensions/Translate/utils/TranslationEditPage.php
@@ -169,21 +169,14 @@ class TranslationEditPage {
 	public static function jsEdit( Title $title, $group = "" ) {
 		global $wgUser;
 
-		if ( !$wgUser->isAllowed( 'translate' ) ) {
+		if ( !$wgUser->isAllowed( 'translate' ) ||  !$wgUser->getOption( 'translate-jsedit' ) ) {
 			return array();
 		}
-
-		if ( !$wgUser->getOption( 'translate-jsedit' ) ) {
-			return array();
-		}
-
-		$dbKey = $title->getPrefixedDbKey();
-		$jsTitle = Xml::escapeJsString( $dbKey );
-		$jsGroup = Xml::escapeJsString( $group );
 
 		return array(
-			'onclick' => "return trlOpenJsEdit( \"$jsTitle\", \"$jsGroup\" );",
-			'title' => wfMsg( 'translate-edit-title', $dbKey )
+			'onclick' => TranslateBC::encodeJsCall(
+				'return trlOpenJsEdit', array( $title->getPrefixedDbKey(), $group ) ),
+			'title' => wfMsg( 'translate-edit-title', $title->getPrefixedText() )
 		);
 	}
 }

commit 39765ca8272ad513e885ce5dc4eb8d782a976a78

Fix js error introduced in r76102
git-svn-id: svn+ssh://svn.wikimedia.org/svnroot/mediawiki/trunk@76183 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

extensions/Translate/utils/TranslationHelpers.php
@@ -983,7 +983,7 @@ class TranslationHelpers {
 			$target = self::jQueryPathId( $this->getTextareaId() );
 			$source = self::jQueryPathId( $source );
 			$params = array(
-				'onclick' => "jQuery($target).val($source).text()).focus(); return false;",
+				'onclick' => "jQuery($target).val(jQuery($source).text()).focus(); return false;",
 				'href' => '#',
 				'title' => wfMsg( 'translate-use-suggestion' )
 			);