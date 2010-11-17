651b631196863602a847af5070b1cb56a9d0a6ef
dale <dale@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2010-11-17 00:02:08 +0000

    refactored addPlayBtnLarge for android support added helper mobile device identifiers small css fix for player button outside of interface div
    git-svn-id: svn+ssh://svn.wikimedia.org/svnroot/mediawiki/branches/MwEmbedStandAlone@76862 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- mwEmbed.js
+++ mwEmbed.js
@@ -1168,10 +1168,10 @@ if( typeof preMwEmbedConfig == 'undefined') {
 	 */
 	mw.isHTML5FallForwardNative = function(){
 		// Check for a mobile html5 user agent:
-		if ( (navigator.userAgent.indexOf('iPhone') != -1) ||
-			(navigator.userAgent.indexOf('iPod') != -1) ||
-			(navigator.userAgent.indexOf('iPad') != -1) ||
-			(navigator.userAgent.indexOf('Android 2.') != -1) ||
+		if ( ( mw.isIphone() ) ||
+			( mw.isIpod() ) || 
+			( mw.isIpad() ) ||
+			( mw.isAndroid2() ) ||
 			// to debug in chrome / desktop safari
 			(document.URL.indexOf('forceMobileHTML5') != -1 )
 		) {
@@ -1191,9 +1191,14 @@ if( typeof preMwEmbedConfig == 'undefined') {
 					return false;
 				}
 			}
+<<<<<<< .mine
+		}		
+		// For IE: 
+=======
 		}
 
 		// For IE:
+>>>>>>> .r76860
 		var hasObj = true;
 		try {
 			var obj = new ActiveXObject( 'ShockwaveFlash.ShockwaveFlash' );
@@ -1212,12 +1217,22 @@ if( typeof preMwEmbedConfig == 'undefined') {
 		// No video tag or flash, return false ( normal "install flash" user flow )
 		return false;
 	}
+<<<<<<< .mine
+	mw.isIphone = function(){
+		return ( navigator.userAgent.indexOf('iPhone') != -1 );
+	};
+	mw.isIpod = function(){
+		return (  navigator.userAgent.indexOf('iPod') != -1 );
+	};
+	mw.isIpad = function(){
+		return ( navigator.userAgent.indexOf('iPad') != -1 );
+	};
+	// Android 2 has some restrictions vs other mobile platforms 
+=======
 	// Android 2 has some restrictions vs other mobile platforms
-	mw.isAndroid2 = function(){
-		if ( navigator.userAgent.indexOf('Android 2.') != -1) {
-			return true;
-		}
-		return false;
+>>>>>>> .r76860
+	mw.isAndroid2 = function(){		
+		return ( navigator.userAgent.indexOf( 'Android 2.') != -1 );
 	};
 
 	/**
