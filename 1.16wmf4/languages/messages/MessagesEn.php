3baa6439ff9ad406b88a46bef8f3fa9b28571290
werdna <werdna@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2010-12-02 04:42:17 +0000

    Merge r77555 into core. As a bonus, fix the PHP notices that HTMLForm has been spewing on 1.16wmf4 for a few months
    git-svn-id: svn+ssh://svn.wikimedia.org/svnroot/mediawiki/branches/wmf@77557 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- 1.16wmf4/languages/messages/MessagesEn.php
+++ 1.16wmf4/languages/messages/MessagesEn.php
@@ -4236,4 +4236,16 @@ Click "Save" to save your edit.',
 'ajax-remove-category-error'   => 'It was not possible to remove this category.
 This usually occurs when the category has been added to the page in a template.',
 
+## Special:DisableAccount
+'disableaccount-desc' => 'Allows administrators to disable individual accounts.',
+'right-disableaccount' => 'Disable accounts',
+'disableaccount' => 'Disable a user account',
+'disableaccount-user' => 'User name:',
+'disableaccount-confirm' => "Disable this user account.
+The user will not be able to log in, reset their password, or receive email notifications.
+If the user is currently logged in anywhere, they will be immediately logged out.
+''Note that disabling an account is not reversible without system administrator intervention.''",
+'disableaccount-mustconfirm' => 'You must confirm that you wish to disable this account.',
+'disableaccount-nosuchuser' => 'The user account "$1" does not exist.',
+'disableaccount-success' => 'The user account "$1" has been permanently disabled.',
 );
