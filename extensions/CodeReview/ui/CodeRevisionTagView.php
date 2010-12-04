68a196bea913404c8f6a0c098fb38ec80d7519b5
demon <demon@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2010-12-04 19:01:51 +0000

    MFT r77733-77734
    git-svn-id: svn+ssh://svn.wikimedia.org/svnroot/mediawiki/branches/wmf/1.16wmf4@77736 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- extensions/CodeReview/ui/CodeRevisionTagView.php
+++ extensions/CodeReview/ui/CodeRevisionTagView.php
@@ -19,7 +19,9 @@ class SvnRevTagTablePager extends SvnRevTablePager {
 
 	function getQueryInfo() {
 		$info = parent::getQueryInfo();
-		$info['tables'][] = 'code_tags';
+		//Don't change table order, see http://www.mediawiki.org/wiki/Special:Code/MediaWiki/77733
+		//Bug in mysql 4 allowed incorrect table ordering joins to work
+		$info['tables'] = array_merge(  array( 'code_tags' ), $info['tables'] );
 		$info['conds'][] = 'cr_repo_id=ct_repo_id';
 		$info['conds'][] = 'cr_id=ct_rev_id';
 		$info['conds']['ct_tag'] = $this->mTag; // fixme: normalize input?
