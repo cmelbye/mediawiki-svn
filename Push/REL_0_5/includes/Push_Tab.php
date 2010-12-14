efe4a6f557337094f9d42cf044ba479e932df697
jeroendedauw <jeroendedauw@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2010-12-14 19:26:51 +0000

    Tag for version 0.5.
    git-svn-id: svn+ssh://svn.wikimedia.org/svnroot/mediawiki/tags/extensions@78397 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

---
+++ Push/REL_0_5/includes/Push_Tab.php
@@ -0,0 +1,360 @@
+<?php
+
+/**
+ * Statis class with methods to create and handle the push tab.
+ *
+ * @since 0.1
+ *
+ * @file Push_Tab.php
+ * @ingroup Push
+ *
+ * @author Jeroen De Dauw < jeroendedauw@gmail.com >
+ */
+final class PushTab {
+	
+	/**
+	 * Adds an "action" (i.e., a tab) to allow pushing the current article.
+	 */
+	public static function displayTab( $obj, &$content_actions ) {
+		global $wgUser, $egPushTargets;
+		
+		// Make sure that this is not a special page, the page has contents, and the user can push.
+		if (isset( $obj->mTitle ) 
+			&& $obj->mTitle->getNamespace() != NS_SPECIAL
+			&& $obj->mTitle->exists()
+			&& $wgUser->isAllowed( 'push' )
+			&& count( $egPushTargets ) > 0 ) {
+				
+			global $wgRequest;
+			
+			$content_actions['push'] = array(
+				'text' => wfMsg( 'push-tab-text' ),
+				'class' => $wgRequest->getVal( 'action' ) == 'push' ? 'selected' : '',
+				'href' => $obj->mTitle->getLocalURL( 'action=push' )
+			);
+		}
+		
+		return true;
+	}
+
+	/**
+	 * Function currently called only for the 'Vector' skin, added in
+	 * MW 1.16 - will possibly be called for additional skins later
+	 */
+	public static function displayTab2( $obj, &$links ) {
+		global $egPushShowTab;
+		
+		// The old '$content_actions' array is thankfully just a sub-array of this one
+		$views_links = $links[$egPushShowTab ? 'views' : 'actions'];
+		self::displayTab( $obj, $views_links );
+		$links[$egPushShowTab ? 'views' : 'actions'] = $views_links;
+		
+		return true;
+	}
+
+	/**
+	 * Handle actions not known to MediaWiki. If the action is push,
+	 * display the push page by calling the displayPushPage method.
+	 *  
+	 * @param string $action
+	 * @param Article $article
+	 * 
+	 * @return true
+	 */
+	public static function onUnknownAction( $action, Article $article ) {
+		if ( $action == 'push' ) {
+			return self::displayPushPage( $article );
+		}
+		else {
+			return true;
+		}
+	}
+	
+	/**
+	 * Loads the needed JavaScript.
+	 * Takes care of non-RL compatibility.
+	 * 
+	 * @since 0.1
+	 */
+	protected static function loadJs() {
+		global $wgOut;
+		
+		// For backward compatibility with MW < 1.17.
+		if ( is_callable( array( $wgOut, 'addModules' ) ) ) {
+			$wgOut->addModules( 'ext.push.tab' );
+		}
+		else {
+			global $egPushScriptPath;
+			
+			PushFunctions::addJSLocalisation();
+			
+			$wgOut->includeJQuery();
+			
+			$wgOut->addHeadItem(
+				'ext.push.tab',
+				Html::linkedScript( $egPushScriptPath . '/includes/ext.push.tab.js' )
+			);
+		}		
+	}
+	
+	/**
+	 * The function called if we're in index.php (as opposed to one of the
+	 * special pages)
+	 * 
+	 * @since 0.1
+	 */
+	public static function displayPushPage( Article $article ) {
+		global $wgOut, $wgUser, $wgTitle, $wgSitename, $egPushTargets;
+		
+		$wgOut->setPageTitle( wfMsgExt( 'push-tab-title', 'parsemag', $article->getTitle()->getText() ) );
+		
+		if ( !$wgUser->isAllowed( 'push' ) ) {
+			$wgOut->permissionRequired( 'push' );
+			return false;
+		}		
+		
+		$wgOut->addHTML( '<p>' . htmlspecialchars( wfMsg( 'push-tab-desc'  ) ) . '</p>' );
+		
+		if ( count( $egPushTargets ) == 0 ) {
+			$wgOut->addHTML( '<p>' . htmlspecialchars( wfMsg( 'push-tab-no-targets'  ) ) . '</p>' );
+			return false;
+		}
+		
+		self::loadJs();
+		
+		$wgOut->addHTML(
+			Html::hidden( 'pageName', $wgTitle->getFullText(), array( 'id' => 'pageName' ) ) .
+			Html::hidden( 'siteName', $wgSitename, array( 'id' => 'siteName' ) )
+		);
+		
+		self::displayPushList();
+		
+		self::displayPushOptions();
+		
+		return false;
+	}
+	
+	/**
+	 * Displays a list with all targets to which can be pushed.
+	 * 
+	 * @since 0.1
+	 */
+	protected static function displayPushList() {
+		global $wgOut, $egPushTargets, $wgLang;
+		
+		$items = array(
+			Html::rawElement(
+				'tr',
+				array(),
+				Html::element(
+					'th',
+					array( 'width' => '200px' ),
+					wfMsg( 'push-targets' )
+				) .
+				Html::element(
+					'th',
+					array( 'style' => 'min-width:400px;' ),
+					wfMsg( 'push-remote-pages' )
+				) .
+				Html::element(
+					'th',
+					array( 'width' => '125px' ),
+					''
+				)
+			)
+		);
+		
+		foreach ( $egPushTargets as $name => $url ) {
+			$items[] = self::getPushItem( $name, $url );
+		}
+		
+		// If there is more then one item, display the 'push all' row.
+		if ( count( $egPushTargets ) > 1 ) {
+			$items[] = Html::rawElement(
+				'tr',
+				array(),
+				Html::element(
+					'th',
+					array( 'colspan' => 2, 'style' => 'text-align: left' ),
+					wfMsgExt( 'push-targets-total', 'parsemag', $wgLang->formatNum( count( $egPushTargets ) ) )
+				) .
+				Html::rawElement(
+					'th',
+					array( 'width' => '125px' ),
+					Html::element(
+						'button',
+						array(
+							'id' => 'push-all-button',
+							'style' => 'width: 125px; height: 30px',
+						),
+						wfMsg( 'push-button-all' )
+					)				
+				)
+			);			
+		}
+		
+		$wgOut->addHtml(
+			Html::rawElement(
+				'table',
+				array( 'class' => 'wikitable', 'width' => '50%' ),
+				implode( "\n", $items )
+			)
+		);
+	}
+	
+	/**
+	 * Returns the HTML for a single push target.
+	 * 
+	 * @since 0.1
+	 * 
+	 * @param string $name
+	 * @param string $url
+	 * 
+	 * @return string
+	 */
+	protected static function getPushItem( $name, $url ) {
+		global $wgTitle;
+		
+		static $targetId = 0;
+		$targetId++;
+		
+		return Html::rawElement(
+			'tr',
+			array(),
+			Html::element(
+				'td',
+				array(),
+				$name
+			) .
+			Html::rawElement(
+				'td',
+				array( 'height' => '45px' ),
+				Html::element(
+					'a',
+					array(
+						'href' => $url . '/index.php?title=' . $wgTitle->getFullText(),
+						'rel' => 'nofollow',
+						'id' => 'targetlink' . $targetId
+					),
+					wfMsgExt( 'push-remote-page-link', 'parsemag', $wgTitle->getFullText(), $name ) 
+				) . 
+				Html::element(
+					'div',
+					array(
+						'id' => 'targetinfo' . $targetId,
+						'style' => 'display:none; color:darkgray'
+					)
+				) .
+				Html::element(
+					'div',
+					array(
+						'id' => 'targeterrors' . $targetId,
+						'style' => 'display:none; color:darkred'
+					)
+				)				
+			) .	
+			Html::rawElement(
+				'td',
+				array(),
+				Html::element(
+					'button',
+					array(
+						'class' => 'push-button',
+						'pushtarget' => $url,
+						'style' => 'width: 125px; height: 30px',
+						'targetid' => $targetId,
+						'targetname' => $name
+					),
+					wfMsg( 'push-button-text' )
+				)
+			)
+		);
+	}
+	
+	/**
+	 * Outputs the HTML for the push options.
+	 * 
+	 * @since 0.4
+	 */
+	protected static function displayPushOptions() {
+		global $wgOut, $wgUser;
+		
+		$wgOut->addHTML( '<h3>' . htmlspecialchars( wfMsg( 'push-tab-push-options' ) ) . '</h3>' );
+		
+		self::displayIncTemplatesOption();
+		
+		if ( $wgUser->isAllowed( 'filepush' ) ) {
+			self::displayIncFilesOption();
+		}
+	}
+	
+	/**
+	 * Outputs the HTML for the "include templates" option.
+	 * 
+	 * @since 0.4
+	 */
+	protected static function displayIncTemplatesOption() {
+		global $wgOut, $wgTitle, $wgLang, $egPushIncTemplates;
+		
+		$usedTemplates = array_keys(
+			PushFunctions::getTemplates(
+				array( $wgTitle->getFullText() ),
+				array( $wgTitle->getFullText() => true )
+			)
+		);
+		
+		array_shift( $usedTemplates );
+
+		$wgOut->addInlineScript(
+			'var wgPushTemplates = ' . json_encode( $usedTemplates ) . ';'
+		);				
+		
+		foreach ( $usedTemplates as &$template ) {
+			$template = "[[$template]]";
+		}
+
+		$wgOut->addHTML(
+			Html::rawElement(
+				'div',
+				array( 'id' => 'divIncTemplates', 'style' => 'display: table-cell' ),
+				Xml::check( 'checkIncTemplates', $egPushIncTemplates, array( 'id' => 'checkIncTemplates' ) ) .
+				Html::element(
+					'label',
+					array( 'id' => 'lblIncTemplates', 'for' => 'checkIncTemplates' ),
+					wfMsg( 'push-tab-inc-templates' )
+				) .		
+				'&#160;' . 
+				Html::rawElement(
+					'div',
+					array( 'style' => 'display:inline; opacity:0', 'id' => 'txtTemplateList' ),
+					count( $usedTemplates ) > 0 ?
+						 wfMsgExt( 'push-tab-used-templates', 'parseinline', $wgLang->listToText( $usedTemplates ), count( $usedTemplates ) ) :
+						 htmlspecialchars( wfMsg( 'push-tab-no-used-templates' ) )
+				)				
+			)
+		);		
+	}
+	
+	/**
+	 * Outputs the HTML for the "include files" option.
+	 * 
+	 * @since 0.4
+	 */	
+	protected static function displayIncFilesOption() {
+		global $wgOut, $wgTitle, $egPushIncFiles;
+		
+		$wgOut->addHTML(
+			Html::rawElement(
+				'div',
+				array( 'id' => 'divIncFiles' ),
+				Xml::check( 'checkIncFiles', $egPushIncFiles, array( 'id' => 'checkIncFiles' ) ) .
+				Html::element(
+					'label',
+					array( 'id' => 'lblIncFiles', 'for' => 'checkIncFiles' ),
+					wfMsg( 'push-tab-inc-files' )
+				) 			
+			)
+		);			
+	}
+	
+}
