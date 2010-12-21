6b6e502910ecb8025592629655a540b5fb6b7be7
reedy <reedy@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2010-12-21 16:22:15 +0000

    Fixup wrong table alias syntax
    Add some braces, fixup spacing
    
    
    git-svn-id: svn+ssh://svn.wikimedia.org/svnroot/mediawiki/branches/querypage-work2@78688 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- phase3/includes/specials/SpecialDisambiguations.php
+++ phase3/includes/specials/SpecialDisambiguations.php
@@ -35,21 +35,20 @@ class DisambiguationsPage extends PageQueryPage {
 	function isExpensive() { return true; }
 	function isSyndicated() { return false; }
 
-
 	function getPageHeader() {
 		return wfMsgExt( 'disambiguations-text', array( 'parse' ) );
 	}
 
 	function getQueryInfo() {
 		$dbr = wfGetDB( DB_SLAVE );
-		$dMsgText = wfMsgForContent('disambiguationspage');
+		$dMsgText = wfMsgForContent( 'disambiguationspage' );
 		$linkBatch = new LinkBatch;
 
 		# If the text can be treated as a title, use it verbatim.
 		# Otherwise, pull the titles from the links table
 		$dp = Title::newFromText($dMsgText);
 		if( $dp ) {
-			if($dp->getNamespace() != NS_TEMPLATE) {
+			if( $dp->getNamespace() != NS_TEMPLATE ) {
 				# FIXME we assume the disambiguation message is a template but
 				# the page can potentially be from another namespace :/
 				wfDebug("Mediawiki:disambiguationspage message does not refer to a template!\n");
@@ -61,8 +60,10 @@ class DisambiguationsPage extends PageQueryPage {
 				$res = $dbr->select(
 					array('pagelinks', 'page'),
 					'pl_title',
-					array('page_id = pl_from', 'pl_namespace' => NS_TEMPLATE,
-						'page_namespace' => $disPageObj->getNamespace(), 'page_title' => $disPageObj->getDBkey()),
+					array('page_id = pl_from',
+						'pl_namespace' => NS_TEMPLATE,
+						'page_namespace' => $disPageObj->getNamespace(),
+						'page_title' => $disPageObj->getDBkey()),
 					__METHOD__ );
 
 				foreach ( $res as $row ) {
@@ -79,7 +80,7 @@ class DisambiguationsPage extends PageQueryPage {
 		
 		// FIXME: What are pagelinks and p2 doing here?
 		return array (
-			'tables' => array( 'templatelinks', 'page AS p1', 'pagelinks', 'page AS p2' ),
+			'tables' => array( 'templatelinks', 'p1' => 'page', 'pagelinks', 'p2' => 'page' ),
 			'fields' => array( 'p1.page_namespace AS namespace',
 					'p1.page_title AS title',
 					'pl_from AS value' ),
@@ -105,14 +106,16 @@ class DisambiguationsPage extends PageQueryPage {
 	 */
 	function preprocessResults( $db, $res ) {
 		$batch = new LinkBatch;
-		while ( $row = $db->fetchObject( $res ) )
+		foreach ( $res as $row ) {
 			$batch->add( $row->namespace, $row->title );
+		}
 		$batch->execute();
 
 		// Back to start for display
-		if ( $db->numRows( $res ) > 0 )
+		if ( $db->numRows( $res ) > 0 ) {
 			// If there are no rows we get an error seeking.
 			$db->dataSeek( $res, 0 );
+		}
 	}
 
 
@@ -122,7 +125,8 @@ class DisambiguationsPage extends PageQueryPage {
 		$dp = Title::makeTitle( $result->namespace, $result->title );
 
 		$from = $skin->link( $title );
-		$edit = $skin->link( $title, wfMsgExt( 'parentheses', array( 'escape' ), wfMsg( 'editlink' ) ) , array(), array( 'redirect' => 'no', 'action' => 'edit' ) );
+		$edit = $skin->link( $title, wfMsgExt( 'parentheses', array( 'escape' ), wfMsg( 'editlink' ) ) ,
+			array(), array( 'redirect' => 'no', 'action' => 'edit' ) );
 		$arr  = $wgContLang->getArrow();
 		$to   = $skin->link( $dp );
 
