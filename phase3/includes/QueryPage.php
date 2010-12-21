fa9c07a1cf4455d1ec7548bd17ff247fc861a782
reedy <reedy@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2010-12-21 21:16:09 +0000

    Adding deprecated since 1.18 (just for ease of reference)
    git-svn-id: svn+ssh://svn.wikimedia.org/svnroot/mediawiki/branches/querypage-work2@78749 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- phase3/includes/QueryPage.php
+++ phase3/includes/QueryPage.php
@@ -143,7 +143,7 @@ abstract class QueryPage extends SpecialPage {
 	 * For back-compat, subclasses may return a raw SQL query here, as a string.
 	 * This is stronly deprecated; getQueryInfo() should be overridden instead.
 	 * @return string
-	 * @deprecated
+	 * @deprecated since 1.18
 	 */
 	function getSQL() {
 		throw new MWException( "Bug in a QueryPage: doesn't implement getQueryInfo() nor getQuery() properly" );
