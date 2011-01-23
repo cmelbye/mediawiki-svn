cmelbye/mediawiki-svn

commit bff93a27b7656a60ad79043b27341447bf05942d

[CodeReview] Modified CodeRepository::getDiff() so it now returns a bit more information about problems that are encountered.  This means we can give more meaningful output in svnImport.php (which I have done) and probably elsewhere as well (which I have not).  Previously the function returned the diff as a string, or an empty string (or maybe null) if an error occurred.  Now it returns a string containing the diff on success, or an integer value indicating the type of error on failure.  These values are defined by the DIFFRESULT_* constants.  All code that calls this function has been updated to handle these new return values.
Note that getDiff() used to call $revision->isDiffable(), but this just returns a boolean, without indicating why it is not diffable.  I therefore had to semi-duplicate this code so we can report the reason properly  (i.e. whether because there are no paths, or too many paths).

git-svn-id: svn+ssh://svn.wikimedia.org/svnroot/mediawiki/trunk@80691 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

extensions/CodeReview/backend/CodeRepository.php
@@ -259,29 +259,54 @@ class CodeRepository {
 	 * @param int $rev Revision ID
 	 * @param $useCache 'skipcache' to avoid caching
 	 *                   'cached' to *only* fetch if cached
+	 * @return string|int The diff text on success, a DIFFRESULT_* constant on failure.
 	 */
 	public function getDiff( $rev, $useCache = '' ) {
-		global $wgMemc;
+		global $wgMemc, $wgCodeReviewMaxDiffPaths;
 		wfProfileIn( __METHOD__ );
+
+		$data = null;
 
 		$rev1 = $rev - 1;
 		$rev2 = $rev;
 
+	// Check that a valid revision was specified.
 		$revision = $this->getRevision( $rev );
-		if ( $revision == null || !$revision->isDiffable() ) {
+		if ( $revision == null ) {
+			$data = DIFFRESULT_BadRevision;
+		}
+	// Check that there is at least one, and at most $wgCodeReviewMaxDiffPaths
+	// paths changed in this revision.
+		else {
+			$paths = $revision->getModifiedPaths();
+			if ( !$paths->numRows() ) {
+				$data = DIFFRESULT_NothingToCompare;
+			}
+			elseif ( $wgCodeReviewMaxDiffPaths > 0 && $paths->numRows() > $wgCodeReviewMaxDiffPaths ) {
+				$data = DIFFRESULT_TooManyPaths;
+			}
+		}
+	
+	// If an error has occurred, return it.
+		if ( $data !== null ) {
 			wfProfileOut( __METHOD__ );
-			return false;
+			return $data;
 		}
 
-		# Try memcached...
+	// Set up the cache key, which will be used both to check if already in the
+	// cache, and to write the final result to the cache.
 		$key = wfMemcKey( 'svn', md5( $this->path ), 'diff', $rev1, $rev2 );
+
+	// If not set to explicitly skip the cache, get the current diff from memcached
+	// directly.
 		if ( $useCache === 'skipcache' ) {
 			$data = null;
 		} else {
 			$data = $wgMemc->get( $key );
 		}
 
-		# Try the database...
+	// If the diff hasn't already been retrieved from the cache, see if we can get 
+	// it from the DB.
 		if ( !$data && $useCache != 'skipcache' ) {
 			$dbr = wfGetDB( DB_SLAVE );
 			$row = $dbr->selectRow( 'code_rev',
@@ -292,7 +317,7 @@ class CodeRepository {
 			if ( $row ) {
 				$flags = explode( ',', $row->cr_flags );
 				$data = $row->cr_diff;
-				// If the text was fetched without an error, convert it
+			// If the text was fetched without an error, convert it
 				if ( $data !== false && in_array( 'gzip', $flags ) ) {
 					# Deal with optional compression of archived pages.
 					# This can be done periodically via maintenance/compressOld.php, and
@@ -302,22 +327,36 @@ class CodeRepository {
 			}
 		}
 
-		# Generate the diff as needed...
+	// If the data was not already in the cache or in the DB, we need to retrieve
+	// it from SVN.
 		if ( !$data && $useCache !== 'cached' ) {
 			$svn = SubversionAdaptor::newFromRepo( $this->path );
 			$data = $svn->getDiff( '', $rev1, $rev2 );
+
+		// If $data is blank, report the error that no data was returned.
+		// TODO: Currently we can't tell the difference between an SVN/connection
+		//		 failure and an empty diff.  See if we can remedy this!
+			if ($data == "") {
+				$data = DIFFRESULT_NoDataReturned;
+			}
+		// Otherwise, store the resulting diff to both the temporary cache and
+		// permanent DB storage.
+			else {
 			// Store to cache
-			$wgMemc->set( $key, $data, 3600 * 24 * 3 );
+				$wgMemc->set( $key, $data, 3600 * 24 * 3 );
+
 			// Permanent DB storage
-			$storedData = $data;
-			$flags = Revision::compressRevisionText( $storedData );
-			$dbw = wfGetDB( DB_MASTER );
-			$dbw->update( 'code_rev',
-				array( 'cr_diff' => $storedData, 'cr_flags' => $flags ),
-				array( 'cr_repo_id' => $this->id, 'cr_id' => $rev ),
-				__METHOD__
-			);
+				$storedData = $data;
+				$flags = Revision::compressRevisionText( $storedData );
+				$dbw = wfGetDB( DB_MASTER );
+				$dbw->update( 'code_rev',
+					array( 'cr_diff' => $storedData, 'cr_flags' => $flags ),
+					array( 'cr_repo_id' => $this->id, 'cr_id' => $rev ),
+					__METHOD__
+				);
+			}
 		}
+
 		wfProfileOut( __METHOD__ );
 		return $data;
 	}

extensions/CodeReview/ui/CodeRevisionView.php
@@ -365,7 +365,7 @@ class CodeRevisionView extends CodeView {
 			$cache = '';
 		}
 		$diff = $this->mRepo->getDiff( $this->mRev->getId(), $cache );
-		if ( !$diff && $deferDiffs ) {
+		if ( is_integer($diff) && $deferDiffs ) {
 			// We'll try loading it by AJAX...
 			return $this->stubDiffLoader();
 		} elseif ( strlen( $diff ) > $wgCodeReviewMaxDiffSize ) {

extensions/CodeReview/CodeReview.php
@@ -114,6 +114,12 @@ $wgGroupPermissions['user']['codereview-associate'] = true;
 
 $wgGroupPermissions['steward']['repoadmin'] = true; // temp
 
+// Constants returned from CodeRepository::getDiff() when no diff can be calculated.
+define("DIFFRESULT_BadRevision",        0);
+define("DIFFRESULT_NothingToCompare",   1);
+define("DIFFRESULT_TooManyPaths",       2);
+define("DIFFRESULT_NoDataReturned",     3);
+
 // If you can't directly access the remote SVN repo, you can set this
 // to an offsite proxy running this fun little proxy tool:
 // http://svn.wikimedia.org/viewvc/mediawiki/trunk/tools/codereview-proxy/

extensions/CodeReview/api/ApiCodeDiff.php
@@ -40,7 +40,7 @@ class ApiCodeDiff extends ApiBase {
 
 		$diff = $repo->getDiff( $params['rev'] );
 
-		if ( strval( $diff ) === '' ) {
+		if ( !is_string( $diff ) ) {
 			// FIXME: Are we sure we don't want to throw an error here?
 			$html = 'Failed to load diff.';
 		} elseif ( strlen( $diff ) > $wgCodeReviewMaxDiffSize ) {

extensions/CodeReview/svnImport.php
@@ -50,7 +50,7 @@ class SvnImport extends Maintenance {
 	 * @param $start Int Revision to begin the import from (Default: null, means last stored revision);
 	 */
 	private function importRepo( $repoName, $start = null, $cacheSize = 0 ) {
-		global $wgCodeReviewImportBatchSize;
+		global $wgCodeReviewImportBatchSize, $wgCodeReviewMaxDiffPaths;
 
 		$repo = CodeRepository::newFromName( $repoName );
 
@@ -136,6 +136,14 @@ class SvnImport extends Maintenance {
 				$options['LIMIT'] = $cacheSize;
 			}
 
+		// Get all rows for this repository that don't already have a diff filled in.  
+		// This is LIMITed according to the $cacheSize setting, above, so only the 
+		// rows that we plan to pre-cache are returned.
+		// TODO: This was optimised in order to skip rows that already have a diff, 
+		//		 which is mostly what is required, but there may be situations where 
+		//		 you want to re-calculate diffs (e.g. if $wgCodeReviewMaxDiffPaths
+		//		 changes).  If these situations arise we will either want to revert
+		//		 this behaviour, or add a --force flag or something.
 			$res = $dbw->select( 'code_rev', 'cr_id',
 				array( 'cr_repo_id' => $repo->getId(), 'cr_diff IS NULL OR cr_diff = ""' ),
 				__METHOD__,
@@ -143,8 +151,33 @@ class SvnImport extends Maintenance {
 			);
 			foreach ( $res as $row ) {
 				$repo->getRevision( $row->cr_id );
-				$repo->getDiff( $row->cr_id ); // trigger caching
-				$this->output( "Diff r{$row->cr_id} done\n" );
+				$diff = $repo->getDiff( $row->cr_id ); // trigger caching
+				$msg = "Diff r{$row->cr_id} ";
+				if (is_integer($diff)) {
+					$msg .= "Skipped: ";
+					switch ($diff) {
+						case DIFFRESULT_BadRevision:
+							$msg .= "Bad revision";
+							break;
+						case DIFFRESULT_NothingToCompare:
+							$msg .= "Nothing to compare";
+							break;
+						case DIFFRESULT_TooManyPaths:
+							$msg .= "Too many paths (\$wgCodeReviewMaxDiffPaths = " 
+							      . $wgCodeReviewMaxDiffPaths . ")";
+							break;
+						case DIFFRESULT_NoDataReturned:
+							$msg .= "No data returned - no diff data, or connection lost.";
+							break;
+						default:
+							$msg .= "Unknown reason!";
+							break;
+					}
+				}
+				else {
+					$msg .= "done";
+				}
+				$this->output( $msg . "\n" );
 			}
 		}
 		else {

commit 273ff37583f3ea8069385e8493b89a091df0609a

Fix for CodeReview on-demand AJAX diff loading; regression caused by refactoring in r80691.
This adds an is_null() check alongside the is_integer() check; CodeRepository::getDiff() currently returns null, not an integer result code, when no cached diff is available and we're preferring cached data only.




git-svn-id: svn+ssh://svn.wikimedia.org/svnroot/mediawiki/trunk@80794 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

extensions/CodeReview/backend/CodeRepository.php
@@ -260,6 +260,9 @@ class CodeRepository {
 	 * @param $useCache 'skipcache' to avoid caching
 	 *                   'cached' to *only* fetch if cached
 	 * @return string|int The diff text on success, a DIFFRESULT_* constant on failure.
+	 * @fixme Actually returns null if $useCache='cached' and there's no cached
+	 *        data. Either add a relevant constant or fix the comment above;
+	 *        caller in CodeRevisionView fixed by adding is_null check.
 	 */
 	public function getDiff( $rev, $useCache = '' ) {
 		global $wgMemc, $wgCodeReviewMaxDiffPaths;

extensions/CodeReview/ui/CodeRevisionView.php
@@ -365,7 +365,7 @@ class CodeRevisionView extends CodeView {
 			$cache = '';
 		}
 		$diff = $this->mRepo->getDiff( $this->mRev->getId(), $cache );
-		if ( is_integer($diff) && $deferDiffs ) {
+		if ( (is_null($diff) || is_integer($diff)) && $deferDiffs ) {
 			// We'll try loading it by AJAX...
 			return $this->stubDiffLoader();
 		} elseif ( strlen( $diff ) > $wgCodeReviewMaxDiffSize ) {