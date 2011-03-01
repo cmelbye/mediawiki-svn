57b8bbae2a4db99a7ebce68611a1c2a6ad112e68
tstarling <tstarling@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-03-01 12:52:02 +0000

    Revert r81741 (temporary 1.17 transition workaround to avoid parser cache clear). Was broken, see CR.
    git-svn-id: svn+ssh://svn.wikimedia.org/svnroot/mediawiki/branches/wmf@83013 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- 1.17wmf1/includes/parser/ParserCache.php
+++ 1.17wmf1/includes/parser/ParserCache.php
@@ -136,11 +136,6 @@ class ParserCache {
 
 		$value = $this->mMemc->get( $parserOutputKey );
 		if ( !$value ) {
-			wfDebug( "New format parser cache miss.\n" );
-			$parserOutputKey = $this->getParserOutputKey( $article, ParserOptions::legacyOptions() );
-			$value = $this->mMemc->get( $parserOutputKey );
-		}
-		if ( !$value ) {
 			wfDebug( "Parser cache miss.\n" );
 			wfIncrStats( "pcache_miss_absent" );
 			wfProfileOut( __METHOD__ );
