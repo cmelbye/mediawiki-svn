3e0f5e1027494fef3ab06ed9b7789a07a61b598e
jeroendedauw <jeroendedauw@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-03-05 20:37:47 +0000

    Tag for version 0.3.
    git-svn-id: svn+ssh://svn.wikimedia.org/svnroot/mediawiki/tags/extensions@83328 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

---
+++ SubPageList/REL_0_3/SubPageList.i18n.php
@@ -0,0 +1,285 @@
+<?php
+
+/**
+ * Internationalization file for the SubPageList extension.
+ *
+ * @file SubPageList.i18n.php
+ * @ingroup SPL
+ *
+ * @licence GNU GPL v3 or later
+ *
+ * @author Jeroen De Dauw < jeroendedauw@gmail.com >
+ */
+
+$messages = array();
+
+/** English
+ * @author Jeroen De Dauw
+ */
+$messages['en'] = array(
+	'spl-desc' => 'Adds a <code><nowiki><splist /></nowiki></code> tag that enables you to list subpages',
+
+	'spl-nosubpages' => '$1 has no subpages to list.',
+
+	'spl-subpages-par-sort' => 'The direction to sort in.',
+	'spl-subpages-par-sortby' => 'What to sort the subpages by.',
+	'spl-subpages-par-format' => 'The subpage list can be displayed in several formats. Numbered lists (ol), bulleted lists (ul) and comma-separated lists (list).',
+	'spl-subpages-par-page' => 'The page to show the subpages for. Defaults to the current page.',
+	'spl-subpages-par-showpage' => 'Indicates if the page itself should be shown in the list or not.',
+	'spl-subpages-par-pathstyle' => 'The style of the path for subpages in the list.',
+	'spl-subpages-par-kidsonly' => 'Allows showing only direct subpages.',
+	'spl-subpages-par-limit' => 'The maximum number of pages to list.',
+);
+
+/** Message documentation (Message documentation)
+ * @author Purodha
+ */
+$messages['qqq'] = array(
+	'spl-subpages-par-format' => 'I believe, that the brackettet words should not be translated. --[[User:Purodha|Purodha Blissenbach]] 00:04, 21 January 2011 (UTC)',
+);
+
+/** Belarusian (Taraškievica orthography) (‪Беларуская (тарашкевіца)‬)
+ * @author EugeneZelenko
+ * @author Jim-by
+ * @author Wizardist
+ */
+$messages['be-tarask'] = array(
+	'spl-desc' => 'Дадае тэг <code><nowiki><splist /></nowiki></code>, які выводзіць сьпіс падстаронак',
+	'spl-nosubpages' => '$1 ня мае падстаронак.',
+	'spl-subpages-par-sort' => 'Напрамак сартаваньня.',
+	'spl-subpages-par-sortby' => 'Як сартаваць падстаронкі.',
+	'spl-subpages-par-format' => 'Сьпіс падстаронак можа быць паказаны ў некалькіх фарматах. Нумараваныя сьпісы (ol), маркіраваныя сьпісы (ul) і сьпісы падзеленыя коскамі (list).',
+	'spl-subpages-par-page' => 'Старонка для якой паказваць сьпіс падстаронак. Па змоўчваньні цяперашняя старонка.',
+	'spl-subpages-par-showpage' => 'Паказвае, ці павінна паказвацца старонка ў сьпісе.',
+	'spl-subpages-par-pathstyle' => 'Стыль шляху для падстаронак у сьпісе.',
+	'spl-subpages-par-kidsonly' => 'Паказваць толькі прамыя падстаронкі.',
+	'spl-subpages-par-limit' => 'Максымальная колькасьць старонак для паказу.',
+);
+
+/** Breton (Brezhoneg)
+ * @author Y-M D
+ */
+$messages['br'] = array(
+	'spl-nosubpages' => "N'eus ispajenn ebet da rollañ evit $1",
+);
+
+/** Bosnian (Bosanski)
+ * @author CERminator
+ */
+$messages['bs'] = array(
+	'spl-desc' => 'Dodaje <code><nowiki><splist /></nowiki></code> oznaku koja vam omogućuje da pregledate podstranice',
+	'spl-nosubpages' => '$1 nema podstranica za prikaz.',
+	'spl-subpages-par-sort' => 'Smijer za redanje.',
+);
+
+/** German (Deutsch)
+ * @author Kghbln
+ * @author Purodha
+ */
+$messages['de'] = array(
+	'spl-desc' => 'Fügt das <code><nowiki><splist /></nowiki></code>-Tag zur Anzeige von Unterseiten hinzu',
+	'spl-nosubpages' => '$1 hat keine Unterseiten.',
+	'spl-subpages-par-sort' => 'Sortierreihenfolge der Unterseiten',
+	'spl-subpages-par-sortby' => 'Sortierkriterium der Unterseiten',
+	'spl-subpages-par-format' => 'Die Liste der Unterseiten kann in verschiedenen Formaten angezeigt werden, als nummerierte Liste (ol), als Aufzählung (ul) oder als kommagetrennte Liste (Liste).',
+	'spl-subpages-par-page' => 'Die Seite für welche die Unterseiten angezeigt werden sollen. Standardmäßig ist dies die aktuelle Seite.',
+	'spl-subpages-par-showpage' => 'Gibt an, ob die Seite selbst in der Liste ihrer Unterseiten angezeigt werden soll oder nicht.',
+	'spl-subpages-par-pathstyle' => 'Anzeigestil für die Pfade in der Liste angezeigten Unterseiten',
+	'spl-subpages-par-kidsonly' => 'Ermöglicht ausschließlich die Anzeige der direkten Unterseiten.',
+	'spl-subpages-par-limit' => 'Die Höchstzahl der aufzulistenden Unterseiten.',
+);
+
+/** French (Français)
+ * @author Sherbrooke
+ */
+$messages['fr'] = array(
+	'spl-desc' => 'Ajoute une balise <nowiki><splist /></nowiki> qui permet de lister les sous-pages',
+	'spl-nosubpages' => "$1 n'a pas de sous-pages à lister.",
+	'spl-subpages-par-sort' => 'La direction de tri',
+	'spl-subpages-par-sortby' => 'Trie des sous-pages selon.',
+	'spl-subpages-par-format' => 'La liste des sous-pages peut être affichée en différents formats : listes numérotées (ol), listes à puces (ul) ou listes séparées par des virgules (liste).',
+	'spl-subpages-par-page' => 'La page pour voir les sous-pages. Le défaut est la page courante.',
+	'spl-subpages-par-showpage' => 'Indique si la page elle-même doit figurer dans la liste ou non.',
+	'spl-subpages-par-pathstyle' => 'Style du chemin des sous-pages dans la liste.',
+	'spl-subpages-par-kidsonly' => "Permet de n'afficher que les sous-pages immédiates.",
+	'spl-subpages-par-limit' => 'La quantité maximale de pages à lister.',
+);
+
+/** Upper Sorbian (Hornjoserbsce)
+ * @author Michawiki
+ */
+$messages['hsb'] = array(
+	'spl-desc' => 'Přidawa element code><nowiki><splist /></nowiki></code>, kotryž ći zmóžnja podstrony nalistować',
+	'spl-nosubpages' => '$1 nima podstrony.',
+	'spl-subpages-par-sort' => 'Sortěrowanski porjad.',
+	'spl-subpages-par-sortby' => 'Sortěrowanski kriterij podstronow',
+	'spl-subpages-par-format' => 'Lisćina podstronow hodźi so we wšelakich formatach zwobraznić, čisłowane lisćiny (ol), naličenja (ul) a lisćiny dźělene přez komu (lisćina).',
+	'spl-subpages-par-page' => 'Strona, za kotruž maja so podstrony pokazać. Standard je aktualna strona.',
+	'spl-subpages-par-showpage' => 'Podawa, hač strona sama měła so w lisćinje pokazać abo nic.',
+	'spl-subpages-par-pathstyle' => 'Stil šćežki za podstrony w lisćinje.',
+	'spl-subpages-par-kidsonly' => 'Móže  jenož direktne podstrony pokazać.',
+	'spl-subpages-par-limit' => 'Maksimalna ličba stronow, kotrež maja so nalsitować.',
+);
+
+/** Interlingua (Interlingua)
+ * @author McDutchie
+ */
+$messages['ia'] = array(
+	'spl-desc' => 'Adde un etiquetta <code><nowiki><splist /></nowiki></code> que permitte listar subpaginas',
+	'spl-nosubpages' => '$1 non ha subpaginas a listar.',
+	'spl-subpages-par-sort' => 'Le direction in le qual ordinar le subpaginas.',
+	'spl-subpages-par-sortby' => 'Criterio secundo le qual ordinar le subpaginas.',
+	'spl-subpages-par-format' => 'Le lista de subpaginas pote esser presentate in plure formatos: listas a numeros (ol), listas a punctos (ul) e listas separate per commas (list).',
+	'spl-subpages-par-page' => 'Le pagina del qual presentar le subpaginas. Le predefinition es le pagina actual.',
+	'spl-subpages-par-showpage' => 'Indica si le pagina mesme debe figurar in le lista o non.',
+	'spl-subpages-par-pathstyle' => 'Le stilo del cammino pro subpaginas in le lista.',
+	'spl-subpages-par-kidsonly' => 'Permitte monstrar solmente subpaginas directe.',
+	'spl-subpages-par-limit' => 'Le numero maxime de paginas a listar.',
+);
+
+/** Indonesian (Bahasa Indonesia)
+ * @author IvanLanin
+ */
+$messages['id'] = array(
+	'spl-desc' => 'Memberikan tag <code><nowiki><splist /></nowiki></code> yang memungkinkan Anda untuk melihat daftar subhalaman',
+	'spl-nosubpages' => '$1 tidak memiliki subhalaman untuk ditampilkan.',
+	'spl-subpages-par-sort' => 'Arah urutan.',
+	'spl-subpages-par-sortby' => 'Cara pengurutan subhalaman.',
+	'spl-subpages-par-format' => 'Daftar subhalaman dapat ditampilkan dalam berbagai format, yaitu daftar bernomor (ol), daftar butir (ul), dan daftar dipisahkan koma (list).',
+	'spl-subpages-par-page' => 'Halaman yang akan ditampilkan subhalamannya. Setelan bawaan: halaman saat ini.',
+	'spl-subpages-par-showpage' => 'Menunjukkan apakah halaman itu sendiri harus ditampilkan atau tidak dalam daftar.',
+	'spl-subpages-par-pathstyle' => 'Gaya jalur subhalaman dalam daftar.',
+	'spl-subpages-par-kidsonly' => 'Hanya tampilkan subhalaman langsung.',
+	'spl-subpages-par-limit' => 'Jumlah halaman maks. yang ditampilkan.',
+);
+
+/** Colognian (Ripoarisch)
+ * @author Purodha
+ */
+$messages['ksh'] = array(
+	'spl-desc' => 'Deiht dä Befähl <code><nowiki><splist /></nowiki></code> en et Wiki, för Ongersigge aanzezeije.',
+	'spl-nosubpages' => 'De Sigg „$1“ hät kei Ongersigge zom Opleßte.',
+	'spl-subpages-par-sort' => 'Wieröm zoteet wääde sull.',
+	'spl-subpages-par-sortby' => 'Noh wat de Ongersigge zoteet wääde sulle.',
+	'spl-subpages-par-format' => 'De Leß met de Ongersigge kann ongerscheidlijje Jeschtalte han: met Nummere (ol) met Punkte (ul) un alles ein eine Reih met Kommas dozwesche (list)',
+	'spl-subpages-par-page' => 'De Sigg, woh de Ongersigge vun jezeich wääde sulle. Wam_mer nix säät, es dat de Sigg, di jraad jezeich weed.',
+	'spl-subpages-par-showpage' => 'Jitt aan, ov de Sigg selver och en dä Leß met dä iehre Ongersigge aanjezeisch wääde sull, udder nit.',
+	'spl-subpages-par-pathstyle' => 'Dä Stil vun de Aanzeije vun däm Pad vun de Ongersigge en dä Leß.',
+	'spl-subpages-par-kidsonly' => 'Määt et müjjelesch, bloß de diräkte Ongersigge opzeleßte.',
+	'spl-subpages-par-limit' => 'De jrüüßte Zahl Sigge för opzeleste.',
+);
+
+/** Luxembourgish (Lëtzebuergesch)
+ * @author Robby
+ */
+$messages['lb'] = array(
+	'spl-desc' => 'Setzt den <code><nowiki><splist /></nowiki></code>-Tag fir Ënnersäiten ze weisen derbäi',
+	'spl-nosubpages' => '$1 huet keng Ënnersäite fir ze weisen',
+	'spl-subpages-par-sort' => 'Reiefolleg wéi zortéiert soll ginn.',
+	'spl-subpages-par-sortby' => "Wourop d'Ënnersäiten zortéiert ginn:",
+	'spl-subpages-par-format' => 'D\'Lëscht vun den Ënnersäite kann a verschiddene Formater gewise ginn. Numeréiert Lëschten (ol), Lëschte mat Punkten (ul) oder "comma seperated" Lëschten (list).',
+	'spl-subpages-par-page' => "D'Säit fir déi Ënnersäite gewise solle ginn.als Standard ass dat déi aktuell Säit.",
+	'spl-subpages-par-showpage' => "Gëtt un ob d'Säit selwer an der Lëscht gewise soll ginn oder net.",
+	'spl-subpages-par-pathstyle' => 'Styl vum Wee (path) fir Ënnersäiten an der Lëscht.',
+	'spl-subpages-par-kidsonly' => 'Erlaabt fir nëmmen direkt Ënnersäiten ze weisen.',
+	'spl-subpages-par-limit' => "D'Maximalzuel vu Säiten déi gewise ginn.",
+);
+
+/** Macedonian (Македонски)
+ * @author Bjankuloski06
+ */
+$messages['mk'] = array(
+	'spl-desc' => 'Додава ознака <code><nowiki><splist /></nowiki></code> што овозможува наведување на потстраници во список',
+	'spl-nosubpages' => '$1 нема потстраници за наведување.',
+	'spl-subpages-par-sort' => 'Насока на подредување.',
+	'spl-subpages-par-sortby' => 'По што да се подредат потстраниците.',
+	'spl-subpages-par-format' => 'Списокот на потстраници може да се прикаже во неколку формати: со редни броеви (ol), со потточки (ul) одделено со запирка (list).',
+	'spl-subpages-par-page' => 'За која страница да се прикажат потстраниците (по основно - тековната страница).',
+	'spl-subpages-par-showpage' => 'Назначува дали во списокот да се прикаже и самата страница.',
+	'spl-subpages-par-pathstyle' => 'Стилот на патеката за потстраниците во списокот.',
+	'spl-subpages-par-kidsonly' => 'Овозможува приказ само на директни потстраници.',
+	'spl-subpages-par-limit' => 'Максималниот број на страници за наведување во списокот.',
+);
+
+/** Dutch (Nederlands)
+ * @author Siebrand
+ */
+$messages['nl'] = array(
+	'spl-desc' => "Voegt het label <code><nowiki><splist /></nowiki></code> toe dat het mogelijk maakt een lijst met subpagina's weer te geven",
+	'spl-nosubpages' => "$1 heeft geen subpagina's.",
+	'spl-subpages-par-sort' => 'De sorteervolgorde',
+	'spl-subpages-par-sortby' => "Hoe de subpagina's te sorteren.",
+	'spl-subpages-par-format' => "De lijst met subpagina's kan op verschillende manieren weergegeven worden. Als genummerde lijst (ol), ongenummerde lijst (ul) en als door komma's gescheiden lijst (list).",
+	'spl-subpages-par-page' => "De pagina waarvoor subpagina's weergegeven moeten worden. Dit is standaard de huidige pagina.",
+	'spl-subpages-par-showpage' => 'Geeft aan of de pagina zelf weergegeven moet worden in de lijst of niet.',
+	'spl-subpages-par-pathstyle' => "De stijl van het pad voor subpagina's in de lijst.",
+	'spl-subpages-par-kidsonly' => "Maakt het mogelijk om alleen subpagina's van het eerste niveau weer te geven.",
+	'spl-subpages-par-limit' => "Het maximale aantal weer te geven pagina's.",
+);
+
+/** Norwegian (bokmål)‬ (‪Norsk (bokmål)‬)
+ * @author Nghtwlkr
+ */
+$messages['no'] = array(
+	'spl-desc' => 'Legger til et <code><nowiki><splist /></nowiki></code>-element som lar deg liste opp undersider',
+	'spl-nosubpages' => '$1 har ingen undersider å liste opp.',
+	'spl-subpages-par-sort' => 'Retningen du vil sortere i.',
+	'spl-subpages-par-sortby' => 'Hva du vil sortere undersidene etter.',
+	'spl-subpages-par-format' => 'Undersidelisten kan vises i flere format. Nummererte lister (ol), punktlister (ul) og kommaseparerte lister (list).',
+	'spl-subpages-par-page' => 'Siden undersidene skal vises for. Standard er den gjeldende siden.',
+	'spl-subpages-par-showpage' => 'Indikerer om selve siden skal vises i listen eller ikke.',
+	'spl-subpages-par-pathstyle' => 'Stilen på banen for undersidene i listen.',
+	'spl-subpages-par-kidsonly' => 'Tillater kun å vise direkte undersider.',
+	'spl-subpages-par-limit' => 'Maksimum antall sider å liste opp.',
+);
+
+/** Portuguese (Português)
+ * @author Hamilton Abreu
+ */
+$messages['pt'] = array(
+	'spl-desc' => 'Acrescenta um elemento <code><nowiki><splist /></nowiki></code> que permite listar subpáginas',
+	'spl-nosubpages' => '$1 não tem subpáginas para listar.',
+	'spl-subpages-par-sort' => 'A direcção da ordenação.',
+	'spl-subpages-par-sortby' => 'O critério de ordenação.',
+	'spl-subpages-par-format' => 'A lista de subpáginas pode ser apresentada em vários formatos. Listas numeradas (ol), listas com marcadores (ul) e listas separadas por vírgulas (list).',
+	'spl-subpages-par-page' => 'A página que tem as subpáginas. Por omissão, será a página corrente.',
+	'spl-subpages-par-showpage' => 'Indica se a própria página deve ser mostrada na lista ou não.',
+	'spl-subpages-par-pathstyle' => 'O estilo do caminho para as subpáginas na lista.',
+	'spl-subpages-par-kidsonly' => 'Permite mostrar só subpáginas directas.',
+	'spl-subpages-par-limit' => 'O número máximo de páginas listadas.',
+);
+
+/** Russian (Русский)
+ * @author Александр Сигачёв
+ */
+$messages['ru'] = array(
+	'spl-desc' => 'Добавляет тег <code><nowiki><splist></splist></nowiki></code>, выводящий список подстраниц',
+	'spl-nosubpages' => '$1 не имеет подстраниц.',
+	'spl-subpages-par-sort' => 'Направление сортировки.',
+	'spl-subpages-par-sortby' => 'По чему сортировать подстраницы.',
+	'spl-subpages-par-format' => 'Список подстраниц может быть показан в нескольких форматах. Нумерованный список (ol), маркированный список (ul), список через запятые (list).',
+	'spl-subpages-par-page' => 'Страница для которой показывать список подстраниц. По умолчанию текущая страница.',
+	'spl-subpages-par-showpage' => 'Указывает, должна ли отображаться сама страница.',
+	'spl-subpages-par-pathstyle' => 'Стиль пути для подстраниц в списке.',
+	'spl-subpages-par-kidsonly' => 'Показывать только прямые подстраницы.',
+	'spl-subpages-par-limit' => 'Максимальное количество страниц в список.',
+);
+
+/** Serbian Cyrillic ekavian (‪Српски (ћирилица)‬)
+ * @author Rancher
+ */
+$messages['sr-ec'] = array(
+	'spl-subpages-par-limit' => 'Највећи број страница за приказивање.',
+);
+
+/** Ukrainian (Українська)
+ * @author Тест
+ */
+$messages['uk'] = array(
+	'spl-desc' => 'Додає тег <code><nowiki><splist /></nowiki></code>, який виводить список підсторінок',
+	'spl-nosubpages' => '$1 не має підсторінок для складання списку.',
+	'spl-subpages-par-sort' => 'Напрямок сортування.',
+	'spl-subpages-par-page' => 'Сторінка, для якої показати підсторінки. За умовчанням — поточна сторінка.',
+	'spl-subpages-par-limit' => 'Максимальна кількість сторінок у списку.',
+);
+
