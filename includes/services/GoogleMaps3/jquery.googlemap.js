d434cce17f26e673913a03769acaffb23268d095
jeroendedauw <jeroendedauw@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-03-05 01:55:22 +0000

    work on markers and centre support for gmaps3
    git-svn-id: svn+ssh://svn.wikimedia.org/svnroot/mediawiki/branches/Maps0.8@83268 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- includes/services/GoogleMaps3/jquery.googlemap.js
+++ includes/services/GoogleMaps3/jquery.googlemap.js
@@ -1,26 +1,52 @@
+/**
+ * JavasSript for Google Maps v3 maps in the Maps extension.
+ * @see http://www.mediawiki.org/wiki/Extension:Maps
+ * 
+ * @author Jeroen De Dauw <jeroendedauw at gmail dot com>
+ */
+
 (function( $ ){ $.fn.googlemaps = function( options ) {
 
 	var mapOptions = {
-		//disableDefaultUI: true,
+		disableDefaultUI: true,
 		zoom: options.zoom,
 		mapTypeId: eval( options.type ),
 	};
 	
+	// TODO
 	mapOptions.center = new google.maps.LatLng(-34.397, 150.644);
 	
+	// Map controls
 	mapOptions.panControl = $.inArray( 'pan', options.controls ) != -1;
 	mapOptions.zoomControl = $.inArray( 'zoom', options.controls ) != -1;
 	mapOptions.mapTypeControl = $.inArray( 'type', options.controls ) != -1;
 	mapOptions.scaleControl = $.inArray( 'scale', options.controls ) != -1;
 	mapOptions.streetViewControl = $.inArray( 'streetview', options.controls ) != -1;
 	
+	// Map control styles
 	mapOptions.zoomControlOptions = { style: eval( options.zoomstyle ) }
 	mapOptions.mapTypeControlOptions = { style: eval( options.typestyle ) }	
 	
-	
-
+	// Create the map.
 	var map = new google.maps.Map( this.get( 0 ), mapOptions );
 
+	// Add the markers.
+	for ( var i = options.locations.length - 1; i >= 0; i-- ) {
+		var location = options.locations[i];
+		var marker = new google.maps.Marker({
+			map: map,
+			position: new google.maps.LatLng( location.lat , location.lon ),
+			title: location.title
+		});
+	}
+	
+	if ( options.centre === false ) {
+		
+	}
+	else {
+		map.setCenter( new google.maps.LatLng( options.centre.lat , options.centre.lon ) );
+	}
+	
 	return this;
 	
 }; })( jQuery );
