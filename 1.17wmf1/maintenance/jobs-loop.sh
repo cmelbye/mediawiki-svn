ab5d5c9e342d6836a8060650f90ff41688f54c37
tstarling <tstarling@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-03-08 05:44:01 +0000

    Prioritise various types of jobs above refreshLinks2. Committing for test on server.
    git-svn-id: svn+ssh://svn.wikimedia.org/svnroot/mediawiki/branches/wmf@83510 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- 1.17wmf1/maintenance/jobs-loop.sh
+++ 1.17wmf1/maintenance/jobs-loop.sh
@@ -6,28 +6,32 @@ trap 'kill %-; exit' SIGTERM
     type=$1
 }
 
+types="htmlCacheUpdate sendMail enotifNotify uploadFromUrl fixDoubleRedirect"
+
 cd `readlink -f /usr/local/apache/common/php/maintenance`
 while [ 1 ];do
+	# Do the prioritised types
+	for type in $types; do
+		db=`php -n nextJobDB.php --type="$type"`
+		if [ -n "$db" ]; then
+			echo "$db $type"
+			nice -n 20 php runJobs.php --wiki="$db" --procs=4 "$type"
+		fi
+	done
+
+	# Do the remaining types
 	db=
 	while [ -z $db ];do
-                if [ ! -z "$type" ]; then
-                    db=`php -n nextJobDB.php --type=$type`
-                else
-                    db=`php -n nextJobDB.php`
-                fi
+		db=`php -n nextJobDB.php`
 
 		if [ -z $db ];then
 			# No jobs to do, wait for a while
 			echo "No jobs..."
 			sleep 5
+	 	else
+			echo $db
+			nice -n 20 php runJobs.php --wiki="$db" --procs=4
 		fi
 	done
-	echo $db
-        if [ ! -z "$type" ]; then
-            nice -n 20 php runJobs.php $db --procs=4 $type &
-        else
-            nice -n 20 php runJobs.php $db --procs=4 &
-        fi
-	wait
 done
 
