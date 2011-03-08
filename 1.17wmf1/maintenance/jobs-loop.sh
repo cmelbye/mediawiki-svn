b2bc1b54ce3db7ccf1e7eba4cf1c8701d2c1b9ae
tstarling <tstarling@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-03-08 06:06:13 +0000

    * Prioritise even more aggressively * Fixed --type parameter * Fixed non-prioritised jobs, they shouldn't have been in their own loop
    git-svn-id: svn+ssh://svn.wikimedia.org/svnroot/mediawiki/branches/wmf@83511 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- 1.17wmf1/maintenance/jobs-loop.sh
+++ 1.17wmf1/maintenance/jobs-loop.sh
@@ -11,27 +11,29 @@ types="htmlCacheUpdate sendMail enotifNotify uploadFromUrl fixDoubleRedirect"
 cd `readlink -f /usr/local/apache/common/php/maintenance`
 while [ 1 ];do
 	# Do the prioritised types
-	for type in $types; do
-		db=`php -n nextJobDB.php --type="$type"`
-		if [ -n "$db" ]; then
-			echo "$db $type"
-			nice -n 20 php runJobs.php --wiki="$db" --procs=4 "$type"
-		fi
+	moreprio=y
+	while [ -n "$moreprio" ] ; do
+		moreprio=
+		for type in $types; do
+			db=`php -n nextJobDB.php --type="$type"`
+			if [ -n "$db" ]; then
+				echo "$db $type"
+				nice -n 20 php runJobs.php --wiki="$db" --procs=4 --type="$type"
+				moreprio=y
+			fi
+		done
 	done
 
 	# Do the remaining types
-	db=
-	while [ -z $db ];do
-		db=`php -n nextJobDB.php`
+	db=`php -n nextJobDB.php`
 
-		if [ -z $db ];then
-			# No jobs to do, wait for a while
-			echo "No jobs..."
-			sleep 5
-	 	else
-			echo $db
-			nice -n 20 php runJobs.php --wiki="$db" --procs=4
-		fi
-	done
+	if [ -z "$db" ];then
+		# No jobs to do, wait for a while
+		echo "No jobs..."
+		sleep 5
+	else
+		echo "$db"
+		nice -n 20 php runJobs.php --wiki="$db" --procs=4
+	fi
 done
 
