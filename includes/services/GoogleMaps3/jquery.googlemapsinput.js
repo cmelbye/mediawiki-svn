3dbc935b5945c075a42468c014d8c995bf6dbf11
jeroendedauw <jeroendedauw@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-03-09 12:15:06 +0000

    adding js files and copy of old file
    git-svn-id: svn+ssh://svn.wikimedia.org/svnroot/mediawiki/branches/SemanticMaps0.8@83582 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

---
+++ includes/services/GoogleMaps3/jquery.googlemapsinput.js
@@ -0,0 +1,18 @@
+/**
+ * JavasSript for the Google Maps v3 form input of the Semantic Maps extension.
+ * @see http://www.mediawiki.org/wiki/Extension:Semantic_Maps
+ * 
+ * @since 0.8
+ * @ingroup SemanticMaps
+ * 
+ * @licence GNU GPL v3
+ * @author Jeroen De Dauw <jeroendedauw at gmail dot com>
+ */
+
+(function( $ ){ $.fn.googlemapsinput = function( options ) {
+	
+	
+	
+	return this;
+	
+}; })( jQuery );
---
+++ includes/services/OpenLayers/jquery.openlayersinput.js
@@ -0,0 +1,18 @@
+/**
+ * JavasSript for the OpenLayers form input of the Semantic Maps extension.
+ * @see http://www.mediawiki.org/wiki/Extension:Semantic_Maps
+ * 
+ * @since 0.8
+ * @ingroup SemanticMaps
+ * 
+ * @licence GNU GPL v3
+ * @author Jeroen De Dauw <jeroendedauw at gmail dot com>
+ */
+
+(function( $ ){ $.fn.openlayersinput = function( options ) {
+	
+	
+	
+	return this;
+	
+}; })( jQuery );
