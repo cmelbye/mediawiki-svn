494b0a1e1001561a3450a61f9c8b296f25d7fa3e
tstarling <tstarling@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-03-15 02:27:09 +0000

    aggregated profiling
    git-svn-id: svn+ssh://svn.wikimedia.org/svnroot/mediawiki/branches/wmf@83990 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- 1.17wmf1/StartProfiler.php
+++ 1.17wmf1/StartProfiler.php
@@ -27,9 +27,14 @@ if ( @$_SERVER['REQUEST_URI'] == '/w/index.php?title=United_States&action=submit
   	require_once( dirname(__FILE__).'/includes/ProfilerSimpleUDP.php' );
 	$wgProfiler = new ProfilerSimpleUDP;
 	$wgProfiler->setProfileID( 'thumb' );
+} elseif ( $host == 'test2.wikipedia.org' ) {
+  	require_once( dirname(__FILE__).'/includes/ProfilerSimpleUDP.php' );
+	$wgProfiler = new ProfilerSimpleUDP;
+	$wgProfiler->setProfileID( 'test2' );
 } elseif ( !( $rand % 50 ) ) {
   	require_once( dirname(__FILE__).'/includes/ProfilerSimpleUDP.php' );
 	$wgProfiler = new ProfilerSimpleUDP;
+	/*
 	if ( $host == 'en.wikipedia.org' ) {
 		$wgProfiler->setProfileID( 'enwiki' );
 	} elseif ( $host == 'de.wikipedia.org' ) {
@@ -40,8 +45,12 @@ if ( @$_SERVER['REQUEST_URI'] == '/w/index.php?title=United_States&action=submit
 		$wgProfiler->setProfileID( 'flaggedrevs' );
 	} else {
 		$wgProfiler->setProfileID( 'others' );
+	}*/
+	if ( php_sapi_name() == 'cli' ) {
+		$wgProfiler->setProfileID( 'cli' );
+	} else {
+		$wgProfiler->setProfileID( 'all' );
 	}
-	#$wgProfiler->setProfileID( 'all' );
 	#$wgProfiler->setMinimum(5 /* seconds */);
 }
 elseif ( defined( 'MW_FORCE_PROFILE' ) ) {
