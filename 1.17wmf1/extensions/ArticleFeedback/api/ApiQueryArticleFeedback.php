bfb5d964dfc6ceb075d2bacf952e45fc2fe1b654
catrope <catrope@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-03-17 22:26:29 +0000

    1.17wmf1: MFT r84059, r84208
    git-svn-id: svn+ssh://svn.wikimedia.org/svnroot/mediawiki/branches/wmf@84210 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- 1.17wmf1/extensions/ArticleFeedback/api/ApiQueryArticleFeedback.php
+++ 1.17wmf1/extensions/ArticleFeedback/api/ApiQueryArticleFeedback.php
@@ -103,11 +103,12 @@ class ApiQueryArticleFeedback extends ApiQueryBase {
 			$ratings[$pageId]['ratings'][] = $thisRow;
 		}
 
-		//Only can actually be "stale" if the user has rated the article before
+		// Ratings can only be expired if the user has rated before
+		$ratings[$params['pageid']]['status'] = 'current';
 		if ( $params['userrating'] && $userRatedArticle ) {
 			$dbr = wfGetDb( DB_SLAVE );
 
-			global $wgArticleFeedbackStaleCount;
+			global $wgArticleFeedbackRatingLifetime;
 
 			$res = $dbr->select(
 				'revision',
@@ -120,12 +121,12 @@ class ApiQueryArticleFeedback extends ApiQueryBase {
 				array ( 'LIMIT', $wgArticleFeedbackStaleCount + 1 )
 			);
 
-			if ( $res && $dbr->numRows( $res ) > $wgArticleFeedbackStaleCount ) {
-				//it's stale!
-				$ratings[$params['pageid']]['stale'] = '';
+			if ( $res && $dbr->numRows( $res ) > $wgArticleFeedbackRatingLifetime ) {
+				// Expired status
+				$ratings[$params['pageid']]['status'] = 'expired';
 			}
 		}
-
+		
 		foreach ( $ratings as $rat ) {
 			$result->setIndexedTagName( $rat['ratings'], 'r' );
 			$result->addValue( array( 'query', $this->getModuleName() ), null, $rat );
