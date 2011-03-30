4dde3a0294b213794e8d3e209563156f33fba5e0
catrope <catrope@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-03-30 08:28:58 +0000

    1.17wmf1: Copy UploadWizard from trunk
    git-svn-id: svn+ssh://svn.wikimedia.org/svnroot/mediawiki/branches/wmf@85002 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

---
+++ 1.17wmf1/extensions/UploadWizard/UploadWizard.config.php
@@ -0,0 +1,117 @@
+<?php
+/**
+ * Upload Wizard Configuration 
+ * Do not modify this file, instead use localsettings.php and set: 
+ * $wgUploadWizardConfig[ 'name'] =  'value';
+ */
+global $wgFileExtensions, $wgServer, $wgScriptPath, $wgAPIModules;
+return array(
+	// Upload wizard has an internal debug flag	
+	'debug' => false,
+
+	// File extensions acceptable in this wiki
+	'fileExtensions' =>  $wgFileExtensions, 
+
+	// The default api url is for the current wiki ( can override at run time )
+	'apiUrl' => $wgServer . $wgScriptPath . '/api.php',
+	
+	// If the uploaded file should be auto categorized
+	'autoCategory' => false,
+
+	// These templates must be installed for this to work
+	'licenses' => array(
+		array( 
+			'template' => 'Cc-by-sa-3.0',
+			'messageKey' => 'mwe-upwiz-license-cc-by-sa-3.0', 
+			'default' => true 
+		),
+		array( 
+			'template' => 'Cc-by-3.0', 	
+			'messageKey' => 'mwe-upwiz-license-cc-by-3.0', 		
+			'default' => false 
+		),
+		array( 
+			'template' => 'Cc-zero', 	
+			'messageKey' => 'mwe-upwiz-license-cc-zero', 		
+			'default' => false 
+		)
+	),
+
+	// Default thumbnail width
+	'thumbnailWidth' => 120, 
+ 
+	// Max thumbnail height:
+	'thumbnailMaxHeight' => 200,
+
+	// Min thumbnail width
+	'smallThumbnailWidth' => 60,  
+
+	// Small thumbnail max height
+	'smallThumbnailMaxHeight' => 100,
+
+	// Icon thumbnail width: 
+	'iconThumbnailWidth' =>  32,
+
+	// Icon thumbnail height: 
+	'iconThumbnailMaxHeight' => 32,
+
+	// Max author string length
+	'maxAuthorLength' => 50,
+
+	// Min author string length
+	'minAuthorLength' => 2,
+
+	// Max source string length 
+	'maxSourceLength' => 200,
+
+	// Min source string length
+	'minSourceLength' => 5,
+
+	// Max file title string length
+	'maxTitleLength' => 200,
+
+	// Min file title string length 
+	'minTitleLength' => 5,
+
+	// Max file description length
+	'maxDescriptionLength' => 4096,
+
+	// Min file description length
+	'minDescriptionLength' => 5,
+
+	// Max length for other file information: 
+	'maxOtherInformationLength' => 4096,
+
+	// Max number of simultaneous upload requests 
+	'maxSimultaneousConnections' => 1,
+
+	// Max number of uploads for a given form
+	'maxUploads' => 10,
+
+	// not for use with all wikis. 
+	// The ISO 639 code for the language tagalog is "tl".
+	// Normally we name templates for languages by the ISO 639 code.
+	// Commons already had a template called 'tl:  though.
+	// so, this workaround will cause tagalog descriptions to be saved with this template instead.
+	'languageTemplateFixups' =>  array( 'tl' => 'tgl' ), 
+
+		// XXX this is horribly confusing -- some file restrictions are client side, others are server side
+		// the filename prefix blacklist is at least server side -- all this should be replaced with PHP regex config
+		// or actually, in an ideal world, we'd have some way to reliably detect gibberish, rather than trying to 
+		// figure out what is bad via individual regexes, we'd detect badness. Might not be too hard.
+		//
+		// we can export these to JS if we so want.
+		// filenamePrefixBlacklist: wgFilenamePrefixBlacklist,
+		// 
+		// filenameRegexBlacklist: [
+		//	/^(test|image|img|bild|example?[\s_-]*)$/,  // test stuff
+		//	/^(\d{10}[\s_-][0-9a-f]{10}[\s_-][a-z])$/   // flickr
+		// ]	
+
+	// Check if we want to enable firefogg ( for transcoding ) 
+	'enableFirefogg' => false, 
+
+	// Check if we have the firefogg upload api module enabled: 
+	'enableFirefoggChunkUpload' => isset( $wgAPIModules['firefoggupload'] )? true : false,
+
+);
---
+++ 1.17wmf1/extensions/UploadWizard/UploadWizardHooks.php
@@ -0,0 +1,314 @@
+<?php
+
+/**
+ * Contains list of related resources and hooks which anticipate the use of Resource Loader, whenever that is released
+ */
+class UploadWizardHooks {
+
+	/* We define scripts here for Resource Loader */
+
+	public static $modules = array(
+		// n.b. we tend not to use mediawiki.language functions, they are better implemented in mediawiki.language.parser.
+		// however, loading mediawiki.language will a) create the namespace b) load the language files with convertPlural for the current language and all. 
+		'ext.uploadwizard.mediawiki.language.parser' => array(
+			'dependencies' => array( 'mediawiki.language', 'mediawiki.util' ),
+			'scripts' => 'resources/mediawiki.language.parser.js'
+		),
+		'ext.uploadWizard' => array(
+			'dependencies' => array( 
+				'jquery.ui.core',
+				'jquery.ui.dialog',
+				'jquery.ui.datepicker',
+				'jquery.ui.progressbar',
+				'jquery.ui.widget',
+				'mediawiki.language',
+				'mediawiki.util',
+				'ext.uploadwizard.mediawiki.language.parser'
+			),
+			'scripts' => array(
+
+				// jquery interface helpers
+				'resources/jquery/jquery.tipsy.js',
+				'resources/jquery/jquery.morphCrossfade.js',
+				'resources/jquery/jquery.validate.js',
+				'resources/jquery/jquery.arrowSteps.js',
+				'resources/jquery/jquery.autocomplete.js',
+				'resources/jquery/jquery.spinner.js',
+				'resources/jquery/jquery.autoEllipsis.js',
+				'resources/jquery/jquery.suggestions.js',
+				'resources/jquery/jquery.removeCtrl.js',
+
+				// mediawiki-specific interface helper (relies on mediawiki globals)
+				'resources/jquery/jquery.mwCoolCats.js',
+
+				// common utilities
+				'resources/mw.Log.js',
+				'resources/mw.Utilities.js',
+				'resources/mw.UtilitiesTime.js',
+				'resources/mw.Uri.js',
+				'resources/mw.Api.js',
+				'resources/mw.Api.edit.js',
+				'resources/mw.Title.js',
+
+				// language menus
+				'resources/mw.LanguageUpWiz.js',
+
+				// workhorse libraries
+				'resources/mw.IframeTransport.js',			
+				'resources/mw.ApiUploadHandler.js',
+				'resources/mw.DestinationChecker.js',
+				'resources/mw.UploadWizardUtil.js',			
+
+				// interface libraries
+				'resources/mw.GroupProgressBar.js',
+
+				// UploadWizard specific abstractions
+				'resources/mw.UploadWizardDeed.js',
+				'resources/mw.UploadWizardLicenseInput.js',
+
+				// main library
+				'resources/mw.UploadWizard.js',
+			
+				// main library components: 
+				'resources/mw.UploadWizardDeed.js',
+				'resources/mw.UploadWizardDescription.js',
+				'resources/mw.UploadWizardDetails.js',
+				'resources/mw.UploadWizardUploadInterface.js',
+				
+
+				// launcher
+				'UploadWizardPage.js'
+			),
+			'styles' => array(
+				'resources/jquery/jquery.tipsy.css',
+				'resources/jquery/jquery.suggestions.css',
+				'resources/uploadWizard.css',
+				'resources/jquery/jquery.arrowSteps.css',
+				'resources/jquery/jquery.mwCoolCats.css',
+				'resources/jquery/jquery.removeCtrl.css',
+			),
+			'messages' => array(
+				'uploadwizard',
+				'uploadwizard-desc',
+				'mwe-upwiz-js-off',
+				'mwe-loading-upwiz',
+				'mwe-upwiz-code-unknown',
+				'mwe-upwiz-step-tutorial',
+				'mwe-upwiz-step-file',
+				'mwe-upwiz-step-deeds',
+				'mwe-upwiz-step-details',
+				'mwe-upwiz-step-thanks',
+				'mwe-upwiz-api-error-http',
+				'mwe-upwiz-api-error-ok-but-empty',
+				'mwe-upwiz-api-error-unknown-code',
+				'mwe-upwiz-api-error-uploaddisabled',
+				'mwe-upwiz-api-error-nomodule',
+				'mwe-upwiz-api-error-mustbeposted',
+				'mwe-upwiz-api-error-badaccess-groups',
+				'mwe-upwiz-api-error-stashfailed',
+				'mwe-upwiz-api-error-missingresult',
+				'mwe-upwiz-api-error-missingparam',
+				'mwe-upwiz-api-error-invalid-session-key',
+				'mwe-upwiz-api-error-copyuploaddisabled',
+				'mwe-upwiz-api-error-mustbeloggedin',
+				'mwe-upwiz-api-error-empty-file',
+				'mwe-upwiz-api-error-file-too-large',
+				'mwe-upwiz-api-error-filetype-missing',
+				'mwe-upwiz-api-error-filetype-banned',
+				'mwe-upwiz-api-error-filename-tooshort',
+				'mwe-upwiz-api-error-illegal-filename',
+				'mwe-upwiz-api-error-verification-error',
+				'mwe-upwiz-api-error-hookaborted',
+				'mwe-upwiz-api-error-unknown-error',
+				'mwe-upwiz-api-error-internal-error',
+				'mwe-upwiz-api-error-overwrite',
+				'mwe-upwiz-api-error-badtoken',
+				'mwe-upwiz-api-error-fetchfileerror',
+				'mwe-upwiz-api-error-duplicate',
+				'mwe-upwiz-api-error-duplicate-popup-title',
+				'mwe-upwiz-api-error-timeout',
+				'mwe-upwiz-api-error-noimageinfo',
+				'mwe-upwiz-api-warning-exists',
+				'mwe-upwiz-tutorial-error-localized-file-missing',
+				'mwe-upwiz-tutorial-error-file-missing',
+				'mwe-upwiz-tutorial-error-cannot-transform',
+				'mwe-upwiz-help-desk',
+				'mwe-upwiz-add-file-n',
+				'mwe-upwiz-add-file-0',
+				'mwe-upwiz-add-file-0-free',
+				'mwe-upwiz-browse',
+				'mwe-upwiz-transport-started',
+				'mwe-upwiz-uploading',
+				'mwe-upwiz-transported',
+				'mwe-upwiz-stashed-upload',
+				'mwe-upwiz-getting-metadata',
+				'mwe-upwiz-failed',
+				'mwe-upwiz-click-here',
+				'mwe-upwiz-editing',
+				'mwe-upwiz-remove',
+				'mwe-upwiz-remove-upload',
+				'mwe-upwiz-remove-description',
+				'mwe-upwiz-upload',
+				'mwe-upwiz-file-all-ok',
+				'mwe-upwiz-file-some-failed',
+				'mwe-upwiz-file-retry',
+				'mwe-upwiz-next-file-despite-failures',
+				'mwe-upwiz-skip-tutorial-future',
+				'mwe-upwiz-file-all-failed',
+				'mwe-upwiz-upload-count',
+				'mwe-upwiz-progressbar-uploading',
+				'mwe-upwiz-finished',
+				'mwe-upwiz-secs-remaining',
+				'mwe-upwiz-mins-secs-remaining',
+				'mwe-upwiz-hrs-mins-secs-remaining',
+				'mwe-upwiz-deeds-macro-prompt',
+				'mwe-upwiz-deeds-custom-prompt',
+				'mwe-upwiz-source-ownwork',
+				'mwe-upwiz-source-ownwork-assert-any-license',
+				'mwe-upwiz-source-ownwork-assert',
+				'mwe-upwiz-source-ownwork-assert-custom',
+				'mwe-upwiz-source-ownwork-assert-note',
+				'mwe-upwiz-source-permission',
+				'mwe-upwiz-source-thirdparty',
+				'mwe-upwiz-source-thirdparty-intro',
+				'mwe-upwiz-source-thirdparty-custom-multiple-intro',
+				'mwe-upwiz-source-thirdparty-license',
+				'mwe-upwiz-source-thirdparty-accept',
+				'mwe-upwiz-source-custom',
+				'mwe-upwiz-more-options',
+				'mwe-upwiz-fewer-options',
+				'mwe-upwiz-desc',
+				'mwe-upwiz-desc-add-n',
+				'mwe-upwiz-desc-add-0',
+				'mwe-upwiz-title',
+				'mwe-upwiz-categories-intro',
+				'mwe-upwiz-categories-another',
+				'mwe-upwiz-previously-uploaded',
+				'mwe-upwiz-about-this-work',
+				'mwe-upwiz-media-type',
+				'mwe-upwiz-date-created',
+				'mwe-upwiz-location',
+				'mwe-upwiz-copyright-info',
+				'mwe-upwiz-author',
+				'mwe-upwiz-license',
+				'mwe-upwiz-about-format',
+				'mwe-upwiz-autoconverted',
+				'mwe-upwiz-filename-tag',
+				'mwe-upwiz-other',
+				'mwe-upwiz-other-prefill',
+				'mwe-upwiz-showall',
+				'mwe-upwiz-source',
+				'mwe-upwiz-macro-edit-intro',
+				'mwe-upwiz-macro-edit',
+				'mwe-upwiz-thanks-intro',
+				'mwe-upwiz-thanks-explain',
+				'mwe-upwiz-thanks-link',
+				'mwe-upwiz-thanks-wikitext',
+				'mwe-upwiz-thanks-url',
+				'mwe-upwiz-upload-error-bad-filename-extension',
+				'mwe-upwiz-upload-error-bad-filename-no-extension',
+				'mwe-upwiz-allowed-filename-extensions',
+				'mwe-upwiz-help-allowed-filename-extensions',
+				'mwe-upwiz-upload-error-duplicate',
+				'mwe-upwiz-upload-error-stashed-anyway',
+				'mwe-upwiz-ok',
+				'mwe-upwiz-cancel',
+				'mwe-upwiz-change',
+				'mwe-upwiz-fileexists-replace',
+				'mwe-upwiz-fileexists',
+				'mwe-upwiz-thumbnail-more',
+				'mwe-upwiz-overwrite',
+				'mwe-copyright-macro',
+				'mwe-copyright-custom',
+				'mwe-upwiz-next',
+				'mwe-upwiz-next-file',
+				'mwe-upwiz-next-deeds',
+				'mwe-upwiz-next-details',
+				'mwe-upwiz-home',
+				'mwe-upwiz-upload-another',
+				'mwe-prevent-close',
+				'mwe-upwiz-files-complete',
+				'mwe-upwiz-tooltip-author',
+				'mwe-upwiz-tooltip-source',
+				'mwe-upwiz-tooltip-sign',
+				'mwe-upwiz-tooltip-title',
+				'mwe-upwiz-dialog-title',
+				'mwe-upwiz-tooltip-description',
+				'mwe-upwiz-tooltip-categories',
+				'mwe-upwiz-tooltip-other',
+				'mwe-upwiz-tooltip-more-info',
+				'mwe-upwiz-file-need-file',
+				'mwe-upwiz-file-need-start',
+				'mwe-upwiz-file-need-complete',
+				'mwe-upwiz-deeds-need-deed',
+				'mwe-upwiz-deeds-need-license',
+				'mwe-upwiz-license-incompatible-pd',
+				'mwe-upwiz-license-incompatible-cc',
+				'mwe-upwiz-license-show-all-any-license',
+				'mwe-upwiz-license-show-all',
+				'mwe-upwiz-license-show-recommended',
+				'mwe-upwiz-error-signature-blank',
+				'mwe-upwiz-error-signature-too-long',
+				'mwe-upwiz-error-signature-too-short',
+				'mwe-upwiz-error-signature-bad-chars',
+				'mwe-upwiz-error-blank',
+				'mwe-upwiz-error-too-long',
+				'mwe-upwiz-error-too-short',
+				'mwe-upwiz-error-bad-chars',
+				'mwe-upwiz-error-date',
+				'mwe-upwiz-license-cc-by-sa-3.0',
+				'mwe-upwiz-license-cc-by-3.0',
+				'mwe-upwiz-license-cc-zero',
+				'mwe-upwiz-license-gfdl',
+				'mwe-upwiz-license-pd-us',
+				'mwe-upwiz-categories',
+				'mwe-upwiz-categories-add',
+				'mwe-upwiz-category-remove',
+				'mwe-upwiz-thanks-caption',
+				'mwe-upwiz-help-popup',
+				'mwe-upwiz-help-popup-title',
+				'mwe-upwiz-thumbnail-failed',
+				'mwe-upwiz-unparseable-filename'
+			),
+			'group' => 'ext.uploadWizard'
+		),
+		'ext.uploadWizard.tests' => array(
+			'scripts' => array(
+				'resources/mw.MockUploadHandler.js'
+			),
+		),
+	);
+
+	/*
+	 * ResourceLoaderRegisterModules hook
+	 *
+	 * Adds modules to ResourceLoader
+	 */
+	public static function resourceLoaderRegisterModules( &$resourceLoader ) {
+		global $wgExtensionAssetsPath, $wgUploadWizardDisableResourceLoader;
+		if ( $wgUploadWizardDisableResourceLoader ) {
+			return true;
+		}
+
+		$localpath = dirname( __FILE__ );
+		$remotepath = "$wgExtensionAssetsPath/UploadWizard";
+		foreach ( self::$modules as $name => $resources ) {
+			$resourceLoader->register( $name, new ResourceLoaderFileModule( $resources, $localpath, $remotepath ) );
+		}
+		return true;
+	}
+
+	public static function CanonicalNamespaces( array $namespaces ) {
+		global $wgNamespaceAliases;
+		// add proper aliases for NS_FILE, otherwise an error is being thrown
+		// in combined.min.js when the content language code is not 'en':
+		// "unrecognized namespace=File" due to undefiled 'File' key in wgNamespaceIds
+		if ( !isset( $wgNamespaceAliases['File'] ) ) {
+			$wgNamespaceAliases['File'] = NS_FILE;
+		}
+		if ( !isset( $wgNamespaceAliases['File_talk'] ) ) {
+			$wgNamespaceAliases['File_talk'] = NS_FILE_TALK;
+		}
+		return true;
+	}
+}
