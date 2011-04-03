aed161a1a28e2c39196229b283d305dcf4f116bd
nikola <nikola@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-04-03 02:25:37 +0000

    Don't convert text to a variant, if it is already written in that variant (to reduce use of -{}-)
    git-svn-id: svn+ssh://svn.wikimedia.org/svnroot/mediawiki/branches/nikola@85224 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- phase3/languages/classes/LanguageSr.php
+++ phase3/languages/classes/LanguageSr.php
@@ -148,6 +148,26 @@ class SrConverter extends LanguageConverter {
 
 		return $ret;
 	}
+
+	/**
+	 * Guess if a text is written in Cyrillic or Latin.
+	 *
+	 * @author Nikola Smolenski <smolensk@eunet.rs>
+	 */
+	function guessVariant( $text, $variant ) {
+		$numCyrillic = preg_match_all("/(ш|ђ|ч|ћ|ж|Ш|Ђ|Ч|Ћ|Ж)/", $text, $dummy);
+		$numLatin = preg_match_all("/(š|đ|č|ć|ž|Š|Đ|Č|Ć|Ž)/", $text, $dummy);
+
+		if( $variant == 'sr-ec' ) {
+			return $numCyrillic > $numLatin;
+		} else if( $variant == 'sr-el' ) {
+			return $numLatin > $numCyrillic;
+		} else {
+			return false;
+		}
+
+	}
+
 }
 
 /**
@@ -202,4 +222,5 @@ class LanguageSr extends LanguageSr_ec {
 			}
 		}
 	}
+
 }
