fba9d24269ce082b538f6a5d353b88e0520a8491
catrope <catrope@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-04-26 20:26:58 +0000

    1.17wmf1: Recreate CustomUserSignup by copying from trunk
    git-svn-id: svn+ssh://svn.wikimedia.org/svnroot/mediawiki/branches/wmf@86986 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

---
+++ 1.17wmf1/extensions/CustomUserSignup/CustomUserSignup.i18n.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Internationalization file for the CustomUserSignup extension.
+ *
+ * @file
+ * @ingroup Extensions
+ */
+
+$messages = array();
+
+/** English
+ * @author Nimish Gautam
+ */
+$messages['en'] = array(
+	'customusersignup' => 'Custom User Signup',
+	'customusersignup-desc' => 'Allows for customizable messages during the account creation process',
+);
